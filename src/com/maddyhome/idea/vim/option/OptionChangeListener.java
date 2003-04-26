ccd575a234ba6164be799bf9bc7417491569d56c
rmaddy <rmaddy>
2003-04-26 16:22:44 +0000

    Added to setup of option change listeners

---
+++ src/com/maddyhome/idea/vim/option/OptionChangeListener.java
@@ -0,0 +1,34 @@
+package com.maddyhome.idea.vim.option;
+
+/*
+* IdeaVim - A Vim emulator plugin for IntelliJ Idea
+* Copyright (C) 2003 Rick Maddy
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+import java.util.EventListener;
+
+/**
+ * This interface is used for classes that wish to be notified whenever the value of an option has changed
+ */
+public interface OptionChangeListener extends EventListener
+{
+    /**
+     * The value of the option has changed.
+     * @param event The change event
+     */
+    void valueChange(OptionChangeEvent event);
+}
