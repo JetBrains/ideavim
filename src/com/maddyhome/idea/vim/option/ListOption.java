b7e7ac93bbd2f3fab2d1fa957dfaf286b4fa7290
rmaddy <rmaddy>
2003-04-26 16:03:47 +0000

    *** empty log message ***

--- src/com/maddyhome/idea/vim/option/ListOption.java
+++ src/com/maddyhome/idea/vim/option/ListOption.java
@@ -23,20 +23,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.List;
 
 /**
- *
+ * This is an option that accepts an arbitrary list of values
  */
 public class ListOption extends TextOption
 {
-    ListOption(String name, String abbrev, String[] dflt)
+    /**
+     * Creates the option
+     * @param name The name of the option
+     * @param abbrev The short name
+     * @param dflt The option's default values
+     * @param pattern A regular expression that is used to validate new values. null if no check needed
+     */
+    ListOption(String name, String abbrev, String[] dflt, String pattern)
     {
         super(name, abbrev);
 
         this.dflt = new ArrayList(Arrays.asList(dflt));
         this.value = new ArrayList(this.dflt);
+        this.pattern = pattern;
     }
 
+    /**
+     * Gets the value of the option as a comma separated list of values
+     * @return The option's value
+     */
     public String getValue()
     {
         StringBuffer res = new StringBuffer();
@@ -54,26 +67,64 @@ public class ListOption extends TextOption
         return res.toString();
     }
 
+    /**
+     * Gets the option's values as a list
+     * @return The option's values
+     */
+    public List values()
+    {
+        return value;
+    }
+
+    /**
+     * Determines if this option has all the values listed on the supplied value
+     * @param val A comma separated list of values to look for
+     * @return True if all the supplied values are set in this option, false if not
+     */
     public boolean contains(String val)
     {
         return value.containsAll(parseVals(val));
     }
 
+    /**
+     * Sets this option to contain just the supplied values. If any of the supplied values are invalid then this
+     * option is not updated at all.
+     * @param val A comma separated list of values
+     * @return True if all the supplied values were correct, false if not
+     */
     public boolean set(String val)
     {
         return set(parseVals(val));
     }
 
+    /**
+     * Adds the supplied values to the current list of values. If any of the supplied values are invalid then this
+     * option is not updated at all.
+     * @param val A comma separated list of values
+     * @return True if all the supplied values were correct, false if not
+     */
     public boolean append(String val)
     {
         return append(parseVals(val));
     }
 
+    /**
+     * Adds the supplied values to the start of the current list of values. If any of the supplied values are invalid
+     * then this option is not updated at all.
+     * @param val A comma separated list of values
+     * @return True if all the supplied values were correct, false if not
+     */
     public boolean prepend(String val)
     {
         return prepend(parseVals(val));
     }
 
+    /**
+     * Removes the supplied values from the current list of values. If any of the supplied values are invalid then this
+     * option is not updated at all.
+     * @param val A comma separated list of values
+     * @return True if all the supplied values were correct, false if not
+     */
     public boolean remove(String val)
     {
         return remove(parseVals(val));
@@ -81,28 +132,52 @@ public class ListOption extends TextOption
 
     protected boolean set(ArrayList vals)
     {
+        if (vals == null)
+        {
+            return false;
+        }
+
         value = vals;
+        fireOptionChangeEvent();
 
         return true;
     }
 
     protected boolean append(ArrayList vals)
     {
+        if (vals == null)
+        {
+            return false;
+        }
+
         value.addAll(vals);
+        fireOptionChangeEvent();
 
         return true;
     }
 
     protected boolean prepend(ArrayList vals)
     {
+        if (vals == null)
+        {
+            return false;
+        }
+
         value.addAll(0, vals);
+        fireOptionChangeEvent();
 
         return true;
     }
 
     protected boolean remove(ArrayList vals)
     {
+        if (vals == null)
+        {
+            return false;
+        }
+
         value.removeAll(vals);
+        fireOptionChangeEvent();
 
         return true;
     }
@@ -110,26 +185,48 @@ public class ListOption extends TextOption
     protected ArrayList parseVals(String val)
     {
         ArrayList res = new ArrayList();
-        StringTokenizer tokenizer = new StringTokenizer(",");
+        StringTokenizer tokenizer = new StringTokenizer(val, ",");
         while (tokenizer.hasMoreTokens())
         {
             String token = tokenizer.nextToken().trim();
-            res.add(token);
+            if (pattern == null || token.matches(pattern))
+            {
+                res.add(token);
+            }
+            else
+            {
+                return null;
+            }
         }
 
         return res;
     }
 
+    /**
+     * Checks to see if the current value of the option matches the default value
+     * @return True if equal to default, false if not
+     */
     public boolean isDefault()
     {
         return dflt.equals(value);
     }
 
+    /**
+     * Resets the option to its default value
+     */
     public void resetDefault()
     {
-        value = new ArrayList(dflt);
+        if (!dflt.equals(value))
+        {
+            value = new ArrayList(dflt);
+            fireOptionChangeEvent();
+        }
     }
 
+    /**
+     * Gets the string representation appropriate for output to :set all
+     * @return The option as a string {name}={value list}
+     */
     public String toString()
     {
         StringBuffer res = new StringBuffer();
@@ -143,4 +240,5 @@ public class ListOption extends TextOption
 
     protected ArrayList dflt;
     protected ArrayList value;
+    protected String pattern;
 }
