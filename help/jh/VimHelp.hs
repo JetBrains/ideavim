96c5a4bc5b40e8bf76d98b72475c6411a047710e
olegshpynov <olegshpynov>
2010-02-25 16:20:41 +0000

    help content was restored

---
+++ help/jh/VimHelp.hs
@@ -0,0 +1,42 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+    PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+    "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
+<helpset version="1.0">
+
+    <!-- title -->
+    <title>IdeaVim Help</title>
+
+    <!-- maps -->
+    <maps>
+        <homeID>vim.help.txt</homeID>
+        <mapref location="Map.jhm"/>
+    </maps>
+
+    <!-- views -->
+    <view>
+        <name>TOC</name>
+        <label>Table Of Contents</label>
+        <type>javax.help.TOCView</type>
+        <data>VimTOC.xml</data>
+    </view>
+
+    <!--
+    <view>
+        <name>Index</name>
+        <label>Index</label>
+        <type>javax.help.IndexView</type>
+        <data>VimIndex.xml</data>
+    </view>
+    -->
+
+    <view>
+        <name>Search</name>
+        <label>Search</label>
+        <type>javax.help.SearchView</type>
+        <data>MasterSearchIndex</data>
+    </view>
+
+</helpset>
+
