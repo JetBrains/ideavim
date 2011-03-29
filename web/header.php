e6fc8cd65eaf6132863bf837e7063125de5b4f41
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2011-03-29 18:52:17 +0400

    Cleanup resources

--- web/header.php
+++
@@ -1,8 +0,0 @@
-<div align="center">
-<a href="index.php">Home</a>
-| <a href="vim/index.txt">Reference</a>
-| <a href="http://sourceforge.net/project/showfiles.php?group_id=79039">Download</a>
-| <a href="http://sourceforge.net/tracker/?group_id=79039&atid=555309">Bug Reporting</a>
-| <a href="http://sourceforge.net/cvs/?group_id=79039">CVS Access</a>
-| <a href="faq.php">FAQ</a>
-</div>
