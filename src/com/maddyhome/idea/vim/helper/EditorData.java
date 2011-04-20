fbfbaad0c0e5e2be85ed38cfe437400573a1b914
Oleg Shpynov <oleg.shpynov@jebrains.com>
2011-04-20 17:50:41 +0400

    DRY refactoring

--- src/com/maddyhome/idea/vim/helper/EditorData.java
+++ src/com/maddyhome/idea/vim/helper/EditorData.java
@@ -19,13 +19,13 @@ package com.maddyhome.idea.vim.helper;
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-import com.intellij.injected.editor.EditorWindow;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.markup.RangeHighlighter;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtil;
 import com.intellij.testFramework.LightVirtualFile;
 import com.maddyhome.idea.vim.command.CommandState;
 import com.maddyhome.idea.vim.command.VisualChange;
@@ -45,7 +45,7 @@ public class EditorData {
    * @param editor The editor to initialize
    */
   public static void initializeEditor(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     if (logger.isDebugEnabled()) logger.debug("editor created: " + editor);
   }
 
@@ -55,7 +55,7 @@ public class EditorData {
    * @param editor The editor to cleanup
    */
   public static void uninitializeEditor(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     if (logger.isDebugEnabled()) logger.debug("editor closed: " + editor);
     editor.putUserData(COMMAND_STATE, null);
     editor.putUserData(LAST_HIGHLIGHTS, null);
@@ -70,7 +70,7 @@ public class EditorData {
    * @return Returns the last column as set by {@link #setLastColumn} or the current cursor column
    */
   public static int getLastColumn(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     Integer col = editor.getUserData(LAST_COLUMN);
     if (col == null) {
       return EditorHelper.getCurrentVisualColumn(editor);
@@ -87,29 +87,29 @@ public class EditorData {
    * @param editor The editor
    */
   public static void setLastColumn(Editor editor, int col) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     editor.putUserData(LAST_COLUMN, col);
     int t = getLastColumn(editor);
     if (logger.isDebugEnabled()) logger.debug("setLastColumn(" + col + ") is now " + t);
   }
 
   public static String getLastSearch(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     return editor.getUserData(LAST_SEARCH);
   }
 
   public static void setLastSearch(Editor editor, String search) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     editor.putUserData(LAST_SEARCH, search);
   }
 
   public static Collection<RangeHighlighter> getLastHighlights(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     return editor.getUserData(LAST_HIGHLIGHTS);
   }
 
   public static void setLastHighlights(Editor editor, Collection<RangeHighlighter> highlights) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     editor.putUserData(LAST_HIGHLIGHTS, highlights);
   }
 
@@ -120,7 +120,7 @@ public class EditorData {
    * @return The last visual range, null if no previous range
    */
   public static VisualRange getLastVisualRange(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     return editor.getDocument().getUserData(VISUAL);
   }
 
@@ -131,7 +131,7 @@ public class EditorData {
    * @param range  The visual range
    */
   public static void setLastVisualRange(Editor editor, VisualRange range) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     editor.getDocument().putUserData(VISUAL, range);
   }
 
@@ -142,7 +142,7 @@ public class EditorData {
    * @return The last visual range, null if no previous range
    */
   public static VisualChange getLastVisualOperatorRange(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     return editor.getDocument().getUserData(VISUAL_OP);
   }
 
@@ -153,22 +153,22 @@ public class EditorData {
    * @param range  The visual range
    */
   public static void setLastVisualOperatorRange(Editor editor, VisualChange range) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     editor.getDocument().putUserData(VISUAL_OP, range);
   }
 
   public static CommandState getCommandState(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     return editor.getUserData(COMMAND_STATE);
   }
 
   public static void setCommandState(Editor editor, CommandState state) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     editor.putUserData(COMMAND_STATE, state);
   }
 
   public static boolean getChangeGroup(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     Boolean res = editor.getUserData(CHANGE_GROUP);
     if (res != null) {
       return res;
@@ -179,22 +179,22 @@ public class EditorData {
   }
 
   public static void setChangeGroup(Editor editor, boolean adapter) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     editor.putUserData(CHANGE_GROUP, adapter);
   }
 
   public static boolean getMotionGroup(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     return editor.getUserData(MOTION_GROUP) == Boolean.TRUE;
   }
 
   public static void setMotionGroup(Editor editor, boolean adapter) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     editor.putUserData(MOTION_GROUP, adapter);
   }
 
   public static boolean isConsoleOutput(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     Object res = editor.getUserData(CONSOLE_OUTPUT);
     logger.debug("isConsoleOutput for editor " + editor + " - " + res);
     if (res != null) {
@@ -212,7 +212,7 @@ public class EditorData {
    * @return The virtual file for the editor
    */
   public static VirtualFile getVirtualFile(Editor editor) {
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     return FileDocumentManager.getInstance().getFile(editor.getDocument());
   }
 
@@ -265,19 +265,11 @@ public class EditorData {
     }
   }
 
-  public static Editor getTopmostEditor(Editor editor) {
-    // Fix for the VIM-61 "enter" key not inserting new line in insert mode and similar problems
-    // Actually one file can have several editors organized in the hierarhy.
-    // However ideavim should not care about and store it information within the top editor.
-    editor = editor instanceof EditorWindow ? ((EditorWindow)editor).getDelegate() : editor;
-    return editor;
-  }
-
   /**
    * Checks if editor is file editor, also it takes into account that editor can be placed in editors hierarhy
    */
   public static boolean isFileEditor(Editor editor){
-    editor = getTopmostEditor(editor);
+    editor = InjectedLanguageUtil.getTopLevelEditor(editor);
     final VirtualFile virtualFile = EditorData.getVirtualFile(editor);
     return virtualFile != null && !(virtualFile instanceof LightVirtualFile);
   }
