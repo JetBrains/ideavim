5b580a0a69fbb7dcf1e003d13c7da3490205117e
Ksenia V. Mamich <kvmamich@gmail.com>
2011-10-12 14:59:47 +0400

    Added 'lang' directory & VimScriptLanguage

---
+++ src/com/maddyhome/idea/vim/lang/VimscriptLanguage.java
@@ -0,0 +1,11 @@
+package com.maddyhome.idea.vim.lang;
+
+/**
+ * <p>Date: 12.10.11</p>
+ * <p>DESCRIPTION</p>
+ *
+ * @author Ksenia V. Mamich
+ * @version 1.0
+ */
+public class VimscriptLanguage {
+}
