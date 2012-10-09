df4ac6d6424862cae0b7d86326c474e2c1ed51dd
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2012-10-09 21:15:51 +0400

    Base class for action tests

---
+++ test/org/jetbrains/plugins/ideavim/VimTestCase.java
@@ -0,0 +1,46 @@
+package org.jetbrains.plugins.ideavim;
+
+import com.intellij.openapi.application.PathManager;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.PlatformTestCase;
+import com.intellij.testFramework.UsefulTestCase;
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
+import com.intellij.testFramework.fixtures.TestFixtureBuilder;
+import com.intellij.testFramework.fixtures.impl.LightTempDirTestFixtureImpl;
+
+/**
+ * @author vlan
+ */
+public abstract class VimTestCase extends UsefulTestCase {
+  protected CodeInsightTestFixture myFixture;
+
+  public VimTestCase() {
+    PlatformTestCase.initPlatformLangPrefix();
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    final IdeaTestFixtureFactory factory = IdeaTestFixtureFactory.getFixtureFactory();
+    final LightProjectDescriptor projectDescriptor = LightProjectDescriptor.EMPTY_PROJECT_DESCRIPTOR;
+    final TestFixtureBuilder<IdeaProjectTestFixture> fixtureBuilder = factory.createLightFixtureBuilder(projectDescriptor);
+    final IdeaProjectTestFixture fixture = fixtureBuilder.getFixture();
+    myFixture = IdeaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(fixture,
+                                                                                    new LightTempDirTestFixtureImpl(true));
+    myFixture.setUp();
+    myFixture.setTestDataPath(getTestDataPath());
+  }
+
+  protected String getTestDataPath() {
+    return PathManager.getHomePath() + "/community/plugins/ideavim/testData";
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    myFixture.tearDown();
+    myFixture = null;
+    super.tearDown();
+  }
+}
