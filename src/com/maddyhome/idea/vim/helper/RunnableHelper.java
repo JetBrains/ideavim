228f0a3f315c6db27cec4337e489925d05c888bc
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2012-10-09 21:41:36 +0400

    Nullable annotations

--- src/com/maddyhome/idea/vim/helper/RunnableHelper.java
+++ src/com/maddyhome/idea/vim/helper/RunnableHelper.java
@@ -24,47 +24,52 @@ import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.maddyhome.idea.vim.KeyHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * This provides some helper methods to run code as a command and an application write action
  */
 public class RunnableHelper {
-  public static void runReadCommand(Project project, Runnable cmd, String name, Object groupId) {
-    if (logger.isDebugEnabled()) logger.debug("read command " + cmd);
+  private static final Logger logger = Logger.getInstance(KeyHandler.class.getName());
+
+  private RunnableHelper() {}
+
+  public static void runReadCommand(@Nullable Project project, @NotNull Runnable cmd, @Nullable String name, @Nullable Object groupId) {
+    if (logger.isDebugEnabled()) {
+      logger.debug("read command " + cmd);
+    }
     CommandProcessor.getInstance().executeCommand(project, new ReadAction(cmd), name, groupId);
   }
 
-  public static void runWriteCommand(Project project, Runnable cmd, String name, Object groupId) {
-    if (logger.isDebugEnabled()) logger.debug("write command " + cmd);
+  public static void runWriteCommand(@Nullable Project project, @NotNull Runnable cmd, @Nullable String name, @Nullable Object groupId) {
+    if (logger.isDebugEnabled()) {
+      logger.debug("write command " + cmd);
+    }
     CommandProcessor.getInstance().executeCommand(project, new WriteAction(cmd), name, groupId);
   }
 
   static class ReadAction implements Runnable {
-    ReadAction(Runnable cmd) {
+    @NotNull private final Runnable cmd;
+
+    ReadAction(@NotNull Runnable cmd) {
       this.cmd = cmd;
     }
 
     public void run() {
       ApplicationManager.getApplication().runReadAction(cmd);
     }
-
-    Runnable cmd;
   }
 
   static class WriteAction implements Runnable {
-    WriteAction(Runnable cmd) {
+    @NotNull private final Runnable cmd;
+
+    WriteAction(@NotNull Runnable cmd) {
       this.cmd = cmd;
     }
 
     public void run() {
       ApplicationManager.getApplication().runWriteAction(cmd);
     }
-
-    Runnable cmd;
   }
-
-  private RunnableHelper() {
-  }
-
-  private static Logger logger = Logger.getInstance(KeyHandler.class.getName());
 }
