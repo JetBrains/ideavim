0872a0c3b57a14f1ec551fbb12d4db3e8287d58f
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2012-10-15 20:21:46 +0400

    More motion tests

--- test/org/jetbrains/plugins/ideavim/action/MotionActionTest.java
+++ test/org/jetbrains/plugins/ideavim/action/MotionActionTest.java
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.ideavim.action;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.maddyhome.idea.vim.KeyHandler;
+import com.maddyhome.idea.vim.command.CommandState;
 import com.maddyhome.idea.vim.helper.EditorDataContext;
 import com.maddyhome.idea.vim.helper.RunnableHelper;
 import org.jetbrains.annotations.NotNull;
@@ -17,27 +18,93 @@ import java.util.List;
 public class MotionActionTest extends VimTestCase {
   // VIM-198
   public void testVisualMotionInnerWordNewLineAtEOF() {
-    final Editor editor = typeTextInFile("viw",
+    final Editor editor = typeTextInFile(toKeyStrokes("viw"),
                                          "one tw<caret>o\n");
     final String selected = editor.getSelectionModel().getSelectedText();
     assertEquals("two", selected);
   }
 
   public void testVisualMotionInnerBigWord() {
-    final Editor editor = typeTextInFile("viW",
+    final Editor editor = typeTextInFile(toKeyStrokes("viW"),
                                          "one tw<caret>o.three four\n");
     final String selected = editor.getSelectionModel().getSelectedText();
     assertEquals("two.three", selected);
   }
 
+  public void testEscapeInCommand() {
+    final List<KeyStroke> keys = toKeyStrokes("f");
+    keys.add(KeyStroke.getKeyStroke("ESCAPE"));
+    // We cannot check yet that the plugin indicates an error (see 'visualbell') after the second ESCAPE, so we just check the caret
+    // position and the editor mode
+    keys.add(KeyStroke.getKeyStroke("ESCAPE"));
+    final Editor editor = typeTextInFile(keys,
+                                         "on<caret>e two\n" +
+                                         "three\n");
+    final int offset = editor.getCaretModel().getOffset();
+    assertEquals(2, offset);
+    final int mode = CommandState.getInstance(editor).getMode();
+    assertEquals(CommandState.MODE_COMMAND, mode);
+  }
+
+  public void testFewDigitsCountMove() {
+    final Editor editor = typeTextInFile(toKeyStrokes("12l"),
+                                         "on<caret>e two three four five six seven\n");
+    final int offset = editor.getCaretModel().getOffset();
+    assertEquals(14, offset);
+  }
+
+  public void testDeleteDigitsInCount() {
+    final List<KeyStroke> keys = toKeyStrokes("42");
+    keys.add(KeyStroke.getKeyStroke("DELETE"));
+    keys.add(KeyStroke.getKeyStroke('l'));
+    final Editor editor = typeTextInFile(keys,
+                                         "on<caret>e two three four five six seven\n");
+    final int offset = editor.getCaretModel().getOffset();
+    assertEquals(6, offset);
+  }
+
+  public void testForwardToTab() {
+    final List<KeyStroke> keys = toKeyStrokes("f");
+    keys.add(KeyStroke.getKeyStroke("TAB"));
+    final Editor editor = typeTextInFile(keys,
+                                         "on<caret>e two\tthree\nfour\n");
+    final int offset = editor.getCaretModel().getOffset();
+    assertEquals(7, offset);
+    final int mode = CommandState.getInstance(editor).getMode();
+    assertEquals(CommandState.MODE_COMMAND, mode);
+  }
+
+  public void testIllegalCharArgument() {
+    final List<KeyStroke> keys = toKeyStrokes("f");
+    keys.add(KeyStroke.getKeyStroke("INSERT"));
+    final Editor editor = typeTextInFile(keys,
+                                         "on<caret>e two three four five six seven\n");
+    final int offset = editor.getCaretModel().getOffset();
+    assertEquals(2, offset);
+    final int mode = CommandState.getInstance(editor).getMode();
+    assertEquals(CommandState.MODE_COMMAND, mode);
+  }
+
+  public void testBackToDigraph() {
+    final List<KeyStroke> keys = toKeyStrokes("F");
+    keys.add(KeyStroke.getKeyStroke("control K"));
+    keys.add(KeyStroke.getKeyStroke('O'));
+    keys.add(KeyStroke.getKeyStroke(':'));
+    final Editor editor = typeTextInFile(keys,
+                                         "Hallo, Öster<caret>reich!\n");
+    final int offset = editor.getCaretModel().getOffset();
+    assertEquals(7, offset);
+    final int mode = CommandState.getInstance(editor).getMode();
+    assertEquals(CommandState.MODE_COMMAND, mode);
+  }
+
   @NotNull
-  private Editor typeTextInFile(@NotNull String input, @NotNull String fileContents) {
+  private Editor typeTextInFile(@NotNull final List<KeyStroke> keys, @NotNull String fileContents) {
     myFixture.configureByText("a.java", fileContents);
     final Editor editor = myFixture.getEditor();
     final KeyHandler keyHandler = KeyHandler.getInstance();
     final EditorDataContext dataContext = new EditorDataContext(editor);
     final Project project = myFixture.getProject();
-    final List<KeyStroke> keys = toKeyStrokes(input);
     RunnableHelper.runWriteCommand(project, new Runnable() {
       @Override
       public void run() {
