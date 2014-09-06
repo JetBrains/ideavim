16ce16c6322b52211697b209701496d219c8017f
Dathan Bennett <dbennett@palantir.com>
2014-09-06 02:59:49 -0700

    Add test for VIM-569 fix

--- test/org/jetbrains/plugins/ideavim/action/ChangeActionTest.java
+++ test/org/jetbrains/plugins/ideavim/action/ChangeActionTest.java
@@ -244,6 +244,13 @@ public class ChangeActionTest extends VimTestCase {
            "fooar\n");
   }
 
+  // VIM-569 |i| |i_CTRL-W|
+  public void testDeletePreviousWordRightMargin() {
+    doTest(parseKeys("i", "<C-w>"),
+           "this is a sentence.<caret>",
+           "this is a<caret> ");
+  }
+
   private void doTest(final List<KeyStroke> keys, String before, String after) {
     myFixture.configureByText("a.java", before);
     final Editor editor = myFixture.getEditor();
