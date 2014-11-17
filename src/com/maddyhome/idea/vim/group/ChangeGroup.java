858541221571c71c72d9c5a641ef4077b97bc7ee
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2014-11-18 01:23:29 +0200

    Optimize character case changing operations
    Previously, performing e.g. visual '~' on a large file would completely
    lock up the IDE. The culprit seemed to be the calls to replaceText() one
    character at a time, which is not a cheap operation since each
    replaceText() will for example trigger a DocumentChanged event.

--- src/com/maddyhome/idea/vim/group/ChangeGroup.java
+++ src/com/maddyhome/idea/vim/group/ChangeGroup.java
@@ -1292,16 +1292,11 @@ public class ChangeGroup {
     }
 
     CharSequence chars = editor.getDocument().getCharsSequence();
+    StringBuilder sb = new StringBuilder();
     for (int i = start; i < end; i++) {
-      if (i >= chars.length()) {
-        break;
-      }
-
-      char ch = CharacterHelper.changeCase(chars.charAt(i), type);
-      if (ch != chars.charAt(i)) {
-        replaceText(editor, i, i + 1, Character.toString(ch));
-      }
+      sb.append(CharacterHelper.changeCase(chars.charAt(i), type));
     }
+    replaceText(editor, start, end, sb.toString());
   }
 
   public void autoIndentLines(@NotNull DataContext context) {
