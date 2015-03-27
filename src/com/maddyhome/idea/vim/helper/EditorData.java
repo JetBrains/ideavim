89e853158c67d99966d830f75ecaf745911a1adc
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2015-03-27 21:19:56 +0300

    Typos

--- src/com/maddyhome/idea/vim/helper/EditorData.java
+++ src/com/maddyhome/idea/vim/helper/EditorData.java
@@ -59,7 +59,7 @@ public class EditorData {
    *
    * @param editor The editor to cleanup
    */
-  public static void uninitializeEditor(@NotNull Editor editor) {
+  public static void unInitializeEditor(@NotNull Editor editor) {
     if (logger.isDebugEnabled()) logger.debug("editor closed: " + editor);
     editor.putUserData(COMMAND_STATE, null);
     editor.putUserData(LAST_HIGHLIGHTS, null);
@@ -73,7 +73,7 @@ public class EditorData {
   /**
    * This gets the last column the cursor was in for the editor.
    *
-   * @param editor The editr to get the last column from
+   * @param editor The editor to get the last column from
    * @return Returns the last column as set by {@link #setLastColumn} or the current cursor column
    */
   public static int getLastColumn(@NotNull Editor editor) {
@@ -285,8 +285,8 @@ public class EditorData {
       // coded such that two keys with the same name are treated as different keys - oh well.
       // This code will work as long as the key I need is the first one in the ConsoleViewImpl.
       Class cvi = Class.forName("com.intellij.execution.impl.ConsoleViewImpl");
-      Field[] flds = cvi.getDeclaredFields();
-      for (Field f : flds) {
+      Field[] fields = cvi.getDeclaredFields();
+      for (Field f : fields) {
         if (f.getType().equals(Key.class)) {
           f.setAccessible(true);
           CONSOLE_VIEW_IN_EDITOR_VIEW = (Key)f.get(null);
@@ -303,7 +303,7 @@ public class EditorData {
   }
 
   /**
-   * Checks if editor is file editor, also it takes into account that editor can be placed in editors hierarhy
+   * Checks if editor is file editor, also it takes into account that editor can be placed in editors hierarchy
    */
   public static boolean isFileEditor(@NotNull Editor editor){
     final VirtualFile virtualFile = EditorData.getVirtualFile(editor);
