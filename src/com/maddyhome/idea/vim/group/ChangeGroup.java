b98427472fb43004516c6b2bb83af7848bf68522
qzed <qzed@users.noreply.github.com>
2016-07-27 03:06:12 +0200

    VIM-1110 Fix caret jumping after escape from block-insert mode

--- src/com/maddyhome/idea/vim/group/ChangeGroup.java
+++ src/com/maddyhome/idea/vim/group/ChangeGroup.java
@@ -451,7 +451,7 @@ public class ChangeGroup {
     if (repeatLines > 0) {
       int vline = editor.getCaretModel().getVisualPosition().line;
       int lline = editor.getCaretModel().getLogicalPosition().line;
-      cpos = editor.logicalPositionToOffset(new LogicalPosition(vline, repeatColumn));
+      cpos = editor.logicalPositionToOffset(new LogicalPosition(lline, repeatColumn));
       for (int i = 0; i < repeatLines; i++) {
         if (repeatAppend && repeatColumn < MotionGroup.LAST_COLUMN &&
             EditorHelper.getVisualLineLength(editor, vline + i) < repeatColumn) {
