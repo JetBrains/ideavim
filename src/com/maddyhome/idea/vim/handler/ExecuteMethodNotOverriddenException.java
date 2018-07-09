99e379392bfd28b18c50f756e98c25dc4733457b
Vitalii Karavaev <fkve97@gmail.com>
2018-07-09 09:54:41 +0300

    Fix typo

--- src/com/maddyhome/idea/vim/handler/ExecuteMethodNotOverriddenException.java
+++ src/com/maddyhome/idea/vim/handler/ExecuteMethodNotOverriddenException.java
@@ -2,6 +2,6 @@ package com.maddyhome.idea.vim.handler;
 
 public class ExecuteMethodNotOverriddenException extends Exception {
   public ExecuteMethodNotOverriddenException(Class<?> child) {
-    super("The proper execute() method is not overriden in " + child.getName());
+    super("The proper execute() method is not overridden in " + child.getName());
   }
 }
