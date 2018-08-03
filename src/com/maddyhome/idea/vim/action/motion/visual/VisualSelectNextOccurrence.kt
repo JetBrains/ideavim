fc0a950af13571f28c17b90773a3925f33170fee
Vitalii Karavaev <fkve97@gmail.com>
2018-08-03 14:35:23 +0300

    Cleanup

--- src/com/maddyhome/idea/vim/action/motion/visual/VisualSelectNextOccurrence.kt
+++ src/com/maddyhome/idea/vim/action/motion/visual/VisualSelectNextOccurrence.kt
@@ -1,6 +1,7 @@
 package com.maddyhome.idea.vim.action.motion.visual
 
 import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.editor.Editor
 import com.maddyhome.idea.vim.VimPlugin
 import com.maddyhome.idea.vim.action.VimCommandAction
@@ -10,10 +11,8 @@ import com.maddyhome.idea.vim.command.CommandState
 import com.maddyhome.idea.vim.command.CommandState.Mode
 import com.maddyhome.idea.vim.command.MappingMode
 import com.maddyhome.idea.vim.group.MotionGroup
-import com.maddyhome.idea.vim.handler.CaretOrder
 import com.maddyhome.idea.vim.handler.MotionEditorActionHandler
 import com.maddyhome.idea.vim.helper.CaretData
-import com.maddyhome.idea.vim.helper.EditorHelper
 import com.maddyhome.idea.vim.helper.SearchHelper
 import java.util.*
 import javax.swing.KeyStroke
@@ -33,34 +32,30 @@ class VisualSelectNextOccurrence : VimCommandAction(Handler()) {
 }
 
 fun MotionGroup.selectNextSearch(editor: Editor): Int {
-  val lastCaret = EditorHelper.getOrderedCaretsList(editor,
-                                                    CaretOrder.DECREASING_OFFSET)[0]
 
-  val nextOffset = VimPlugin.getSearch().searchNext(editor, lastCaret, 1)
-  if (nextOffset == -1) return nextOffset
-
-  val state = CommandState.getInstance(editor)
-  if (editor.caretModel.caretCount == 1 && state.mode != Mode.VISUAL) {
-    lastCaret.moveToOffset(nextOffset)
-
-    state.pushState(Mode.VISUAL, CommandState.SubMode.VISUAL_CHARACTER, MappingMode.VISUAL)
-
-    val range = SearchHelper.findWordUnderCursor(editor, lastCaret) ?: return -1
+  fun addNewSelection(caret: Caret): Int {
+    val range = SearchHelper.findWordUnderCursor(editor, caret) ?: return -1
     val startOffset = range.startOffset
     val endOffset = range.endOffset - 1
-    CaretData.setVisualStart(lastCaret, startOffset)
-    updateSelection(editor, lastCaret, endOffset)
+    CaretData.setVisualStart(caret, startOffset)
+    updateSelection(editor, caret, endOffset)
 
     return endOffset
   }
 
-  val caret = editor.caretModel.addCaret(editor.offsetToVisualPosition(nextOffset), true) ?: return -1
-  val range = SearchHelper.findWordUnderCursor(editor, caret) ?: return -1
-  val startOffset = range.startOffset
-  val endOffset = range.endOffset - 1
-  CaretData.setVisualStart(caret, startOffset)
-  updateSelection(editor, caret, endOffset)
+  val caretModel = editor.caretModel
+  val primaryCaret = caretModel.primaryCaret
+  val nextOffset = VimPlugin.getSearch().searchNext(editor, primaryCaret, 1)
+  if (nextOffset == -1) return nextOffset
 
-  return endOffset
+  val state = CommandState.getInstance(editor)
+  if (caretModel.caretCount == 1 && state.mode != Mode.VISUAL) {
+    primaryCaret.moveToOffset(nextOffset)
+    state.pushState(Mode.VISUAL, CommandState.SubMode.VISUAL_CHARACTER, MappingMode.VISUAL)
+    return addNewSelection(primaryCaret)
+  }
+
+  val caret = caretModel.addCaret(editor.offsetToVisualPosition(nextOffset), true) ?: return -1
+  return addNewSelection(caret)
 }
 
