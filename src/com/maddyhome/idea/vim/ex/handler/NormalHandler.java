387c443f8ea577b5b4e219eb7bc63d3f9750378f
Alex Plate <aleksei.plate@jetbrains.com>
2019-01-12 17:04:37 +0300

    Support ranges for normal command

--- src/com/maddyhome/idea/vim/ex/handler/NormalHandler.java
+++ src/com/maddyhome/idea/vim/ex/handler/NormalHandler.java
@@ -9,8 +9,10 @@ import com.maddyhome.idea.vim.command.CommandState;
 import com.maddyhome.idea.vim.ex.CommandHandler;
 import com.maddyhome.idea.vim.ex.ExCommand;
 import com.maddyhome.idea.vim.ex.ExException;
+import com.maddyhome.idea.vim.ex.LineRange;
 import com.maddyhome.idea.vim.handler.CaretOrder;
 import com.maddyhome.idea.vim.handler.ExecuteMethodNotOverriddenException;
+import com.maddyhome.idea.vim.helper.EditorHelper;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -26,29 +28,43 @@ public class NormalHandler extends CommandHandler {
 
     @Override
     public boolean execute(@NotNull Editor editor, @NotNull Caret caret, @NotNull DataContext context, @NotNull ExCommand cmd) throws ExException, ExecuteMethodNotOverriddenException {
+
         String argument = cmd.getArgument();
         boolean useMapping = true;
 
-        if (!argument.isEmpty() && argument.charAt(0) == '!') {
-            useMapping = false;
-            argument = argument.substring(1).trim();
-        }
+        boolean rangeUsed = cmd.getRanges().size() != 0;
+        LineRange range = cmd.getLineRange(editor, caret, context);
 
-        List<KeyStroke> keys = parseKeys(argument);
-        KeyHandler keyHandler = KeyHandler.getInstance();
-        keyHandler.reset(editor);
-        for (KeyStroke key : keys) {
-            keyHandler.handleKey(editor, key, context, useMapping);
-        }
+        for (int line = range.getStartLine(); line <= range.getEndLine(); line++) {
+            if (rangeUsed) {
+                if (editor.getDocument().getLineCount() < line) {
+                    break;
+                }
+                int startOffset = EditorHelper.getLineStartOffset(editor, line);
+                editor.getCaretModel().moveToOffset(startOffset);
+            }
 
-        // Exit if state leaves as insert or cmd_line
-        CommandState commandState = CommandState.getInstance(editor);
-        CommandState.Mode mode = commandState.getMode();
-        if (mode == CommandState.Mode.EX_ENTRY) {
-            VimPlugin.getProcess().cancelExEntry(editor, context);
-        }
-        if (mode == CommandState.Mode.INSERT || mode == CommandState.Mode.REPLACE) {
-            VimPlugin.getChange().processEscape(editor, context);
+            if (!argument.isEmpty() && argument.charAt(0) == '!') {
+                useMapping = false;
+                argument = argument.substring(1).trim();
+            }
+
+            List<KeyStroke> keys = parseKeys(argument);
+            KeyHandler keyHandler = KeyHandler.getInstance();
+            keyHandler.reset(editor);
+            for (KeyStroke key : keys) {
+                keyHandler.handleKey(editor, key, context, useMapping);
+            }
+
+            // Exit if state leaves as insert or cmd_line
+            CommandState commandState = CommandState.getInstance(editor);
+            CommandState.Mode mode = commandState.getMode();
+            if (mode == CommandState.Mode.EX_ENTRY) {
+                VimPlugin.getProcess().cancelExEntry(editor, context);
+            }
+            if (mode == CommandState.Mode.INSERT || mode == CommandState.Mode.REPLACE) {
+                VimPlugin.getChange().processEscape(editor, context);
+            }
         }
         return true;
     }
