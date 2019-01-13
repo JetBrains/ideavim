960d91c0f76daeb7ecb7e3ba630852b3d64e5445
Alex Plate <aleksei.plate@jetbrains.com>
2019-01-13 14:50:39 +0300

    Create tests for normal command

---
+++ test/org/jetbrains/plugins/ideavim/ex/NormalCommandTest.kt
@@ -0,0 +1,77 @@
+package org.jetbrains.plugins.ideavim.ex
+
+import org.jetbrains.plugins.ideavim.VimTestCase
+
+/**
+ * @author Alex Plate
+ *
+ * Tests for [com.maddyhome.idea.vim.ex.handler.NormalHandler]
+ */
+class NormalCommandTest : VimTestCase() {
+    fun `test simple execution`() {
+        doTest("normal x", "123<caret>456", "123<caret>56")
+    }
+
+    fun `test short command`() {
+        doTest("norm x", "123<caret>456", "123<caret>56")
+    }
+
+    fun `test multiple commands`() {
+        doTest("normal xiNewText<Esc>", "123<caret>456", "123NewTex<caret>t56")
+    }
+
+    fun `test range single stroke`() {
+        doTest(".norm x", "123<caret>456", "<caret>23456")
+    }
+
+    fun `test range multiple strokes`() {
+        doTest(
+                "1,3norm x",
+                """
+                    123456
+                    123456
+                    123456<caret>
+                    123456
+                    123456
+                """.trimIndent(),
+                """
+                    23456
+                    23456
+                    <caret>23456
+                    123456
+                    123456
+                """.trimIndent()
+        )
+    }
+
+    fun `test with mapping`() {
+        myFixture.configureByText("a.java", """<caret>123456
+            |123456
+            |123456
+        """.trimMargin())
+        typeText(commandToKeys("map G dd"))
+        typeText(commandToKeys("normal G"))
+        myFixture.checkResult("""<caret>123456
+            |123456
+        """.trimMargin())
+    }
+
+    fun `test with disabled mapping`() {
+        myFixture.configureByText("a.java", """<caret>123456
+            |123456
+            |123456
+        """.trimMargin())
+        typeText(commandToKeys("map G dd"))
+        typeText(commandToKeys("normal! G"))
+        myFixture.checkResult("""123456
+            |123456
+            |<caret>123456
+        """.trimMargin())
+    }
+
+    private fun doTest(command: String, before: String, after: String) {
+        myFixture.configureByText("a.java", before)
+        typeText(commandToKeys(command))
+        myFixture.checkResult(after)
+    }
+}
