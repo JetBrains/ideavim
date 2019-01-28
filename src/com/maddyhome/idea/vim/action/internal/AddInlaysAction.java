51685a2094f5661b3f4e7faf38fce0d053cadb9e
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2019-01-28 20:49:23 +0300

    Renamed attributes according to naming convention

--- src/com/maddyhome/idea/vim/action/internal/AddInlaysAction.java
+++ src/com/maddyhome/idea/vim/action/internal/AddInlaysAction.java
@@ -21,7 +21,7 @@ import java.awt.font.LineMetrics;
 import java.util.Random;
 
 public class AddInlaysAction extends AnAction {
-  private static Random myRandom = new Random();
+  private static Random random = new Random();
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
@@ -33,25 +33,25 @@ public class AddInlaysAction extends AnAction {
 
     Document document = editor.getDocument();
     int lineCount = document.getLineCount();
-    for (int i = myRandom.nextInt(10); i < lineCount; ) {
+    for (int i = random.nextInt(10); i < lineCount; ) {
 
       int offset = document.getLineStartOffset(i);
 
       // Mostly above
-      boolean above = myRandom.nextInt(10) > 3;
+      boolean above = random.nextInt(10) > 3;
 
       // Mostly do one, but occasionally throw in a bunch
-      int count = myRandom.nextInt(10) > 7 ? myRandom.nextInt(5) : 1;
+      int count = random.nextInt(10) > 7 ? random.nextInt(5) : 1;
       for (int j = 0; j < count; j++) {
 
-        float factor = Math.max(1.75f * myRandom.nextFloat(), 0.9f);
+        float factor = Math.max(1.75f * random.nextFloat(), 0.9f);
         String text = String.format("---------- %s line %d ----------", above ? "above" : "below", i + 1);
 
         inlayModel.addBlockElement(offset, true, above, 0, new MyBlockRenderer(factor, text));
       }
 
       // Every 10 lines +/- 3 lines
-      i += 10 + (myRandom.nextInt(6) - 3);
+      i += 10 + (random.nextInt(6) - 3);
     }
   }
 
@@ -62,19 +62,19 @@ public class AddInlaysAction extends AnAction {
   private static class MyBlockRenderer implements EditorCustomElementRenderer {
 
     private static Key<MyFontMetrics> HINT_FONT_METRICS = Key.create("DummyInlayFontMetrics");
-    private float myFactor;
-    private String myText;
+    private float factor;
+    private String text;
 
     MyBlockRenderer(float factor, String text) {
-      myFactor = factor;
-      myText = text;
+      this.factor = factor;
+      this.text = text;
     }
 
     @Override
     public int calcWidthInPixels(@NotNull Inlay inlay) {
       Editor editor = inlay.getEditor();
       FontMetrics fontMetrics = getFontMetrics(editor).metrics;
-      return doCalcWidth(myText, fontMetrics);
+      return doCalcWidth(text, fontMetrics);
     }
 
     @Override
@@ -91,18 +91,18 @@ public class AddInlaysAction extends AnAction {
                       @NotNull TextAttributes textAttributes) {
       Editor editor = inlay.getEditor();
       FontMetrics fontMetrics = getFontMetrics(editor).metrics;
-      LineMetrics lineMetrics = fontMetrics.getLineMetrics(myText, g);
+      LineMetrics lineMetrics = fontMetrics.getLineMetrics(text, g);
 
       g.setColor(JBColor.GRAY);
       g.setFont(fontMetrics.getFont());
-      g.drawString(myText, 0, targetRegion.y + (int)(lineMetrics.getHeight() - lineMetrics.getDescent()));
+      g.drawString(text, 0, targetRegion.y + (int)(lineMetrics.getHeight() - lineMetrics.getDescent()));
       g.setColor(JBColor.LIGHT_GRAY);
       g.drawRect(targetRegion.x, targetRegion.y, targetRegion.width, targetRegion.height);
     }
 
     private MyFontMetrics getFontMetrics(Editor editor) {
       String familyName = UIManager.getFont("Label.font").getFamily();
-      int size = (int)(Math.max(1, editor.getColorsScheme().getEditorFontSize() - 1) * myFactor);
+      int size = (int)(Math.max(1, editor.getColorsScheme().getEditorFontSize() - 1) * factor);
       MyFontMetrics metrics = editor.getUserData(HINT_FONT_METRICS);
       if (metrics != null && !metrics.isActual(editor, familyName, size)) {
         metrics = null;
