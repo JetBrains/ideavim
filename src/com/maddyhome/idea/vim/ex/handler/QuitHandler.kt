08168a7877e28f82bce93063d6ba385091bc05cd
Alex Plate <aleksei.plate@jetbrains.com>
2019-02-15 17:02:57 +0300

    Convert QuitHandler to kotlin

---
+++ src/com/maddyhome/idea/vim/ex/handler/QuitHandler.kt
@@ -0,0 +1,41 @@
+/*
+ * IdeaVim - Vim emulator for IDEs based on the IntelliJ platform
+ * Copyright (C) 2003-2016 The IdeaVim authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.maddyhome.idea.vim.ex.handler
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Editor
+import com.maddyhome.idea.vim.VimPlugin
+import com.maddyhome.idea.vim.ex.CommandHandler
+import com.maddyhome.idea.vim.ex.ExCommand
+import com.maddyhome.idea.vim.ex.commands
+import com.maddyhome.idea.vim.ex.flags
+
+class QuitHandler : CommandHandler(
+        commands {
+            +"q" withOptional "uit"
+            +"clo" withOptional "se"
+            +"hid" withOptional "e"
+        },
+        flags(CommandHandler.ARGUMENT_OPTIONAL, CommandHandler.DONT_REOPEN)
+) {
+    override fun execute(editor: Editor, context: DataContext, cmd: ExCommand): Boolean {
+        VimPlugin.getFile().closeFile(editor, context)
+        return true
+    }
+}
