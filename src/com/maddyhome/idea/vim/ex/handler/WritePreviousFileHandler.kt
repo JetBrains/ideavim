a96a1730a1f103f311d31d4e42eaccefc2f5334b
Alex Plate <aleksei.plate@jetbrains.com>
2019-02-15 19:25:35 +0300

    Convert WritePreviousHandler to kotlin

---
+++ src/com/maddyhome/idea/vim/ex/handler/WritePreviousFileHandler.kt
@@ -0,0 +1,45 @@
+/*
+ * IdeaVim - Vim emulator for IDEs based on the IntelliJ platform
+ * Copyright (C) 2003-2016 The IdeaVim authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.maddyhome.idea.vim.ex.handler
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Editor
+import com.maddyhome.idea.vim.VimPlugin
+import com.maddyhome.idea.vim.ex.CommandHandler
+import com.maddyhome.idea.vim.ex.ExCommand
+import com.maddyhome.idea.vim.ex.commands
+import com.maddyhome.idea.vim.ex.flags
+
+class WritePreviousFileHandler : CommandHandler(
+        commands {
+            +"wN" withOptional "ext"
+            +"wp" withOptional "revious"
+        },
+        flags(RANGE_OPTIONAL, ARGUMENT_OPTIONAL, RANGE_IS_COUNT)
+) {
+    override fun execute(editor: Editor, context: DataContext, cmd: ExCommand): Boolean {
+        val count = cmd.getCount(editor, context, 1, true)
+
+        VimPlugin.getFile().saveFile(editor)
+        VimPlugin.getMark().saveJumpLocation(editor)
+        VimPlugin.getFile().selectNextFile(-count, context)
+
+        return true
+    }
+}
