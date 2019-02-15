30241e0837d982ac084b931a6c9b831d12ed822f
Alex Plate <aleksei.plate@jetbrains.com>
2019-02-15 18:58:14 +0300

    Convert SplitHandler to kotlin

---
+++ src/com/maddyhome/idea/vim/ex/handler/SplitHandler.kt
@@ -0,0 +1,45 @@
+/*
+ * IdeaVim - Vim emulator for IDEs based on the IntelliJ platform
+ * Copyright (C) 2003-2016 The IdeaVim authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.maddyhome.idea.vim.ex.handler
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Editor
+import com.maddyhome.idea.vim.VimPlugin
+import com.maddyhome.idea.vim.ex.CommandHandler
+import com.maddyhome.idea.vim.ex.ExCommand
+import com.maddyhome.idea.vim.ex.commands
+import com.maddyhome.idea.vim.ex.flags
+
+class SplitHandler : CommandHandler(
+        commands {
+            +"vs" withOptional "plit"
+            +"sp" withOptional "lit"
+        },
+        flags(RANGE_FORBIDDEN, ARGUMENT_OPTIONAL, DONT_REOPEN)
+) {
+    override fun execute(editor: Editor, context: DataContext, cmd: ExCommand): Boolean {
+        if (cmd.command.startsWith("v")) {
+            VimPlugin.getWindow().splitWindowVertical(context, cmd.argument)
+        } else {
+            VimPlugin.getWindow().splitWindowHorizontal(context, cmd.argument)
+        }
+
+        return true
+    }
+}
