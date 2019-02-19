5e99c45fa1aeac82a1d3afad2d20d72d972011c9
Alex Plate <aleksei.plate@jetbrains.com>
2019-02-19 19:58:18 +0300

    Convert FindFileHandler to kotlin

---
+++ src/com/maddyhome/idea/vim/ex/handler/FindFileHandler.kt
@@ -0,0 +1,50 @@
+/*
+ * IdeaVim - Vim emulator for IDEs based on the IntelliJ platform
+ * Copyright (C) 2003-2016 The IdeaVim authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.maddyhome.idea.vim.ex.handler
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.editor.Editor
+import com.maddyhome.idea.vim.KeyHandler
+import com.maddyhome.idea.vim.VimPlugin
+import com.maddyhome.idea.vim.ex.CommandHandler
+import com.maddyhome.idea.vim.ex.ExCommand
+import com.maddyhome.idea.vim.ex.commands
+import com.maddyhome.idea.vim.ex.flags
+
+class FindFileHandler : CommandHandler(
+        commands("fin[d]"),
+        flags(RANGE_FORBIDDEN, ARGUMENT_OPTIONAL, DONT_REOPEN)
+) {
+    override fun execute(editor: Editor, context: DataContext, cmd: ExCommand): Boolean {
+        val arg = cmd.argument
+        if (arg.isNotEmpty()) {
+            val res = VimPlugin.getFile().openFile(arg, context)
+            if (res) {
+                VimPlugin.getMark().saveJumpLocation(editor)
+            }
+
+            return res
+        }
+
+        ApplicationManager.getApplication().invokeLater { KeyHandler.executeAction("GotoFile", context) }
+
+        return true
+    }
+}
