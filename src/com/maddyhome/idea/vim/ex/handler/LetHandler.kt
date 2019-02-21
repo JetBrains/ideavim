e02821571804ea97df6801f34a3b51922a84d9d3
Alex Plate <aleksei.plate@jetbrains.com>
2019-02-21 14:22:44 +0300

    Convert LetHandler to kotlin

---
+++ src/com/maddyhome/idea/vim/ex/handler/LetHandler.kt
@@ -0,0 +1,72 @@
+/*
+ * IdeaVim - Vim emulator for IDEs based on the IntelliJ platform
+ * Copyright (C) 2003-2019 The IdeaVim authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.maddyhome.idea.vim.ex.handler
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Editor
+import com.maddyhome.idea.vim.ex.*
+import com.maddyhome.idea.vim.ex.vimscript.VimScriptCommandHandler
+import com.maddyhome.idea.vim.ex.vimscript.VimScriptGlobalEnvironment
+import com.maddyhome.idea.vim.ex.vimscript.VimScriptParser
+import java.util.regex.Pattern
+
+/**
+ * @author vlan
+ */
+class LetHandler : CommandHandler(
+        commands("let"),
+        flags(RANGE_FORBIDDEN, ARGUMENT_OPTIONAL)
+), VimScriptCommandHandler {
+
+    @Throws(ExException::class)
+    override fun execute(editor: Editor, context: DataContext, cmd: ExCommand): Boolean {
+        execute(cmd)
+        return true
+    }
+
+    @Throws(ExException::class)
+    override fun execute(cmd: ExCommand) {
+        val argument = cmd.argument
+        if (argument.trim().isEmpty()) {
+            showVariables()
+        } else {
+            val matcher = SIMPLE_ASSIGNMENT.matcher(argument)
+            if (matcher.matches()) {
+                val name = matcher.group(1)
+                // TODO: Check that 'name' is global
+                val expression = matcher.group(2)
+                val env = VimScriptGlobalEnvironment.getInstance()
+                val globals = env.variables
+                val value = VimScriptParser.evaluate(expression, globals)
+                globals[name] = value
+            } else {
+                throw ExException("Only simple '=' assignments are supported in 'let' expressions")
+            }
+        }
+    }
+
+    @Throws(ExException::class)
+    private fun showVariables() {
+        throw ExException("'let' without arguments is not supported yet")
+    }
+
+    companion object {
+        private val SIMPLE_ASSIGNMENT = Pattern.compile("([A-Za-z_][A-Za-z_0-9]*)[ \\t]*=[ \\t]*(.*)")
+    }
+}
