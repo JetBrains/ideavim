a1236d855bd86b8dd81e67d0de1acf43d7cd73a6
Alex Plate <aleksei.plate@jetbrains.com>
2019-02-22 16:26:37 +0300

    Convert RegistersHandler to kotlin

---
+++ src/com/maddyhome/idea/vim/ex/handler/RegistersHandler.kt
@@ -0,0 +1,41 @@
+/*
+ * IdeaVim - Vim emulator for IDEs based on the IntelliJ platform
+ * Copyright (C) 2003-2019 The IdeaVim authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.maddyhome.idea.vim.ex.handler
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Editor
+import com.maddyhome.idea.vim.VimPlugin
+import com.maddyhome.idea.vim.ex.*
+import com.maddyhome.idea.vim.ex.CommandHandler.ARGUMENT_OPTIONAL
+import com.maddyhome.idea.vim.helper.StringHelper
+
+class RegistersHandler : CommandHandler(
+        commands("di[splay]", "reg[isters]"),
+        ARGUMENT_OPTIONAL
+) {
+  override fun execute(editor: Editor, context: DataContext, cmd: ExCommand): Boolean {
+    val regs = VimPlugin.getRegister().registers.joinToString("\n", prefix = "--- Registers ---\n") { reg ->
+      """"${reg.name}   ${StringHelper.toKeyNotation(reg.keys)}"""
+    }
+
+    ExOutputModel.getInstance(editor).output(regs)
+
+    return true
+  }
+}
