ae1d3d9c09cfde14d1784b352bd0faea9573f562
Alex Plate <aleksei.plate@jetbrains.com>
2019-02-25 12:49:13 +0300

    Convert RepeatHandler to kotlin

---
+++ src/com/maddyhome/idea/vim/ex/handler/RepeatHandler.kt
@@ -0,0 +1,60 @@
+/*
+ * IdeaVim - Vim emulator for IDEs based on the IntelliJ platform
+ * Copyright (C) 2003-2019 The IdeaVim authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.maddyhome.idea.vim.ex.handler
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.editor.Editor
+import com.maddyhome.idea.vim.VimPlugin
+import com.maddyhome.idea.vim.ex.CommandHandler
+import com.maddyhome.idea.vim.ex.CommandParser
+import com.maddyhome.idea.vim.ex.ExCommand
+import com.maddyhome.idea.vim.ex.ExException
+import com.maddyhome.idea.vim.ex.commands
+import com.maddyhome.idea.vim.ex.flags
+import com.maddyhome.idea.vim.group.MotionGroup
+import com.maddyhome.idea.vim.handler.CaretOrder
+
+class RepeatHandler : CommandHandler(commands("@"),
+        flags(RANGE_OPTIONAL, ARGUMENT_REQUIRED, DONT_SAVE_LAST),
+        true, CaretOrder.DECREASING_OFFSET
+) {
+
+  private var lastArg = ':'
+
+  @Throws(ExException::class)
+  override fun execute(editor: Editor, caret: Caret, context: DataContext, cmd: ExCommand): Boolean {
+    var arg = cmd.argument[0]
+    if (arg == '@') arg = lastArg
+    lastArg = arg
+
+    val line = cmd.getLine(editor, caret, context)
+    MotionGroup.moveCaret(editor, caret, VimPlugin.getMotion().moveCaretToLine(editor, line))
+
+    if (arg == ':') {
+      return CommandParser.getInstance().processLastCommand(editor, context, 1)
+    }
+
+    val reg = VimPlugin.getRegister().getPlaybackRegister(arg) ?: return false
+    val text = reg.text ?: return false
+
+    CommandParser.getInstance().processCommand(editor, context, text, 1)
+    return true
+  }
+}
