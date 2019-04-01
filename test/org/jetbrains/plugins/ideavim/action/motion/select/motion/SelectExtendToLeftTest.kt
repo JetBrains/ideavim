a9df13e4d7626fee1de9d74b3d42ce3282c42532
Alex Plate <aleksei.plate@jetbrains.com>
2019-04-01 16:09:54 +0300

    Tests for left-right select motions

---
+++ test/org/jetbrains/plugins/ideavim/action/motion/select/motion/SelectExtendToLeftTest.kt
@@ -0,0 +1,268 @@
+package org.jetbrains.plugins.ideavim.action.motion.select.motion
+
+import com.maddyhome.idea.vim.helper.StringHelper.parseKeys
+import org.jetbrains.plugins.ideavim.VimTestCase
+
+class SelectExtendToLeftTest : VimTestCase() {
+    fun `test simple motion char mode`() {
+        doTest(parseKeys("gh", "<S-Left>"),
+                """
+                A Discovery
+
+                I f${c}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                I f$s$c${se}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test double motion char mode`() {
+        doTest(parseKeys("gh", "<S-Left>".repeat(2)),
+                """
+                A Discovery
+
+                I f${c}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                I $s${c}f${se}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test at line start char mode`() {
+        doTest(parseKeys("gh", "<S-Left>".repeat(2)),
+                """
+                A Discovery
+
+                ${c}I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                $s$c${se}I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test at file start char mode`() {
+        doTest(parseKeys("gh", "<S-Left>".repeat(2)),
+                """
+                ${c}A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                $s$c${se}A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test simple motion line mode`() {
+        doTest(parseKeys("gH", "<S-Left>"),
+                """
+                A Discovery
+
+                I f${c}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                ${s}I ${c}found it in a legendary land$se
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test to line start line mode`() {
+        doTest(parseKeys("gH", "<S-Left>".repeat(5)),
+                """
+                A Discovery
+
+                I f${c}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                $s${c}I found it in a legendary land$se
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test to file start line mode`() {
+        doTest(parseKeys("gH", "<S-Left>".repeat(5)),
+                """
+                A ${c}Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                $s${c}A Discovery$se
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test simple motion block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Left>"),
+                """
+                A Discovery
+
+                I f${c}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                I f$s$c${se}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test twice motion block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Left>".repeat(2)),
+                """
+                A Discovery
+
+                I f${c}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                I $s${c}f${se}ound it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test at line start block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Left>".repeat(2)),
+                """
+                A Discovery
+
+                ${c}I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                $s$c${se}I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test at file start block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Left>".repeat(2)),
+                """
+                ${c}A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                $s$c${se}A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test multiline with empty line block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Down>", "<S-Left>".repeat(2)),
+                """
+                A ${c}Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                ${s}A ${se}Discovery
+                $c
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+        assertCaretsColour()
+    }
+
+    fun `test multiline block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Down>".repeat(2), "<S-Left>".repeat(3)),
+                """
+                A Discovery
+
+                I foun${c}d it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                I fo${s}u${c}n${se}d it in a legendary land
+                all ${s}r${c}o${se}cks and lavender and tufted grass,
+                wher$s${c}e ${se}it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+        assertCaretsColour()
+    }
+}
---
+++ test/org/jetbrains/plugins/ideavim/action/motion/select/motion/SelectExtendToRightTest.kt
@@ -0,0 +1,259 @@
+package org.jetbrains.plugins.ideavim.action.motion.select.motion
+
+import com.maddyhome.idea.vim.helper.StringHelper.parseKeys
+import org.jetbrains.plugins.ideavim.VimTestCase
+
+class SelectExtendToRightTest : VimTestCase() {
+    fun `test simple motion char mode`() {
+        doTest(parseKeys("gh", "<S-Right>"),
+                """
+                A Discovery
+
+                ${c}I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                ${s}I $c${se}found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test at the lineend char mode`() {
+        doTest(parseKeys("gh", "<S-Right>"),
+                """
+                A Discovery
+
+                I found it in a legendary la${c}nd
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                I found it in a legendary la${s}nd$c$se
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test out of line char mode`() {
+        doTest(parseKeys("gh", "<S-Right>".repeat(2)),
+                """
+                A Discovery
+
+                I found it in a legendary lan${c}d
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                I found it in a legendary lan${s}d$c$se
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test file end char mode`() {
+        doTest(parseKeys("gh", "<S-Right>".repeat(2)),
+                """
+                A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass$c.""".trimIndent(),
+                """
+                A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass$s.$c$se""".trimIndent())
+    }
+
+    fun `test simple motion line mode`() {
+        doTest(parseKeys("gH", "<S-Right>"),
+                """
+                A Discovery
+
+                ${c}I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                ${s}I$c found it in a legendary land$se
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test lineend line mode`() {
+        doTest(parseKeys("gH", "<S-Right>"),
+                """
+                A Discovery
+
+                I found it in a legendary lan${c}d
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                ${s}I found it in a legendary land$c$se
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test out of line line mode`() {
+        doTest(parseKeys("gH", "<S-Right>".repeat(2)),
+                """
+                A Discovery
+
+                I found it in a legendary lan${c}d
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                ${s}I found it in a legendary land$c$se
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test fileend line mode`() {
+        doTest(parseKeys("gH", "<S-Right>"),
+                """
+                A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass$c.""".trimIndent(),
+                """
+                A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                ${s}hard by the torrent of a mountain pass.$c$se""".trimIndent())
+    }
+
+    fun `test simple motion block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Right>"),
+                """
+                A Discovery
+
+                ${c}I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                ${s}I $c${se}found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test at the lineend block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Right>"),
+                """
+                A Discovery
+
+                I found it in a legendary la${c}nd
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                I found it in a legendary la${s}nd$c$se
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test out of line block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Right>".repeat(2)),
+                """
+                A Discovery
+
+                I found it in a legendary lan${c}d
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent(),
+                """
+                A Discovery
+
+                I found it in a legendary lan${s}d$c$se
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.
+                    """.trimIndent())
+    }
+
+    fun `test file end block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Right>".repeat(2)),
+                """
+                A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass$c.""".trimIndent(),
+                """
+                A Discovery
+
+                I found it in a legendary land
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass$s.$c$se""".trimIndent())
+    }
+
+    fun `test to longer line block mode`() {
+        doTest(parseKeys("g<C-H>", "<S-Down>", "<S-Right>".repeat(3)),
+                """
+                A Discovery
+
+                I found it in a legendary lan${c}d
+                all rocks and lavender and tufted grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.""".trimIndent(),
+                """
+                A Discovery
+
+                I found it in a legendary lan${s}d$se
+                all rocks and lavender and tu${s}fted$c$se grass,
+                where it was settled on some sodden sand
+                hard by the torrent of a mountain pass.""".trimIndent())
+        assertCaretsColour()
+    }
+}
