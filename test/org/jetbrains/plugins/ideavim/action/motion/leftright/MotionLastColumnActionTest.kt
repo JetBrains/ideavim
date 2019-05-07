712e3df4b89c7a8f45a6f2a37ebcf28eaa92081f
Alex Plate <aleksei.plate@jetbrains.com>
2019-05-07 17:12:15 +0300

    Add muted tests to dollar motion in visual block mode

--- test/org/jetbrains/plugins/ideavim/action/motion/leftright/MotionLastColumnActionTest.kt
+++ test/org/jetbrains/plugins/ideavim/action/motion/leftright/MotionLastColumnActionTest.kt
@@ -16,14 +16,17 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+@file:Suppress("RemoveCurlyBracesFromTemplate")
+
 package org.jetbrains.plugins.ideavim.action.motion.leftright
 
 import com.maddyhome.idea.vim.command.CommandState
 import com.maddyhome.idea.vim.helper.StringHelper.parseKeys
+import com.maddyhome.idea.vim.helper.VimBehaviourDiffers
 import org.jetbrains.plugins.ideavim.VimTestCase
 
 class MotionLastColumnActionTest : VimTestCase() {
-    fun `test motion down in visual block mode`() {
+    fun `test dollar motion`() {
         val keys = parseKeys("$")
         val before = """
             A Discovery
@@ -44,7 +47,7 @@ class MotionLastColumnActionTest : VimTestCase() {
         doTest(keys, before, after, CommandState.Mode.COMMAND, CommandState.SubMode.NONE)
     }
 
-    fun `test motion down in visual block mode with motion to longer line`() {
+    fun `test dollar motion with motion to longer line`() {
         val keys = parseKeys("\$j")
         val before = """
             A Discovery
@@ -63,5 +66,63 @@ class MotionLastColumnActionTest : VimTestCase() {
             hard by the torrent of a mountain pass.
         """.trimIndent()
         doTest(keys, before, after, CommandState.Mode.COMMAND, CommandState.SubMode.NONE)
+    }
+
+    @VimBehaviourDiffers(originalVimAfter = """
+            A Discovery
+
+            I ${s}found it in a legendary lan${c}d${se}
+            al${s}l rocks and lavender and tufted grass,[ additional symbols${c}]${se}
+            wh${s}ere it was settled on some sodden sand${c}${se}
+            hard by the torrent of a mountain pass.
+    """)
+    fun `test dollar motion in visual block mode`() {
+        val keys = parseKeys("<C-V>jj\$")
+        val before = """
+            A Discovery
+
+            I ${c}found it in a legendary land
+            all rocks and lavender and tufted grass,[ additional symbols]
+            where it was settled on some sodden sand
+            hard by the torrent of a mountain pass.
+        """.trimIndent()
+        val after = """
+            A Discovery
+
+            I ${s}found it in a legendary lan${c}d${se}
+            al${s}l rocks and lavender and tufted grass,${c}[ additional symbols]${se}
+            wh${s}ere it was settled on some sodden san${c}d${se}
+            hard by the torrent of a mountain pass.
+        """.trimIndent()
+        doTest(keys, before, after, CommandState.Mode.VISUAL, CommandState.SubMode.VISUAL_BLOCK)
+    }
+
+    @VimBehaviourDiffers(originalVimAfter = """
+            A Discovery
+
+            I ${s}found it in a legendary lan${c}d${se}
+            al${s}l rocks and lavender and tufted grass${c},${se}[ additional symbols]
+            wh${s}ere it was settled on some sodden san${c}d${se}
+            hard by the torrent of a mountain pass.
+    """)
+    fun `test dollar motion in visual block mode with left motion`() {
+        val keys = parseKeys("<C-V>jj\$h")
+        val before = """
+            A Discovery
+
+            I ${c}found it in a legendary land
+            all rocks and lavender and tufted grass,[ additional symbols]
+            where it was settled on some sodden sand
+            hard by the torrent of a mountain pass.
+        """.trimIndent()
+        val after = """
+            A Discovery
+
+            I ${s}found it in a legendary lan${c}d${se}
+            al${s}l rocks and lavender and tufted grass${c},[ additional symbols]${se}
+            wh${s}ere it was settled on some sodden sa${c}nd${se}
+            hard by the torrent of a mountain pass.
+        """.trimIndent()
+        doTest(keys, before, after, CommandState.Mode.VISUAL, CommandState.SubMode.VISUAL_BLOCK)
     }
 }
