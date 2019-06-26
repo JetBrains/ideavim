b3449729317ad6e6b0b85a7a45a2b23dd1069a3c
Alex Plate <aleksei.plate@jetbrains.com>
2019-06-26 13:24:33 +0300

    WIP: Renaming file 'MotionScrollPageUpAction' with Kotlin extension

--- src/com/maddyhome/idea/vim/action/motion/scroll/MotionScrollPageUpAction.kt
+++ src/com/maddyhome/idea/vim/action/motion/scroll/MotionScrollPageUpAction.kt
@@ -22,20 +22,48 @@ import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorAction;
 import com.maddyhome.idea.vim.VimPlugin;
+import com.maddyhome.idea.vim.action.VimCommandAction;
 import com.maddyhome.idea.vim.command.Command;
+import com.maddyhome.idea.vim.command.MappingMode;
 import com.maddyhome.idea.vim.handler.VimActionHandler;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import java.util.List;
+import java.util.Set;
 
-public class MotionScrollPageUpAction extends EditorAction {
-  public MotionScrollPageUpAction() {
-    super(new Handler());
+
+public class MotionScrollPageUpAction extends VimCommandAction {
+  @NotNull
+  @Override
+  public Set<MappingMode> getMappingModes() {
+    return MappingMode.NVO;
   }
 
-  private static class Handler extends VimActionHandler.SingleExecution {
-    @Override
-    public boolean execute(@NotNull Editor editor, @NotNull DataContext context, @NotNull Command cmd) {
-      return VimPlugin.getMotion().scrollFullPage(editor, -cmd.getCount());
-    }
+  @NotNull
+  @Override
+  public Set<List<KeyStroke>> getKeyStrokesSet() {
+    return parseKeysSet("<C-B>", "<PageUp>");
+  }
+
+  @NotNull
+  @Override
+  public Command.Type getType() {
+    return Command.Type.OTHER_READONLY;
+  }
+
+  @NotNull
+  @Override
+  protected VimActionHandler makeActionHandler() {
+    return new VimActionHandler.SingleExecution() {
+      @Override
+      public boolean execute(@NotNull Editor editor, @NotNull DataContext context, @NotNull Command cmd) {
+        return VimPlugin.getMotion().scrollFullPage(editor, -cmd.getCount());
+      }
+    };
   }
 }
+    parser.registerAction(MappingMode.NVO, "VimMotionScrollPageUp", Command.Type.OTHER_READONLY, new Shortcut[]{
+      new Shortcut(KeyStroke.getKeyStroke(KeyEvent.VK_B, KeyEvent.CTRL_MASK)),
+      new Shortcut(KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_UP, 0))
+      });
