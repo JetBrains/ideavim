354262ba9badb0617bd379b9a814f7fd01daf3de
Alex Plate <aleksei.plate@jetbrains.com>
2019-07-15 19:50:52 +0300

    Update ideajoin notification

--- src/com/maddyhome/idea/vim/group/NotificationService.kt
+++ src/com/maddyhome/idea/vim/group/NotificationService.kt
@@ -1,17 +1,28 @@
 package com.maddyhome.idea.vim.group
 
+import com.intellij.icons.AllIcons
+import com.intellij.ide.actions.OpenFileAction
+import com.intellij.ide.actions.ShowFilePathAction
+import com.intellij.ide.browsers.BrowserLauncher
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationListener
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.KeyboardShortcut
 import com.intellij.openapi.keymap.Keymap
 import com.intellij.openapi.keymap.KeymapUtil
 import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.text.TextWithMnemonic
 import com.maddyhome.idea.vim.VimPlugin
+import com.maddyhome.idea.vim.ex.vimscript.VimScriptParser
 import com.maddyhome.idea.vim.key.ShortcutOwner
+import com.maddyhome.idea.vim.option.OptionsManager
 import com.maddyhome.idea.vim.ui.VimEmulationConfigurable
+import java.io.File
 import javax.swing.KeyStroke
 import javax.swing.event.HyperlinkEvent
 
@@ -24,9 +35,75 @@ class NotificationService(private val project: Project?) {
   constructor() : this(null)
 
   fun notifyAboutIdeaJoin() {
-    Notification(IDEAVIM_STICKY_NOTIFICATION_ID, IDEAVIM_NOTIFICATION_TITLE,
-      """Put "<code>set ideajoin</code>" into your <code>.ideavimrc</code> to perform a join via the IDE""",
-      NotificationType.INFORMATION).notify(project)
+    val notification = Notification(IDEAVIM_NOTIFICATION_ID, IDEAVIM_NOTIFICATION_TITLE,
+      """Put <b><code>set ideajoin</code></b> into your <code>~/.ideavimrc</code> to perform a join via the IDE""",
+      NotificationType.INFORMATION)
+
+    notification.addAction(object : AnAction() {
+      init {
+        this.templatePresentation.setTextWithMnemonic(TextWithMnemonic.parse("_Open ~/.ideavimrc"))
+      }
+
+      override fun actionPerformed(e: AnActionEvent) {
+        val eventProject = e.project
+        if (eventProject != null) {
+          val ideaVimRc = VimScriptParser.findOrCreateIdeaVimRc()
+          if (ideaVimRc != null) {
+            OpenFileAction.openFile(ideaVimRc.path, eventProject)
+            // Do not expire a notification. The user should see what they are entering
+            return
+          }
+        }
+        notification.expire()
+        createIdeaVimRcManually("Cannot create configuration file.<br/>Please create <code>~/.ideavimrc</code> manually")
+      }
+    })
+
+    notification.addAction(object : AnAction() {
+      init {
+        this.templatePresentation.setTextWithMnemonic(TextWithMnemonic.parse("_Append to ~/.ideavimrc"))
+      }
+
+      override fun actionPerformed(e: AnActionEvent) {
+        val eventProject = e.project
+        OptionsManager.ideajoin.set()
+        if (eventProject != null) {
+          val ideaVimRc = VimScriptParser.findOrCreateIdeaVimRc()
+          if (ideaVimRc != null && ideaVimRc.canWrite()) {
+            ideaVimRc.appendText("set ideajoin")
+            notification.expire()
+            Notification(IDEAVIM_NOTIFICATION_ID, IDEAVIM_NOTIFICATION_TITLE, "Success.<br/><code>ideajoin</code> is enabled", NotificationType.INFORMATION).notify(project)
+            return
+          }
+        }
+        notification.expire()
+        createIdeaVimRcManually("Option is enabled, but the file is not modified<br/>Please modify <code>~/.ideavimrc</code> manually")
+      }
+    })
+
+    notification.addAction(object : AnAction("", "", AllIcons.General.TodoQuestion) {
+      override fun actionPerformed(e: AnActionEvent) {
+        BrowserLauncher.instance.open(ideajoinExamplesUrl)
+        notification.expire()
+      }
+    })
+    notification.notify(project)
+  }
+
+  private fun createIdeaVimRcManually(message: String) {
+    val notification = Notification(IDEAVIM_NOTIFICATION_ID, IDEAVIM_NOTIFICATION_TITLE, message, NotificationType.WARNING)
+    var actionName = if (SystemInfo.isMac) "Reveal Home in Finder" else "Show Home in " + ShowFilePathAction.getFileManagerName()
+    if (!File(System.getProperty("user.home")).exists()) {
+      actionName = ""
+    }
+    notification.addAction(object : AnAction(actionName) {
+      override fun actionPerformed(e: AnActionEvent) {
+        val homeDir = File(System.getProperty("user.home"))
+        ShowFilePathAction.openDirectory(homeDir)
+        notification.expire()
+      }
+    })
+    notification.notify(project)
   }
 
   fun enableRepeatingMode() = Messages.showYesNoDialog("Do you want to enable repeating keys in Mac OS X on press and hold?\n\n" +
@@ -35,15 +112,15 @@ class NotificationService(private val project: Project?) {
     Messages.getQuestionIcon())
 
   fun specialKeymap(keymap: Keymap, listener: NotificationListener.Adapter) {
-    Notification(IDEAVIM_STICKY_NOTIFICATION_ID, IDEAVIM_NOTIFICATION_TITLE, String.format(
+    Notification(IDEAVIM_STICKY_NOTIFICATION_ID, IDEAVIM_NOTIFICATION_TITLE,
       "IdeaVim plugin doesn't use the special \"Vim\" keymap any longer. " +
-        "Switching to \"%s\" keymap.<br/><br/>" +
+        "Switching to \"${keymap.presentableName}\" keymap.<br/><br/>" +
         "Now it is possible to set up:<br/>" +
         "<ul>" +
         "<li>Vim keys in your ~/.ideavimrc file using key mapping commands</li>" +
         "<li>IDE action shortcuts in \"File | Settings | Keymap\"</li>" +
         "<li>Vim or IDE handlers for conflicting shortcuts in <a href='#settings'>Vim Emulation</a> settings</li>" +
-        "</ul>", keymap.presentableName), NotificationType.INFORMATION, listener).notify(project)
+        "</ul>", NotificationType.INFORMATION, listener).notify(project)
   }
 
   fun noVimrcAsDefault() = Notification(IDEAVIM_STICKY_NOTIFICATION_ID, IDEAVIM_NOTIFICATION_TITLE,
@@ -79,5 +156,6 @@ class NotificationService(private val project: Project?) {
     const val IDEAVIM_STICKY_NOTIFICATION_ID = "ideavim-sticky"
     const val IDEAVIM_NOTIFICATION_ID = "ideavim"
     const val IDEAVIM_NOTIFICATION_TITLE = "IdeaVim"
+    const val ideajoinExamplesUrl = "https://github.com/JetBrains/ideavim/blob/master/doc/ideajoin-examples.md"
   }
 }
