3a014b2a13fa69bbaf1ea4971e12c73ebe1c96ce
Alex Plate <aleksei.plate@jetbrains.com>
2019-10-29 14:39:05 +0300

    Convert VimPluginToggleAction to kotlin

--- src/com/maddyhome/idea/vim/action/VimPluginToggleAction.kt
+++ src/com/maddyhome/idea/vim/action/VimPluginToggleAction.kt
@@ -16,53 +16,29 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.maddyhome.idea.vim.action;
+package com.maddyhome.idea.vim.action
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.DumbAwareToggleAction;
-import com.maddyhome.idea.vim.VimActions;
-import com.maddyhome.idea.vim.VimPlugin;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareToggleAction
+import com.maddyhome.idea.vim.VimActions
+import com.maddyhome.idea.vim.VimPlugin
 
 /**
  * This class is used to handle the Vim Plugin enabled/disabled toggle. This is most likely used as a menu option
  * but could also be used as a toolbar item.
  */
-public class VimPluginToggleAction extends DumbAwareToggleAction {
-  /**
-   * Indicates if the toggle is on or off
-   *
-   * @param event The event that triggered the action
-   * @return true if the toggle is on, false if off
-   */
-  @Override
-  public boolean isSelected(@NotNull AnActionEvent event) {
-    return VimPlugin.isEnabled();
+class VimPluginToggleAction : DumbAwareToggleAction() {
+  override fun isSelected(event: AnActionEvent): Boolean = VimPlugin.isEnabled()
+
+  override fun setSelected(event: AnActionEvent, b: Boolean) {
+    VimPlugin.setEnabled(b)
   }
 
-  /**
-   * Specifies whether the toggle should be on or off
-   *
-   * @param event The event that triggered the action
-   * @param b     The new state - true is on, false is off
-   */
-  @Override
-  public void setSelected(@NotNull AnActionEvent event, boolean b) {
-    VimPlugin.setEnabled(b);
-  }
+  override fun update(e: AnActionEvent) {
+    super.update(e)
 
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    super.update(e);
-    if (VimActions.actionPlace.equals(e.getPlace())) {
-      if (VimPlugin.isEnabled()) {
-        e.getPresentation().setText("Enabled");
-      } else {
-        e.getPresentation().setText("Enable");
-      }
-    }
-    else {
-      e.getPresentation().setText("Vim Emulator");
-    }
+    e.presentation.text = if (VimActions.actionPlace == e.place) {
+      if (VimPlugin.isEnabled()) "Enabled" else "Enable"
+    } else "Vim Emulator"
   }
 }
