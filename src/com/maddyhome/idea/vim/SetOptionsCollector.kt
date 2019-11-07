eaf9f0bbfa73fa7a5988f2d8044dc28134ffde86
Alex Plate <aleksei.plate@jetbrains.com>
2019-11-07 18:24:34 +0300

    Fix compatibility with older IDEA versions

--- src/com/maddyhome/idea/vim/SetOptionsCollector.kt
+++ src/com/maddyhome/idea/vim/SetOptionsCollector.kt
@@ -1,7 +1,7 @@
 package com.maddyhome.idea.vim
 
-import com.intellij.internal.statistic.beans.MetricEvent
-import com.intellij.internal.statistic.beans.newMetric
+import com.intellij.internal.statistic.beans.UsageDescriptor
+import com.intellij.internal.statistic.eventLog.FeatureUsageData
 import com.intellij.internal.statistic.service.fus.collectors.ApplicationUsagesCollector
 import com.maddyhome.idea.vim.option.ClipboardOptionsData
 import com.maddyhome.idea.vim.option.OptionsManager
@@ -12,6 +12,27 @@ class VimSetOptionsCollector : ApplicationUsagesCollector() {
 
   override fun getVersion() = 1
 
+  // TODO: 07.11.2019 [VERSION UPDATE] 192+ should be removed
+  @Suppress("UnstableApiUsage", "DEPRECATION")
+  override fun getUsages(): MutableSet<UsageDescriptor> = mutableSetOf<UsageDescriptor>().apply {
+    add(booleanDescriptor("enabled.ideaput", OptionsManager.clipboard.contains(ClipboardOptionsData.ideaput)))
+    add(booleanDescriptor("enabled.ideamarks", OptionsManager.ideamarks.isSet))
+    add(UsageDescriptor("selected.idearefactormode", FeatureUsageData().addData("value", OptionsManager.idearefactormode.value)))
+    add(booleanDescriptor("enabled.ideaselection", OptionsManager.selectmode.contains(SelectModeOptionData.ideaselection)))
+    add(booleanDescriptor("enabled.ideajoin", OptionsManager.ideajoin.isSet))
+  }
+
+  @Suppress("UnstableApiUsage")
+  private fun booleanDescriptor(key: String, value: Boolean): UsageDescriptor {
+    @Suppress("UnstableApiUsage")
+    return UsageDescriptor(key, FeatureUsageData().addData("enabled", value))
+  }
+}
+
+/*
+  Updated version for 192+
+
+
   override fun getMetrics() = mutableSetOf<MetricEvent>().apply {
     add(newMetric("enabled.ideaput", OptionsManager.clipboard.contains(ClipboardOptionsData.ideaput)))
     add(newMetric("enabled.ideamarks", OptionsManager.ideamarks.isSet))
@@ -19,4 +40,4 @@ class VimSetOptionsCollector : ApplicationUsagesCollector() {
     add(newMetric("enabled.ideaselection", OptionsManager.selectmode.contains(SelectModeOptionData.ideaselection)))
     add(newMetric("enabled.ideajoin", OptionsManager.ideajoin.isSet))
   }
-}
+ */
