6434a06c088010e4775b836f0137301d608198d0
Alex Plate <aleksei.plate@jetbrains.com>
2019-11-28 18:23:44 +0300

    Convert commentary extension test to kt

--- test/org/jetbrains/plugins/ideavim/extension/commentary/CommentaryExtensionTest.kt
+++ test/org/jetbrains/plugins/ideavim/extension/commentary/CommentaryExtensionTest.kt
@@ -1,160 +1,146 @@
-package org.jetbrains.plugins.ideavim.extension.commentary;
+package org.jetbrains.plugins.ideavim.extension.commentary
 
-import com.intellij.ide.highlighter.HtmlFileType;
-import org.jetbrains.plugins.ideavim.JavaVimTestCase;
-
-import static com.maddyhome.idea.vim.command.CommandState.Mode.COMMAND;
-import static com.maddyhome.idea.vim.helper.StringHelper.parseKeys;
+import com.intellij.ide.highlighter.HtmlFileType
+import com.maddyhome.idea.vim.command.CommandState
+import com.maddyhome.idea.vim.helper.StringHelper
+import org.jetbrains.plugins.ideavim.JavaVimTestCase
 
 /**
  * @author dhleong
  */
-public class CommentaryExtensionTest extends JavaVimTestCase {
-
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp();
-    enableExtensions("commentary");
+class CommentaryExtensionTest : JavaVimTestCase() {
+  override fun setUp() {
+    super.setUp()
+    enableExtensions("commentary")
   }
 
   // |gc| |l|
-  public void testBlockCommentSingle() {
-    doTest(parseKeys("gcll"),
-           "<caret>if (condition) {\n" + "}\n",
-           "/<caret>*i*/f (condition) {\n" + "}\n");
-    assertMode(COMMAND);
-    assertSelection(null);
-  }
-
-
-  // |gc| |iw|
-  public void testBlockCommentInnerWord() {
-    doTest(parseKeys("gciw"),
-           "<caret>if (condition) {\n" + "}\n",
-           "<caret>/*if*/ (condition) {\n" + "}\n");
-    assertMode(COMMAND);
-    assertSelection(null);
+  fun testBlockCommentSingle() {
+    doTest(StringHelper.parseKeys("gcll"),
+      "<caret>if (condition) {\n" + "}\n",
+      "/<caret>*i*/f (condition) {\n" + "}\n")
+    assertMode(CommandState.Mode.COMMAND)
+    assertSelection(null)
   }
 
   // |gc| |iw|
-  public void testBlockCommentTillForward() {
-    doTest(parseKeys("gct{"),
-           "<caret>if (condition) {\n" + "}\n",
-           "<caret>/*if (condition) */{\n" + "}\n");
+  fun testBlockCommentInnerWord() {
+    doTest(StringHelper.parseKeys("gciw"),
+      "<caret>if (condition) {\n" + "}\n",
+      "<caret>/*if*/ (condition) {\n" + "}\n")
+    assertMode(CommandState.Mode.COMMAND)
+    assertSelection(null)
+  }
+
+  // |gc| |iw|
+  fun testBlockCommentTillForward() {
+    doTest(StringHelper.parseKeys("gct{"),
+      "<caret>if (condition) {\n" + "}\n",
+      "<caret>/*if (condition) */{\n" + "}\n")
   }
 
   // |gc| |ab|
-  public void testBlockCommentOuterParens() {
-    doTest(parseKeys("gcab"),
-           "if (<caret>condition) {\n" + "}\n",
-           "if <caret>/*(condition)*/ {\n" + "}\n");
+  fun testBlockCommentOuterParens() {
+    doTest(StringHelper.parseKeys("gcab"),
+      "if (<caret>condition) {\n" + "}\n",
+      "if <caret>/*(condition)*/ {\n" + "}\n")
   }
 
   /*
    * NB: linewise motions become linewise comments;
    *  otherwise, they are incredibly difficult to undo
    */
-
-  // |gc| |j|
-  public void testLineCommentDown() {
-    doTest(parseKeys("gcj"),
-           "<caret>if (condition) {\n" + "}\n",
-           "//if (condition) {\n" +
-           "//}\n");
+// |gc| |j|
+  fun testLineCommentDown() {
+    doTest(StringHelper.parseKeys("gcj"),
+      "<caret>if (condition) {\n" + "}\n",
+      "//if (condition) {\n" +
+        "//}\n")
   }
 
   // |gc| |ip|
-  public void testLineCommentInnerParagraph() {
-    doTest(parseKeys("gcip"),
-           "<caret>if (condition) {\n" + "}\n",
-           "//if (condition) {\n" +
-           "//}\n");
+  fun testLineCommentInnerParagraph() {
+    doTest(StringHelper.parseKeys("gcip"),
+      "<caret>if (condition) {\n" + "}\n",
+      "//if (condition) {\n" +
+        "//}\n")
   }
 
   // |gc| |ip|
-  public void testLineCommentSingleLineInnerParagraph() {
-    doTest(parseKeys("gcip"),
-           "<caret>if (condition) {}",
-           "//if (condition) {}");
+  fun testLineCommentSingleLineInnerParagraph() {
+    doTest(StringHelper.parseKeys("gcip"),
+      "<caret>if (condition) {}",
+      "//if (condition) {}")
   }
 
-  /* Ensure uncommenting works as well */
-
-  // |gc| |ip|
-  public void testLineUncommentInnerParagraph() {
-    doTest(parseKeys("gcip"),
-           "<caret>//if (condition) {\n" + "//}\n",
-           "if (condition) {\n" +
-           "}\n");
-    assertMode(COMMAND);
-    assertSelection(null);
-  }
-
-  // |gc| |ip|
-  public void testLineUncommentSingleLineInnerParagraph() {
-    doTest(parseKeys("gcip"),
-           "<caret>//if (condition) {}",
-           "if (condition) {}");
-  }
-
-  /* Visual mode */
-
-  // |gc| |ip|
-  public void testLineCommentVisualInnerParagraph() {
-    doTest(parseKeys("vipgc"),
-           "<caret>if (condition) {\n" + "}\n",
-           "//if (condition) {\n" +
-           "//}\n");
+  /* Ensure uncommenting works as well */ // |gc| |ip|
+  fun testLineUncommentInnerParagraph() {
+    doTest(StringHelper.parseKeys("gcip"),
+      "<caret>//if (condition) {\n" + "//}\n",
+      "if (condition) {\n" +
+        "}\n")
+    assertMode(CommandState.Mode.COMMAND)
+    assertSelection(null)
   }
 
   // |gc| |ip|
-  public void testLineUncommentVisualInnerParagraph() {
-    doTest(parseKeys("vipgc"),
-           "<caret>//if (condition) {\n" + "//}\n",
-           "if (condition) {\n" +
-           "}\n");
+  fun testLineUncommentSingleLineInnerParagraph() {
+    doTest(StringHelper.parseKeys("gcip"),
+      "<caret>//if (condition) {}",
+      "if (condition) {}")
   }
 
+  /* Visual mode */ // |gc| |ip|
+  fun testLineCommentVisualInnerParagraph() {
+    doTest(StringHelper.parseKeys("vipgc"),
+      "<caret>if (condition) {\n" + "}\n",
+      "//if (condition) {\n" +
+        "//}\n")
+  }
 
+  // |gc| |ip|
+  fun testLineUncommentVisualInnerParagraph() {
+    doTest(StringHelper.parseKeys("vipgc"),
+      "<caret>//if (condition) {\n" + "//}\n",
+      "if (condition) {\n" +
+        "}\n")
+  }
 
-
-  /* Special shortcut gcc is always linewise */
-
-  // |gcc|
-  public void testLineCommentShortcut() {
-    doTest(parseKeys("gccj"),
-           "<caret>if (condition) {\n" + "}\n",
-           "//if (condition) {\n" +
-           "<caret>}\n");
-    assertMode(COMMAND);
-    assertSelection(null);
+  /* Special shortcut gcc is always linewise */ // |gcc|
+  fun testLineCommentShortcut() {
+    doTest(StringHelper.parseKeys("gccj"),
+      "<caret>if (condition) {\n" + "}\n",
+      "//if (condition) {\n" +
+        "<caret>}\n")
+    assertMode(CommandState.Mode.COMMAND)
+    assertSelection(null)
   }
 
   // |gcc|
-  public void testLineCommentShortcutPreservesCaret() {
-    doTest(parseKeys("gcc"),
-           "if (<caret>condition) {\n" + "}\n",
-           "//if (<caret>condition) {\n" + "}\n");
-    assertMode(COMMAND);
-    assertSelection(null);
+  fun testLineCommentShortcutPreservesCaret() {
+    doTest(StringHelper.parseKeys("gcc"),
+      "if (<caret>condition) {\n" + "}\n",
+      "//if (<caret>condition) {\n" + "}\n")
+    assertMode(CommandState.Mode.COMMAND)
+    assertSelection(null)
   }
 
   // |gcc|
-  public void testLineUncommentShortcut() {
-    doTest(parseKeys("gcc"),
-           "<caret>//if (condition) {\n" + "}\n",
-           "<caret>if (condition) {\n" +
-           "}\n");
-    assertMode(COMMAND);
-    assertSelection(null);
+  fun testLineUncommentShortcut() {
+    doTest(StringHelper.parseKeys("gcc"),
+      "<caret>//if (condition) {\n" + "}\n",
+      "<caret>if (condition) {\n" +
+        "}\n")
+    assertMode(CommandState.Mode.COMMAND)
+    assertSelection(null)
   }
 
   // |gcc|
-  public void testHTMLCommentShortcut() {
-    myFixture.configureByText(HtmlFileType.INSTANCE, "<div />");
-    typeText(parseKeys("gcc"));
-    myFixture.checkResult("<!--<div />-->");
-    assertMode(COMMAND);
-    assertSelection(null);
+  fun testHTMLCommentShortcut() {
+    myFixture.configureByText(HtmlFileType.INSTANCE, "<div />")
+    typeText(StringHelper.parseKeys("gcc"))
+    myFixture.checkResult("<!--<div />-->")
+    assertMode(CommandState.Mode.COMMAND)
+    assertSelection(null)
   }
 }
