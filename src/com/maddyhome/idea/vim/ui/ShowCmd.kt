0aed324f4005a11ef70cd4df55825b41491b9cb8
Alex Plate <aleksei.plate@jetbrains.com>
2020-04-27 10:49:30 +0300

    Update showcmd widget

--- src/com/maddyhome/idea/vim/ui/ShowCmd.kt
+++ src/com/maddyhome/idea/vim/ui/ShowCmd.kt
@@ -1,14 +1,16 @@
 package com.maddyhome.idea.vim.ui
 
+import com.intellij.ide.lightEdit.LightEditCompatible
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
-import com.intellij.openapi.wm.StatusBarWidgetProvider
+import com.intellij.openapi.wm.StatusBarWidgetFactory
 import com.intellij.openapi.wm.WindowManager
 import com.intellij.openapi.wm.impl.status.EditorBasedWidget
+import com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetsManager
 import com.intellij.util.Consumer
 import com.maddyhome.idea.vim.helper.StringHelper
 import com.maddyhome.idea.vim.helper.vimCommandState
@@ -20,12 +22,14 @@ import java.awt.event.MouseEvent
 object ShowCmd {
   // https://github.com/vim/vim/blob/b376ace1aeaa7614debc725487d75c8f756dd773/src/vim.h#L1721
   private const val SHOWCMD_COLS = 10
+  internal const val ID = "IdeaVim::ShowCmd"
+  internal const val displayName = "IdeaVim showcmd"
 
   fun update() {
     val windowManager = WindowManager.getInstance()
     ProjectManager.getInstance().openProjects.forEach {
       val statusBar = windowManager.getStatusBar(it)
-      statusBar.updateWidget(ShowCmdStatusBarWidget.ID)
+      statusBar.updateWidget(ID)
     }
   }
 
@@ -46,54 +50,67 @@ object ShowCmd {
 object ShowCmdOptionChangeListener: OptionChangeListener<Boolean> {
   override fun valueChange(oldValue: Boolean?, newValue: Boolean?) {
     ShowCmd.update()
+
+    val extension = StatusBarWidgetFactory.EP_NAME.findExtension(ShowCmdStatusBarWidgetFactory::class.java) ?: return
+    val projectManager = ProjectManager.getInstanceIfCreated() ?: return
+    for (project in projectManager.openProjects) {
+      val statusBarWidgetsManager = project.getService(StatusBarWidgetsManager::class.java) ?: continue
+      statusBarWidgetsManager.updateWidget(extension)
+    }
   }
 }
 
-class ShowCmdStatusBarWidget: StatusBarWidgetProvider {
-  companion object {
-    const val ID = "IdeaVim::ShowCmd"
+class ShowCmdStatusBarWidgetFactory : StatusBarWidgetFactory, LightEditCompatible {
+  override fun getId() = ShowCmd.ID
+
+  override fun getDisplayName(): String = ShowCmd.displayName
+
+  override fun disposeWidget(widget: StatusBarWidget) {}
+
+  override fun isAvailable(project: Project): Boolean = OptionsManager.showcmd.isSet
+
+  override fun createWidget(project: Project): StatusBarWidget = Widget(project)
+
+  override fun canBeEnabledOn(statusBar: StatusBar): Boolean = true
+
+  // Should be configured via `set showcmd`
+  override fun isConfigurable(): Boolean = false
+}
+
+// `:help 'showcmd'`
+// Widget shows:
+// * Partial command, as it's being typed
+// * When selecting characters within a line, the number of characters
+//   * Tabs are shown as one char
+//   * If the number of bytes is different, this is also shown: "2-6"
+// * When selecting more than one line, the number of lines
+// * When selecting a block, the size in screen characters: {lines}x{columns}
+//
+// We only need to show partial commands, since the standard PositionPanel shows the other information already, with
+// the exception of "{lines}x{columns}" (it shows "x carets" instead)
+class Widget(project: Project) : EditorBasedWidget(project), StatusBarWidget.Multiframe, StatusBarWidget.TextPresentation {
+
+  override fun ID() = ShowCmd.ID
+
+  override fun getPresentation(): StatusBarWidget.WidgetPresentation? = this
+
+  override fun getClickConsumer(): Consumer<MouseEvent>? = null
+
+  override fun getTooltipText(): String {
+    var count = ShowCmd.getFullText(this.editor)
+    if (!count.isBlank()) count = ": $count"
+    return "${ShowCmd.displayName}$count"
   }
 
-  override fun getWidget(project: Project): StatusBarWidget = Widget(project)
-  override fun getAnchor(): String = StatusBar.Anchors.before(StatusBar.StandardWidgets.POSITION_PANEL)
+  override fun getText(): String = ShowCmd.getWidgetText(editor)
 
-  // `:help 'showcmd'`
-  // Widget shows:
-  // * Partial command, as it's being typed
-  // * When selecting characters within a line, the number of characters
-  //   * Tabs are shown as one char
-  //   * If the number of bytes is different, this is also shown: "2-6"
-  // * When selecting more than one line, the number of lines
-  // * When selecting a block, the size in screen characters: {lines}x{columns}
-  //
-  // We only need to show partial commands, since the standard PositionPanel shows the other information already, with
-  // the exception of "{lines}x{columns}" (it shows "x carets" instead)
-  class Widget(project: Project)
-    : EditorBasedWidget(project), StatusBarWidget.Multiframe, StatusBarWidget.TextPresentation {
+  override fun getAlignment() = Component.CENTER_ALIGNMENT
 
-    override fun ID() = ID
+  // Multiframe#copy to show the widget on popped out editors
+  override fun copy(): StatusBarWidget = Widget(myProject)
 
-    // [VERSION UPDATE] After 193 use `getPresentation()`
-    @Suppress("UnstableApiUsage", "DEPRECATION")
-    override fun getPresentation(type: StatusBarWidget.PlatformType): StatusBarWidget.WidgetPresentation? = this
-
-    override fun getClickConsumer(): Consumer<MouseEvent>? = null
-
-    override fun getTooltipText(): String {
-      var count = ShowCmd.getFullText(this.editor)
-      if (!count.isBlank()) count = ": $count"
-      return "IdeaVim showcmd$count"
-    }
-
-    override fun getText(): String = ShowCmd.getWidgetText(editor)
-    override fun getAlignment() = Component.CENTER_ALIGNMENT
-
-    // Multiframe#copy to show the widget on popped out editors
-    override fun copy(): StatusBarWidget = Widget(myProject)
-
-    override fun selectionChanged(event: FileEditorManagerEvent) {
-      // Update when changing selected editor
-      myStatusBar?.updateWidget(ID)
-    }
+  override fun selectionChanged(event: FileEditorManagerEvent) {
+    // Update when changing selected editor
+    myStatusBar?.updateWidget(ShowCmd.ID)
   }
 }
