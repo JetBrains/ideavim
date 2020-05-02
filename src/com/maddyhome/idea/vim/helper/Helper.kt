fb04e835efb23d87664e5501fb2d3a86afb9c5bb
Alex Plate <aleksei.plate@jetbrains.com>
2020-05-02 13:13:51 +0300

    Update vimBehaviourDiffers annotation description

--- src/com/maddyhome/idea/vim/helper/Helper.kt
+++ src/com/maddyhome/idea/vim/helper/Helper.kt
@@ -27,11 +27,11 @@ import java.util.*
 
 /**
  * This annotation is created for test functions (methods).
- * It means that original vim behavior has small differences from behavior of IdeaVim.
+ * It means that the original vim behavior has small differences from behavior of IdeaVim.
  * [shouldBeFixed] flag indicates whether the given functionality should be fixed
  *   or the given behavior is normal for IdeaVim and should be leaved as is.
  *
- * E.g. after execution some commands original vim has next text:
+ * E.g. after execution of some commands original vim has the following text:
  *    Hello1
  *    Hello2
  *    Hello3
@@ -42,9 +42,11 @@ import java.util.*
  *    Hello2
  *    Hello3
  *
- * Why this annotation exists?
+ * In this case you should still create the test function and mark this function with [VimBehaviorDiffers] annotation.
+ *
+ * Why does this annotation exist?
  * After creating some functionality you can understand that IdeaVim has a bit different behavior, but you
- *   cannot fix it right now because of any reasons (bugs in IDE,
+ *   cannot fix it right now because of any reason (bugs in IDE,
  *   the impossibility of this functionality in IDEA (*[shouldBeFixed] == false*), leak of time for fixing).
  *   In that case, you should NOT remove the corresponding test or leave it without any marks that this test
  *   not fully convenient with vim, but leave the test with IdeaVim's behavior and put this annotation
