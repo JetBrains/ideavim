6c0511a8981c5e70b7b996924df16a800dee5019
Alex Plate <aleksei.plate@jetbrains.com>
2020-05-06 03:24:16 +0300

    Update IdeaVim icons class

--- src/icons/VimIcons.java
+++ src/icons/VimIcons.java
@@ -1,13 +1,18 @@
 package icons;
 
-import com.intellij.openapi.util.IconLoader;
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-public interface VimIcons {
-  Icon IDEAVIM = IconLoader.getIcon("/icons/ideavim.svg");
-  Icon IDEAVIM_DISABLED = IconLoader.getIcon("/icons/ideavim_disabled.svg");
-  Icon GITHUB = IconLoader.getIcon("/icons/github.svg");
-  Icon TWITTER = IconLoader.getIcon("/icons/twitter.svg");
-  Icon YOUTRACK = IconLoader.getIcon("/icons/youtrack.svg");
+public final class VimIcons {
+  private static @NotNull Icon load(@NotNull String path) {
+    return IconManager.getInstance().getIcon(path, VimIcons.class);
+  }
+
+  public static final @NotNull Icon IDEAVIM = load("/icons/ideavim.svg");
+  public static final @NotNull Icon IDEAVIM_DISABLED = load("/icons/ideavim_disabled.svg");
+  public static final @NotNull Icon GITHUB = load("/icons/github.svg");
+  public static final @NotNull Icon TWITTER = load("/icons/twitter.svg");
+  public static final @NotNull Icon YOUTRACK = load("/icons/youtrack.svg");
 }
