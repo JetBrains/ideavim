a42a19a1742767f29ecb24f4f0c40b4d50b9e51f
Alex Plate <aleksei.plate@jetbrains.com>
2020-07-13 11:06:05 +0300

    Add converting mode to vim notation

--- src/com/maddyhome/idea/vim/command/CommandState.kt
+++ src/com/maddyhome/idea/vim/command/CommandState.kt
@@ -192,6 +192,75 @@ class CommandState private constructor() {
 
   fun toSimpleString(): String = modeStates.joinToString { it.toSimpleString() }
 
+  /**
+   * It's a bit more complicated
+   *
+   *  Neovim
+   * :h mode()
+   *
+   * - mode([expr])          Return a string that indicates the current mode.
+   * 
+   *   If [expr] is supplied and it evaluates to a non-zero Number or
+   *   a non-empty String (|non-zero-arg|), then the full mode is
+   *   returned, otherwise only the first letter is returned.
+   *  
+   *   n          Normal
+   *   no         Operator-pending
+   *   nov        Operator-pending (forced characterwise |o_v|)
+   *   noV        Operator-pending (forced linewise |o_V|)
+   *   noCTRL-V   Operator-pending (forced blockwise |o_CTRL-V|)
+   *   niI        Normal using |i_CTRL-O| in |Insert-mode|
+   *   niR        Normal using |i_CTRL-O| in |Replace-mode|
+   *   niV        Normal using |i_CTRL-O| in |Virtual-Replace-mode|
+   *   v          Visual by character
+   *   V          Visual by line
+   *   CTRL-V     Visual blockwise
+   *   s          Select by character
+   *   S          Select by line
+   *   CTRL-S     Select blockwise
+   *   i          Insert
+   *   ic         Insert mode completion |compl-generic|
+   *   ix         Insert mode |i_CTRL-X| completion
+   *   R          Replace |R|
+   *   Rc         Replace mode completion |compl-generic|
+   *   Rv         Virtual Replace |gR|
+   *   Rx         Replace mode |i_CTRL-X| completion
+   *   c          Command-line editing
+   *   cv         Vim Ex mode |gQ|
+   *   ce         Normal Ex mode |Q|
+   *   r          Hit-enter prompt
+   *   rm         The -- more -- prompt
+   *   r?         |:confirm| query of some sort
+   *   !          Shell or external command is executing
+   *   t          Terminal mode: keys go to the job
+   *   This is useful in the 'statusline' option or when used
+   *   with |remote_expr()| In most other places it always returns
+   *   "c" or "n".
+   *   Note that in the future more modes and more specific modes may
+   *   be added. It's better not to compare the whole string but only
+   *   the leading character(s).
+   */
+  fun toVimNotation(): String {
+    return when (mode) {
+      Mode.COMMAND -> "n"
+      Mode.VISUAL -> when (subMode) {
+        SubMode.VISUAL_CHARACTER -> "v"
+        SubMode.VISUAL_LINE -> "V"
+        SubMode.VISUAL_BLOCK -> "\u0016"
+        else -> error("Unexpected state")
+      }
+      Mode.INSERT -> "i"
+      Mode.SELECT -> when (subMode) {
+        SubMode.VISUAL_CHARACTER -> "s"
+        SubMode.VISUAL_LINE -> "S"
+        SubMode.VISUAL_BLOCK -> "\u0013"
+        else -> error("Unexpected state")
+      }
+      Mode.REPLACE -> "R"
+      else -> error("Unexpected state")
+    }
+  }
+
   private fun currentModeState(): ModeState {
     return if (modeStates.size > 0) modeStates.peek() else defaultModeState
   }
