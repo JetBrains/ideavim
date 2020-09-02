632e9fad2e67ad70bd789d32f560be828c562a68
Matt Ellis <m.t.ellis@gmail.com>
2020-09-02 17:18:03 +0100

    Remove incorrect mappings for page up/down
    i_<C-Down> and i_<C-Up> are not standard Vim mappings, but can be set up in .ideavimrc if required

--- src/com/maddyhome/idea/vim/action/motion/scroll/MotionScrollPageDownAction.kt
+++ src/com/maddyhome/idea/vim/action/motion/scroll/MotionScrollPageDownAction.kt
@@ -44,8 +44,6 @@ class MotionScrollPageDownInsertModeAction : VimActionHandler.SingleExecution(),
 
   override val keyStrokesSet: Set<List<KeyStroke>> = setOf(
     listOf(KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_DOWN, 0)),
-    listOf(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, KeyEvent.CTRL_DOWN_MASK)),
-    listOf(KeyStroke.getKeyStroke(KeyEvent.VK_KP_DOWN, KeyEvent.CTRL_DOWN_MASK)),
     listOf(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, KeyEvent.SHIFT_DOWN_MASK)),
     listOf(KeyStroke.getKeyStroke(KeyEvent.VK_KP_DOWN, KeyEvent.SHIFT_DOWN_MASK))
   )
