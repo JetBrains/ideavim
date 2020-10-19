5598de70f1f0d58763ff88fe5be159f040c1bf8e
Alex Plate <aleksei.plate@jetbrains.com>
2020-10-19 23:59:27 +0300

    Trying to extract common code for active tests building

--- .teamcity/_Self/buildTypes/TestsForIntelliJ20202.kt
+++ .teamcity/_Self/buildTypes/TestsForIntelliJ20202.kt
@@ -1,41 +1,50 @@
 package _Self.buildTypes
 
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-object TestsForIntelliJ20202 : BuildType({
+sealed class ActiveTests : BuildType({
+  params {
+    param("env.ORG_GRADLE_PROJECT_downloadIdeaSources", "false")
+    param("env.ORG_GRADLE_PROJECT_instrumentPluginCode", "false")
+  }
+
+  vcs {
+    root(DslContext.settingsRoot)
+
+    checkoutMode = CheckoutMode.ON_SERVER
+  }
+
+  steps {
+    gradle {
+      tasks = "clean test"
+      buildFile = ""
+      enableStacktrace = true
+      param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+    }
+  }
+
+  triggers {
+    vcs {
+      branchFilter = ""
+    }
+  }
+
+  requirements {
+    noLessThanVer("teamcity.agent.jvm.version", "1.8")
+  }
+})
+
+object TestsForIntelliJ20202 : ActiveTests() {
+  init {
     name = "Tests for IntelliJ 2020.2"
     description = "branch 202"
 
     params {
-        param("env.ORG_GRADLE_PROJECT_downloadIdeaSources", "false")
-        param("env.ORG_GRADLE_PROJECT_ideaVersion", "2020.2")
-        param("env.ORG_GRADLE_PROJECT_instrumentPluginCode", "false")
+      param("env.ORG_GRADLE_PROJECT_ideaVersion", "2020.2")
     }
-
-    vcs {
-        root(DslContext.settingsRoot)
-
-        checkoutMode = CheckoutMode.ON_SERVER
-    }
-
-    steps {
-        gradle {
-            tasks = "clean test"
-            buildFile = ""
-            enableStacktrace = true
-            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
-        }
-    }
-
-    triggers {
-        vcs {
-            branchFilter = ""
-        }
-    }
-
-    requirements {
-        noLessThanVer("teamcity.agent.jvm.version", "1.8")
-    }
-})
+  }
+}
