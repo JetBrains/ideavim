800390921ccc78e2431a9fb8033bb01f0bf9419c
Alex Plate <aleksei.plate@jetbrains.com>
2020-10-20 00:59:33 +0300

    Update branch specification for PRs

--- .teamcity/_Self/vcsRoots/GitHubPullRequest.kt
+++ .teamcity/_Self/vcsRoots/GitHubPullRequest.kt
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 object GitHubPullRequest : GitVcsRoot({
     name = "IdeaVim Pull Requests"
     url = "git@github.com:JetBrains/ideavim.git"
-    branchSpec = "+:refs/(pull/*)"
+    branchSpec = "+:refs/(pull/*)/head"
     authMethod = uploadedKey {
       uploadedKey = "Alex Plate TeamCity key"
     }
