d29244efcc2276cb549fe83f79e16c059e0a7b7d
Alex Plate <aleksei.plate@jetbrains.com>
2021-01-13 20:22:58 +0300

    [TC] Add qodana build type

---
+++ .teamcity/_Self/buildTypes/Qodana.kt
@@ -0,0 +1,31 @@
+package _Self.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+
+object Qodana : BuildType({
+  name = "Qodana checks"
+  params {
+    param("env.ORG_GRADLE_PROJECT_downloadIdeaSources", "false")
+    param("env.ORG_GRADLE_PROJECT_ideaVersion", "LATEST-EAP-SNAPSHOT")
+    param("env.ORG_GRADLE_PROJECT_instrumentPluginCode", "false")
+  }
+
+  vcs {
+    root(DslContext.settingsRoot)
+
+    checkoutMode = CheckoutMode.ON_SERVER
+  }
+
+  triggers {
+    vcs {
+      branchFilter = ""
+    }
+  }
+
+  requirements {
+    noLessThanVer("teamcity.agent.jvm.version", "1.8")
+  }
+})
