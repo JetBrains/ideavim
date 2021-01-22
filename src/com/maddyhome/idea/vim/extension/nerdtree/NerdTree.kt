5a969d573ac9cbd2a1ca409649f85e648dbc075c
Alex Plate <aleksei.plate@jetbrains.com>
2021-01-22 12:41:10 +0300

    Add list of commands

--- src/com/maddyhome/idea/vim/extension/nerdtree/NerdTree.kt
+++ src/com/maddyhome/idea/vim/extension/nerdtree/NerdTree.kt
@@ -49,6 +49,55 @@ import java.awt.event.KeyEvent
 import javax.swing.KeyStroke
 import javax.swing.SwingConstants
 
+
+/**
+ * Key      Description                                                  help-tag~
+ *
+ * o........Open files, directories and bookmarks......................|NERDTree-o|
+ * go.......Open selected file, but leave cursor in the NERDTree......|NERDTree-go|
+ * Open selected bookmark dir in current NERDTree
+ * t........Open selected node/bookmark in a new tab...................|NERDTree-t|
+ * T........Same as 't' but keep the focus on the current tab..........|NERDTree-T|
+ * i........Open selected file in a split window.......................|NERDTree-i|
+ * gi.......Same as i, but leave the cursor on the NERDTree...........|NERDTree-gi|
+ * s........Open selected file in a new vsplit.........................|NERDTree-s|
+ * gs.......Same as s, but leave the cursor on the NERDTree...........|NERDTree-gs|
+ * <CR>.....User-definable custom open action.......................|NERDTree-<CR>|
+ * O........Recursively open the selected directory....................|NERDTree-O|
+ * x........Close the current nodes parent.............................|NERDTree-x|
+ * X........Recursively close all children of the current node.........|NERDTree-X|
+ * e........Edit the current dir.......................................|NERDTree-e|
+ *
+ * double-click....same as |NERDTree-o|.
+ * middle-click....same as |NERDTree-i| for files, and |NERDTree-e| for dirs.
+ *
+ * D........Delete the current bookmark ...............................|NERDTree-D|
+ *
+ * P........Jump to the root node......................................|NERDTree-P|
+ * p........Jump to current nodes parent...............................|NERDTree-p|
+ * K........Jump up inside directories at the current tree depth.......|NERDTree-K|
+ * J........Jump down inside directories at the current tree depth.....|NERDTree-J|
+ * <C-J>....Jump down to next sibling of the current directory.......|NERDTree-C-J|
+ * <C-K>....Jump up to previous sibling of the current directory.....|NERDTree-C-K|
+ *
+ * C........Change the tree root to the selected dir...................|NERDTree-C|
+ * u........Move the tree root up one directory........................|NERDTree-u|
+ * U........Same as 'u' except the old root node is left open..........|NERDTree-U|
+ * r........Recursively refresh the current directory..................|NERDTree-r|
+ * R........Recursively refresh the current root.......................|NERDTree-R|
+ * m........Display the NERDTree menu..................................|NERDTree-m|
+ * cd.......Change the CWD to the dir of the selected node............|NERDTree-cd|
+ * CD.......Change tree root to the CWD...............................|NERDTree-CD|
+ *
+ * I........Toggle whether hidden files displayed......................|NERDTree-I|
+ * f........Toggle whether the file filters are used...................|NERDTree-f|
+ * F........Toggle whether files are displayed.........................|NERDTree-F|
+ * B........Toggle whether the bookmark table is displayed.............|NERDTree-B|
+ *
+ * q........Close the NERDTree window..................................|NERDTree-q|
+ * A........Zoom (maximize/minimize) the NERDTree window...............|NERDTree-A|
+ * ?........Toggle the display of the quick help.......................|NERDTree-?|
+ */
 class NerdTree : VimExtension {
   override fun getName(): String = "NERDTree"
 
