3c2640c90d42ee9c0177b8456efae2be68bee00d
Alex Plate <aleksei.plate@jetbrains.com>
2021-02-20 12:19:47 +0300

    [TC] Update fail condition

--- .teamcity/_Self/buildTypes/Nvim.kt
+++ .teamcity/_Self/buildTypes/Nvim.kt
@@ -5,6 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
 import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.BuildFailureOnMetric
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.failOnMetricChange
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 object Nvim : BuildType({
@@ -50,5 +52,17 @@ object Nvim : BuildType({
 
   requirements {
     noLessThanVer("teamcity.agent.jvm.version", "1.8")
+  }
+
+  failureConditions {
+    failOnMetricChange {
+      metric = BuildFailureOnMetric.MetricType.TEST_COUNT
+      threshold = 20
+      units = BuildFailureOnMetric.MetricUnit.PERCENTS
+      comparison = BuildFailureOnMetric.MetricComparison.LESS
+      compareTo = build {
+        buildRule = lastSuccessful()
+      }
+    }
   }
 })
