05f2848d4190c131777a075f39000b7d6a591eb9
Alex Plate <aleksei.plate@jetbrains.com>
2021-02-26 10:02:23 +0300

    Convert gradle to kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,196 +1,237 @@
 import dev.feedforward.markdownto.DownParser
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.detekt
+import java.net.HttpURLConnection
+import java.net.URL
 
 buildscript {
-  repositories {
-    mavenCentral()
-    maven { url 'https://jitpack.io' }
-  }
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
 
-  dependencies {
-    classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
-    classpath "com.github.AlexPl292:mark-down-to-slack:1.1.2"
-  }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.71")
+        classpath("com.github.AlexPl292:mark-down-to-slack:1.1.2")
+    }
 }
 
 plugins {
-  id "org.jetbrains.intellij" version "0.7.2"
-  id "io.gitlab.arturbosch.detekt" version "1.15.0"
-  id "org.jetbrains.changelog" version "1.1.2"
-  // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-  id "org.jlleitschuh.gradle.ktlint" version "10.0.0"
+    java
+    kotlin("jvm") version "1.4.30"
+
+    id("org.jetbrains.intellij") version "0.7.2"
+    id("io.gitlab.arturbosch.detekt") version "1.15.0"
+    id("org.jetbrains.changelog") version "1.1.2"
+    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
 
-apply plugin: 'java'
-apply plugin: 'kotlin'
+// Import variables from gradle.properties file
+val javaVersion: String by project
+val kotlinVersion: String by project
+val ideaVersion: String by project
+val downloadIdeaSources: String by project
+val instrumentPluginCode: String by project
 
-sourceCompatibility = javaVersion
-targetCompatibility = javaVersion
+val publishChannels: String by project
+val publishUsername: String by project
+val publishToken: String by project
 
-tasks.withType(JavaCompile) { options.encoding = 'UTF-8' }
+val slackUrl: String by project
+
+tasks {
+    compileJava {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+
+        options.encoding = "UTF-8"
+    }
+
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = javaVersion
+//            allWarningsAsErrors = true
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = javaVersion
+//            allWarningsAsErrors = true
+        }
+    }
+}
 
 sourceSets {
-  main {
-    java.srcDir 'src'
-    resources.srcDir 'resources'
-  }
-  test {
-    java.srcDir 'test'
-  }
+    main {
+        java.srcDir("src")
+        resources.srcDir("resources")
+    }
+    test {
+        java.srcDir("test")
+    }
 }
 
 intellij {
-  version ideaVersion
-  pluginName 'IdeaVim'
-  updateSinceUntilBuild false
-  downloadSources Boolean.valueOf(downloadIdeaSources)
-  instrumentCode Boolean.valueOf(instrumentPluginCode)
-  intellijRepo = "https://www.jetbrains.com/intellij-repository"
-  plugins = ['java']
-
-  downloadRobotServerPlugin.version = "0.10.0"
-
-  publishPlugin {
-    channels publishChannels.split(',')
-    username publishUsername
-    token publishToken
-  }
+    version = ideaVersion
+    pluginName = "IdeaVim"
+    updateSinceUntilBuild = false
+    downloadSources = downloadIdeaSources.toBoolean()
+    instrumentCode = instrumentPluginCode.toBoolean()
+    intellijRepo = "https://www.jetbrains.com/intellij-repository"
+    setPlugins("java")
 }
 
-runIdeForUiTests {
-  systemProperty "robot-server.port", "8082"
-}
+tasks {
+    downloadRobotServerPlugin {
+        version = "0.10.0"
+    }
 
-runPluginVerifier {
-  ideVersions = ["IC-2020.2.3", "IC-2020.3.2"]
-  downloadDirectory = "${project.buildDir}/pluginVerifier/ides"
-  teamCityOutputFormat = true
+    publishPlugin {
+        channels(publishChannels.split(","))
+        username(publishUsername)
+        token(publishToken)
+    }
+
+    runIdeForUiTests {
+        systemProperty("robot-server.port", "8082")
+    }
+
+    runPluginVerifier {
+        ideVersions(listOf("IC-2020.2.3", "IC-2020.3.2"))
+        downloadDirectory("${project.buildDir}/pluginVerifier/ides")
+        teamCityOutputFormat = true
+    }
 }
 
 repositories {
-  mavenCentral()
-  jcenter()
-  maven { url = "https://jetbrains.bintray.com/intellij-third-party-dependencies" }
+    mavenCentral()
+    jcenter()
+    maven { url = uri("https://jetbrains.bintray.com/intellij-third-party-dependencies") }
 }
 
 dependencies {
-  compileOnly "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
-  compileOnly "org.jetbrains:annotations:20.1.0"
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    compileOnly("org.jetbrains:annotations:20.1.0")
 
-  // https://mvnrepository.com/artifact/com.ensarsarajcic.neovim.java/neovim-api
-  testImplementation("com.ensarsarajcic.neovim.java:neovim-api:0.2.3")
-  testImplementation 'com.ensarsarajcic.neovim.java:core-rpc:0.2.3'
+    // https://mvnrepository.com/artifact/com.ensarsarajcic.neovim.java/neovim-api
+    testImplementation("com.ensarsarajcic.neovim.java:neovim-api:0.2.3")
+    testImplementation("com.ensarsarajcic.neovim.java:core-rpc:0.2.3")
 
-  testImplementation("com.intellij.remoterobot:remote-robot:0.10.3")
-  testImplementation("com.intellij.remoterobot:remote-fixtures:1.1.18")
-}
-
-compileKotlin {
-  kotlinOptions {
-    jvmTarget = javaVersion
-  }
-//  kotlinOptions.allWarningsAsErrors = true
-}
-compileTestKotlin {
-  kotlinOptions {
-    jvmTarget = javaVersion
-  }
-//  kotlinOptions.allWarningsAsErrors = true
+    testImplementation("com.intellij.remoterobot:remote-robot:0.10.3")
+    testImplementation("com.intellij.remoterobot:remote-fixtures:1.1.18")
 }
 
 detekt {
-  config = files("${rootProject.projectDir}/.detekt/config.yaml")
-  baseline = file("${rootProject.projectDir}/.detekt/baseline.xml")
-  input = files("src")
+    config = files("${rootProject.projectDir}/.detekt/config.yaml")
+    baseline = file("${rootProject.projectDir}/.detekt/baseline.xml")
+    input = files("src")
 
-  buildUponDefaultConfig = true
+    buildUponDefaultConfig = true
 
-  reports {
-    html.enabled = false
-    xml.enabled = false
-    txt.enabled = false
-  }
+    reports {
+        html.enabled = false
+        xml.enabled = false
+        txt.enabled = false
+    }
 }
 
-tasks.detekt.jvmTarget = javaVersion
-
-task testWithNeovim(type: Test) {
-  group = "verification"
-  systemProperty "ideavim.nvim.test", 'true'
-  exclude '/ui/**'
+tasks {
+    withType<Detekt> {
+        this.jvmTarget = javaVersion
+    }
 }
 
-test {
-  exclude '**/propertybased/**'
-  exclude '/ui/**'
+tasks.register<Test>("testWithNeovim") {
+    group = "verification"
+    systemProperty("ideavim.nvim.test", "true")
+    exclude("/ui/**")
 }
 
-task testPropertyBased(type: Test) {
-  group = "verification"
-  include '**/propertybased/**'
+tasks.register<Test>("testPropertyBased") {
+    group = "verification"
+    include("**/propertybased/**")
 }
 
-task testUi(type: Test) {
-  group = "verification"
-  include '/ui/**'
+tasks.register<Test>("testUi") {
+    group = "verification"
+    include("/ui/**")
+}
+
+tasks {
+    test {
+        exclude("**/propertybased/**")
+        exclude("/ui/**")
+    }
 }
 
 changelog {
-  groups = ["Features:", "Changes:", "Deprecations:", "Fixes:", "Merged PRs:"]
-  itemPrefix = "*"
-  path = "${project.projectDir}/CHANGES.md"
-  unreleasedTerm = "To Be Released"
-  headerParserRegex = /0\.\d{2}(.\d+)?/
-//  header = { "${project.version}" }
-//  version = "0.60"
+    groups = listOf("Features:", "Changes:", "Deprecations:", "Fixes:", "Merged PRs:")
+    itemPrefix = "*"
+    path = "${project.projectDir}/CHANGES.md"
+    unreleasedTerm = "To Be Released"
+    headerParserRegex = "0\\.\\d{2}(.\\d+)?".toRegex()
+//    header = { "${project.version}" }
+//    version = "0.60"
 }
 
-task getUnreleasedChangelog() {
-  group = "changelog"
-  doLast {
-    def log = changelog.getUnreleased().toHTML()
-    println log
-  }
+tasks.register("getUnreleasedChangelog") {
+    group = "changelog"
+    doLast {
+        val log = changelog.getUnreleased().toHTML()
+        println(log)
+    }
 }
 
 tasks.register("slackEapNotification") {
-  doLast {
-    if (!slackUrl) return
-    def post = new URL(slackUrl).openConnection()
-    def changeLog = changelog.getUnreleased().toText()
-    def slackDown = new DownParser(changeLog, true).toSlack().toString()
-    def message = """
-      {
-          "text": "New version of IdeaVim",
-          "blocks": [
-                  {
-                    "type": "section",
-                    "text": {
-                      "type": "mrkdwn",
-                      "text": "IdeaVim EAP $version has been released\\n$slackDown"
+    doLast {
+        if (slackUrl.isBlank()) {
+            println("Slack Url is not defined")
+            return@doLast
+        }
+        val changeLog = changelog.getUnreleased().toText()
+        val slackDown = DownParser(changeLog, true).toSlack().toString()
+
+        //language=JSON
+        val message = """
+            {
+                "text": "New version of IdeaVim",
+                "blocks": [
+                    {
+                        "type": "selection",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "IdeaVim EAP $version has been released\\n$slackDown"
+                        }
                     }
-                  }
-          ]
-      }
-   """
-    post.setRequestMethod("POST")
-    post.setDoOutput(true)
-    post.setRequestProperty("Content-Type", "application/json")
-    post.getOutputStream().write(message.getBytes("UTF-8"))
-    def postRC = post.getResponseCode()
-    println(postRC)
-    if (postRC == 200) {
-      println(post.getInputStream().getText())
+                ]
+            }
+        """.trimIndent()
+
+        val post = URL(slackUrl)
+        with(post.openConnection() as HttpURLConnection) {
+            requestMethod = "POST"
+            doOutput = true
+            setRequestProperty("Content-Type", "application/json")
+
+            outputStream.write(message.toByteArray())
+
+            val postRc = responseCode
+            println("Response code: $postRc")
+            if (postRc == 200) {
+                println(inputStream.bufferedReader().use { it.readText() })
+            }
+        }
     }
-  }
 }
 
 ktlint {
-  // I don't know how to disable it for java.utils.* only
-  disabledRules = ["no-wildcard-imports"]
+    disabledRules.add("no-wildcard-imports")
 }
 
 gradle.projectsEvaluated {
-  tasks.withType(JavaCompile) {
-    options.compilerArgs << "-Werror" << "-Xlint:deprecation"
-  }
+    tasks.compileJava {
+        options.compilerArgs.add("-Werror")
+        options.compilerArgs.add("-Xlint:deprecation")
+    }
 }
