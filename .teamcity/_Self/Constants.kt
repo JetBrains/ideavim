3b3fffe838c06c9339c32add1ceb1d38e77753f9
Alex Plate <aleksei.plate@jetbrains.com>
2021-04-09 11:14:46 +0300

    [TC] update version

--- .teamcity/_Self/Constants.kt
+++ .teamcity/_Self/Constants.kt
@@ -5,6 +5,6 @@ object Constants {
   const val EAP = "eap"
   const val DEV = "Dev"
 
-  const val VERSION = "0.65"
-  const val DEV_VERSION = "0.66"
+  const val VERSION = "0.66"
+  const val DEV_VERSION = "0.67"
 }
