9b73cc16e63b4b92b562ceeb7418a8d555f88c08
Matt Ellis <m.t.ellis@gmail.com>
2021-05-13 00:12:54 +0100

    Move tab actions out of motion package

--- src/com/maddyhome/idea/vim/action/window/tabs/MotionNextTabAction.kt
+++ src/com/maddyhome/idea/vim/action/window/tabs/MotionNextTabAction.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-package com.maddyhome.idea.vim.action.motion.tabs
+package com.maddyhome.idea.vim.action.window.tabs
 
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
--- test/org/jetbrains/plugins/ideavim/RegisterActionsTest.kt
+++ test/org/jetbrains/plugins/ideavim/RegisterActionsTest.kt
@@ -79,7 +79,7 @@ class RegisterActionsTest : VimTestCase() {
     val after = "I f${c}ound it in a legendary land"
     var motionRightAction: ActionBeanClass? = null
     doTest(keys, before, after, CommandState.Mode.COMMAND, CommandState.SubMode.NONE) {
-      motionRightAction = VIM_ACTIONS_EP.extensions().findAny().get()
+      motionRightAction = VIM_ACTIONS_EP.extensions().filter { it.actionId == "VimMotionPreviousTabAction" }.findFirst().get()
 
       assertNotNull(getCommandNode())
 
