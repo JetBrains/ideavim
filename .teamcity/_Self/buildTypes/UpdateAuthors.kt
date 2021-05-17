90624c03d2f98481419d88f357cd17dd8fbe728c
Alex Plate <aleksei.plate@jetbrains.com>
2021-05-17 17:50:50 +0300

    Apply TC patch

--- .teamcity/_Self/buildTypes/UpdateAuthors.kt
+++ .teamcity/_Self/buildTypes/UpdateAuthors.kt
@@ -3,6 +3,7 @@ package _Self.buildTypes
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
 import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
@@ -12,6 +13,7 @@ object UpdateAuthors : BuildType({
     param("env.ORG_GRADLE_PROJECT_downloadIdeaSources", "false")
     param("env.ORG_GRADLE_PROJECT_ideaVersion", "LATEST-EAP-SNAPSHOT")
     param("env.ORG_GRADLE_PROJECT_instrumentPluginCode", "false")
+    password("env.ORG_GRADLE_PROJECT_updateAuthorsToken", "credentialsJSON:fc75a88f-fd02-46af-899a-a5034f402b7e", label = "Password", display = ParameterDisplay.HIDDEN)
   }
 
   vcs {
