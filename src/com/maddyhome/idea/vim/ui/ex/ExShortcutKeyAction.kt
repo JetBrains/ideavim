fe7dc4902b56ecd721f65c4ea8661d9603169692
Matt Ellis <m.t.ellis@gmail.com>
2021-06-30 16:28:28 +0100

    Make ExShortcutKeyAction dumb aware

--- src/com/maddyhome/idea/vim/ui/ex/ExShortcutKeyAction.kt
+++ src/com/maddyhome/idea/vim/ui/ex/ExShortcutKeyAction.kt
@@ -21,6 +21,7 @@ package com.maddyhome.idea.vim.ui.ex
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.KeyboardShortcut
+import com.intellij.openapi.project.DumbAwareAction
 import com.maddyhome.idea.vim.KeyHandler
 import com.maddyhome.idea.vim.helper.EditorDataContext
 import java.awt.event.KeyEvent
@@ -38,7 +39,7 @@ import javax.swing.KeyStroke
  * component has focus. It registers all shortcuts used by the Swing actions and forwards them directly to the key
  * handler.
  */
-class ExShortcutKeyAction(private val exEntryPanel: ExEntryPanel) : AnAction() {
+class ExShortcutKeyAction(private val exEntryPanel: ExEntryPanel) : DumbAwareAction() {
 
   override fun actionPerformed(e: AnActionEvent) {
     val keyStroke = getKeyStroke(e)
