dc81568c450d098f07a88afb5206200cb2da83ec
lippfi <x@lipp.fi>
2021-08-17 13:59:15 +0300

    Fixed less binary operator

--- src/com/maddyhome/idea/vim/vimscript/model/expressions/operators/handlers/binary/LessHandler.kt
+++ src/com/maddyhome/idea/vim/vimscript/model/expressions/operators/handlers/binary/LessHandler.kt
@@ -1,3 +1,3 @@
 package com.maddyhome.idea.vim.vimscript.model.expressions.operators.handlers.binary
 
-object LessHandler : BinaryOperatorWithIgnoreCaseOption(LessOrEqualsIgnoreCaseHandler, LessCaseSensitiveHandler)
+object LessHandler : BinaryOperatorWithIgnoreCaseOption(LessIgnoreCaseHandler, LessCaseSensitiveHandler)
