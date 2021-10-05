970456e6b0eebcf98bfc167ea14495c7a4bb9861
lippfi <x@lipp.fi>
2021-10-05 13:16:12 +0300

    Added `evaluateWithRange` method to funcrefCalls

--- src/com/maddyhome/idea/vim/vimscript/model/expressions/FuncrefCallExpression.kt
+++ src/com/maddyhome/idea/vim/vimscript/model/expressions/FuncrefCallExpression.kt
@@ -21,19 +21,25 @@ package com.maddyhome.idea.vim.vimscript.model.expressions
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
 import com.maddyhome.idea.vim.ex.ExException
+import com.maddyhome.idea.vim.ex.ranges.Ranges
 import com.maddyhome.idea.vim.vimscript.model.Executable
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimFuncref
 
 data class FuncrefCallExpression(val expression: Expression, val args: List<Expression>) : Expression() {
 
-  override fun evaluate(editor: Editor, context: DataContext, parent: Executable): VimDataType {
+  fun evaluateWithRange(ranges: Ranges?, editor: Editor, context: DataContext, parent: Executable): VimDataType {
     val value = expression.evaluate(editor, context, parent)
     if (value is VimFuncref) {
+      value.handler.ranges = ranges
       return value.execute(args, editor, context, parent)
     } else {
       // todo more exceptions
       throw ExException("E15: Invalid expression")
     }
+  }
+
+  override fun evaluate(editor: Editor, context: DataContext, parent: Executable): VimDataType {
+    return evaluateWithRange(null, editor, context, parent)
   }
 }
