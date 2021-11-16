8d7a2faf1bfdecabf2f1fc1f147952fe1ceede0f
Alex Plate <aleksei.plate@jetbrains.com>
2021-11-16 10:54:19 +0300

    Update copyright info

--- src/main/java/com/maddyhome/idea/vim/extension/textobjindent/VimIndentObject.java
+++ src/main/java/com/maddyhome/idea/vim/extension/textobjindent/VimIndentObject.java
@@ -1,6 +1,6 @@
 /*
  * IdeaVim - Vim emulator for IDEs based on the IntelliJ platform
- * Copyright (C) 2003-2020 The IdeaVim authors
+ * Copyright (C) 2003-2021 The IdeaVim authors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
