3394d93b43795ba367a4c5e86ce913bb05b77ce9
lippfi <x@lipp.fi>
2021-11-19 05:51:32 +0300

    Fix register

--- src/main/java/com/maddyhome/idea/vim/vimscript/model/expressions/Register.kt
+++ src/main/java/com/maddyhome/idea/vim/vimscript/model/expressions/Register.kt
@@ -22,6 +22,7 @@ import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
 import com.maddyhome.idea.vim.VimPlugin
 import com.maddyhome.idea.vim.ex.ExException
+import com.maddyhome.idea.vim.helper.StringHelper
 import com.maddyhome.idea.vim.vimscript.model.Executable
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimString
@@ -30,6 +31,7 @@ data class Register(val char: Char) : Expression() {
 
   override fun evaluate(editor: Editor, context: DataContext, parent: Executable): VimDataType {
     val register = VimPlugin.getRegister().getRegister(char) ?: throw ExException("Register is not supported yet")
-    return register.rawText?.let { VimString(it) } ?: VimString("")
+    // todo Esc like keys should be one char
+    return VimString(StringHelper.toKeyNotation(register.keys))
   }
 }
