f2736204662c31e6e69e2613e3076c9071775b6f
aleksei.plate <aleksei.plate@TeamCity>
2022-04-25 07:11:28 +0000

    TeamCity change in 'IntelliJ IDEA plugins / IdeaVim' project: runners of 'Qodana checks' build configuration were updated

--- .teamcity/patches/buildTypes/Qodana.kts
+++ .teamcity/patches/buildTypes/Qodana.kts
@@ -1,6 +1,8 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.Qodana
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.qodana
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.ScheduleTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
@@ -11,6 +13,35 @@ To apply the patch, change the buildType with id = 'Qodana'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Qodana")) {
+    expectSteps {
+        qodana {
+            name = "Qodana"
+            reportAsTests = true
+            inspectionProfile = customProfile {
+                path = ".idea/inspectionProfiles/Qodana.xml"
+            }
+            param("clonefinder-enable", "true")
+            param("clonefinder-languages", "Java")
+            param("clonefinder-languages-container", "Java Kotlin")
+            param("clonefinder-mode", "")
+            param("clonefinder-queried-project", "src")
+            param("clonefinder-reference-projects", "src")
+            param("fail-build-on-errors", "")
+            param("licenseaudit-enable", "true")
+            param("namesAndTagsCustom", "repo.labs.intellij.net/static-analyser/qodana")
+            param("report-version", "")
+            param("yaml-configuration", "")
+        }
+    }
+    steps {
+        update<Qodana>(0) {
+            clearConditions()
+            linter = jvm {
+                version = Qodana.JVMVersion.LATEST
+            }
+        }
+    }
+
     triggers {
         val trigger1 = find<ScheduleTrigger> {
             schedule {
