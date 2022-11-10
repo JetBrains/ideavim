5051bcd607df01cc8766656492d7d8d0618aff2d
aleksei.plate <aleksei.plate@TeamCity>
2022-11-10 06:55:47 +0000

    TeamCity change in 'IntelliJ IDEA plugins / IdeaVim' project: runners of 'IdeaVim compatibility with external plugins' build configuration were updated

--- .teamcity/patches/buildTypes/IdeaVimCompatibility.kts
+++ .teamcity/patches/buildTypes/IdeaVimCompatibility.kts
@@ -31,14 +31,14 @@ changeBuildType(RelativeId("IdeaVimCompatibility")) {
             clearConditions()
             scriptContent = """
                 java --version
-                java -jar verifier/verifier-cli-dev-all.jar check-plugin '${'$'}org.jetbrains.IdeaVim-EasyMotion' [latest-IU] -team-city
-                java -jar verifier/verifier-cli-dev-all.jar check-plugin '${'$'}io.github.mishkun.ideavimsneak' [latest-IU] -team-city
-                java -jar verifier/verifier-cli-dev-all.jar check-plugin '${'$'}eu.theblob42.idea.whichkey' [latest-IU] -team-city
-                java -jar verifier/verifier-cli-dev-all.jar check-plugin '${'$'}IdeaVimExtension' [latest-IU] -team-city
+                java -jar verifier/verifier-cli-dev-all.jar -runtime-dir /usr/lib/jvm/java-17-amazon-corretto check-plugin '${'$'}org.jetbrains.IdeaVim-EasyMotion' [latest-IU] -team-city
+                java -jar verifier/verifier-cli-dev-all.jar -runtime-dir /usr/lib/jvm/java-17-amazon-corretto check-plugin '${'$'}io.github.mishkun.ideavimsneak' [latest-IU] -team-city
+                java -jar verifier/verifier-cli-dev-all.jar -runtime-dir /usr/lib/jvm/java-17-amazon-corretto check-plugin '${'$'}eu.theblob42.idea.whichkey' [latest-IU] -team-city
+                java -jar verifier/verifier-cli-dev-all.jar -runtime-dir /usr/lib/jvm/java-17-amazon-corretto check-plugin '${'$'}IdeaVimExtension' [latest-IU] -team-city
                 # Outdated java -jar verifier/verifier-cli-dev-all.jar check-plugin '${'$'}github.zgqq.intellij-enhance' [latest-IU] -team-city
-                java -jar verifier/verifier-cli-dev-all.jar check-plugin '${'$'}com.github.copilot' [latest-IU] -team-city
-                java -jar verifier/verifier-cli-dev-all.jar check-plugin '${'$'}com.github.dankinsoid.multicursor' [latest-IU] -team-city
-                java -jar verifier/verifier-cli-dev-all.jar check-plugin '${'$'}com.joshestein.ideavim-quickscope' [latest-IU] -team-city
+                java -jar verifier/verifier-cli-dev-all.jar -runtime-dir /usr/lib/jvm/java-17-amazon-corretto check-plugin '${'$'}com.github.copilot' [latest-IU] -team-city
+                java -jar verifier/verifier-cli-dev-all.jar -runtime-dir /usr/lib/jvm/java-17-amazon-corretto check-plugin '${'$'}com.github.dankinsoid.multicursor' [latest-IU] -team-city
+                java -jar verifier/verifier-cli-dev-all.jar -runtime-dir /usr/lib/jvm/java-17-amazon-corretto check-plugin '${'$'}com.joshestein.ideavim-quickscope' [latest-IU] -team-city
             """.trimIndent()
         }
     }
