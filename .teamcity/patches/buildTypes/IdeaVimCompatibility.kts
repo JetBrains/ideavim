ca8731669176a620c4e0e643844bebf5f426f5af
aleksei.plate <aleksei.plate@TeamCity>
2022-11-10 08:28:29 +0000

    TeamCity change in 'IntelliJ IDEA plugins / IdeaVim' project: runners of 'IdeaVim compatibility with external plugins' build configuration were updated

--- .teamcity/patches/buildTypes/IdeaVimCompatibility.kts
+++ .teamcity/patches/buildTypes/IdeaVimCompatibility.kts
@@ -37,24 +37,6 @@ changeBuildType(RelativeId("IdeaVimCompatibility")) {
         }
         insert(1) {
             script {
-                name = "Check"
-                scriptContent = """
-                    # We use a custom build of verifier that downloads IdeaVim from dev channel
-                    
-                    java --version
-                    java -jar verifier1/verifier-cli-dev-all-1.jar check-plugin '${'$'}org.jetbrains.IdeaVim-EasyMotion' [latest-IU] -team-city
-                    java -jar verifier1/verifier-cli-dev-all-1.jar check-plugin '${'$'}io.github.mishkun.ideavimsneak' [latest-IU] -team-city
-                    java -jar verifier1/verifier-cli-dev-all-1.jar check-plugin '${'$'}eu.theblob42.idea.whichkey' [latest-IU] -team-city
-                    java -jar verifier1/verifier-cli-dev-all-1.jar check-plugin '${'$'}IdeaVimExtension' [latest-IU] -team-city
-                    # Outdated java -jar verifier/verifier-cli-dev-all.jar check-plugin '${'$'}github.zgqq.intellij-enhance' [latest-IU] -team-city
-                    java -jar verifier1/verifier-cli-dev-all-1.jar check-plugin '${'$'}com.github.copilot' [latest-IU] -team-city
-                    java -jar verifier1/verifier-cli-dev-all-1.jar check-plugin '${'$'}com.github.dankinsoid.multicursor' [latest-IU] -team-city
-                    java -jar verifier1/verifier-cli-dev-all-1.jar check-plugin '${'$'}com.joshestein.ideavim-quickscope' [latest-IU] -team-city
-                """.trimIndent()
-            }
-        }
-        insert(2) {
-            script {
                 name = "Check (1)"
                 scriptContent = """
                     # We use a custom build of verifier that downloads IdeaVim from dev channel
