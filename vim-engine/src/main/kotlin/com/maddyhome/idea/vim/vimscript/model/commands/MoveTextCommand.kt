badb61aaea0ded773b2bfa1aa64828b293e332d8
filipp <filipp.vakhitov@jetbrains.com>
2022-11-16 09:30:35 +0200

    Move command no longer removes marks

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/commands/MoveTextCommand.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/commands/MoveTextCommand.kt
@@ -11,16 +11,18 @@ package com.maddyhome.idea.vim.vimscript.model.commands
 import com.maddyhome.idea.vim.api.ExecutionContext
 import com.maddyhome.idea.vim.api.VimCaret
 import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.api.VimLogicalPosition
 import com.maddyhome.idea.vim.api.getText
 import com.maddyhome.idea.vim.api.injector
 import com.maddyhome.idea.vim.command.OperatorArguments
 import com.maddyhome.idea.vim.command.SelectionType
-import com.maddyhome.idea.vim.common.TextRange
 import com.maddyhome.idea.vim.ex.ExException
 import com.maddyhome.idea.vim.ex.InvalidRangeException
 import com.maddyhome.idea.vim.ex.ranges.LineRange
 import com.maddyhome.idea.vim.ex.ranges.Ranges
 import com.maddyhome.idea.vim.helper.Msg
+import com.maddyhome.idea.vim.mark.Mark
+import com.maddyhome.idea.vim.mark.VimMark
 import com.maddyhome.idea.vim.put.PutData
 import com.maddyhome.idea.vim.vimscript.model.ExecutionResult
 import kotlin.math.min
@@ -33,48 +35,59 @@ data class MoveTextCommand(val ranges: Ranges, val argument: String) : Command.S
 
   @Throws(ExException::class)
   override fun processCommand(editor: VimEditor, context: ExecutionContext, operatorArguments: OperatorArguments): ExecutionResult {
-    val carets = editor.sortedCarets()
     val caretCount = editor.nativeCarets().size
+    if (caretCount > 1) {
+      throw ExException("Move command supported only for one caret at the moment")
+    }
+    val caret = editor.primaryCaret()
 
-    val texts = ArrayList<String>(caretCount)
-    val ranges = ArrayList<TextRange>(caretCount)
-    var line = editor.fileSize().toInt()
     val goToLineCommand = injector.vimscriptParser.parseCommand(argument) ?: throw ExException("E16: Invalid range")
+    val lineRange = getLineRange(editor, caret)
 
-    var lastRange: TextRange? = null
-    for (caret in carets) {
-      val range = getTextRange(editor, caret, false)
-      val lineRange = getLineRange(editor, caret)
+    val line = min(editor.fileSize().toInt(), normalizeLine(editor, caret, goToLineCommand, lineRange))
+    val range = getTextRange(editor, caret, false)
+    val shift = line + 1 - editor.offsetToLogicalPosition(range.startOffset).line
 
-      line = min(line, normalizeLine(editor, caret, goToLineCommand, lineRange))
-      texts.add(editor.getText(range))
+    val text = editor.getText(range)
 
-      if (lastRange == null || lastRange.startOffset != range.startOffset && lastRange.endOffset != range.endOffset) {
-        ranges.add(range)
-        lastRange = range
-      }
-    }
+    val localMarks = injector.markService.getAllLocalMarks(caret)
+      .filter { range.contains(it.offset(editor)) }
+      .toSet()
+    val globalMarks = injector.markService.getGlobalMarks(editor)
+      .filter { range.contains(it.offset(editor)) }
+      .map { Pair(it, it.logicalLine) } // we save logical line because it will be cleared after text deletion
+      .toSet()
 
-    ranges.forEach { editor.deleteString(it) }
+    editor.deleteString(range)
 
-    for (i in 0 until caretCount) {
-      val caret = carets[i]
-      val text = texts[i]
+    val textData = PutData.TextData(text, SelectionType.LINE_WISE, emptyList())
+    val putData = PutData(
+      textData,
+      null,
+      1,
+      insertTextBeforeCaret = false,
+      rawIndent = true,
+      caretAfterInsertedText = false,
+      putToLine = line
+    )
+    injector.put.putTextForCaret(editor, caret, context, putData)
 
-      val textData = PutData.TextData(text, SelectionType.LINE_WISE, emptyList())
-      val putData = PutData(
-        textData,
-        null,
-        1,
-        insertTextBeforeCaret = false,
-        rawIndent = true,
-        caretAfterInsertedText = false,
-        putToLine = line
-      )
-      injector.put.putTextForCaret(editor, caret, context, putData)
-    }
+    globalMarks.forEach { shiftGlobalMark(editor, it, shift) }
+    localMarks.forEach { shiftLocalMark(caret, it, shift) }
 
     return ExecutionResult.Success
+  }
+
+  private fun shiftGlobalMark(editor: VimEditor, markAndLine: Pair<Mark, Int>, shift: Int) {
+    val newOffset = editor.logicalPositionToOffset(VimLogicalPosition(markAndLine.second + shift, markAndLine.first.col))
+    injector.markService.setGlobalMark(editor, markAndLine.first.key, newOffset)
+  }
+
+  private fun shiftLocalMark(caret: VimCaret, mark: Mark, shift: Int) {
+    val editor = caret.editor
+    val path = editor.getPath() ?: return
+    val mark = VimMark(mark.key, mark.logicalLine + shift, mark.col, path, editor.extractProtocol())
+    injector.markService.setMark(caret, mark)
   }
 
   @Throws
