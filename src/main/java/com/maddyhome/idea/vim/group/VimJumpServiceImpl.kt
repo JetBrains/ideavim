a8d225f8124299dcee13b568f96032fb30982309
filipp <filipp.vakhitov@jetbrains.com>
2022-12-19 19:07:38 +0200

    Save jumps after IDE close

--- src/main/java/com/maddyhome/idea/vim/group/VimJumpServiceImpl.kt
+++ src/main/java/com/maddyhome/idea/vim/group/VimJumpServiceImpl.kt
@@ -8,18 +8,58 @@
 
 package com.maddyhome.idea.vim.group
 
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.RoamingType
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
 import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory
+import com.intellij.openapi.util.text.StringUtil
 import com.maddyhome.idea.vim.api.VimEditor
-import com.maddyhome.idea.vim.api.VimJumpService
 import com.maddyhome.idea.vim.api.VimJumpServiceBase
+import com.maddyhome.idea.vim.diagnostic.vimLogger
 import com.maddyhome.idea.vim.mark.Jump
 import com.maddyhome.idea.vim.newapi.IjVimEditor
+import org.jdom.Element
 
-class VimJumpServiceImpl : VimJumpServiceBase() {
+@State(name = "VimJumpsSettings", storages = [Storage(value = "\$APP_CONFIG$/vim_settings_local.xml", roamingType = RoamingType.DISABLED)])
+class VimJumpServiceImpl : VimJumpServiceBase(), PersistentStateComponent<Element?> {
+  companion object {
+    private val logger = vimLogger<VimJumpServiceImpl>()
+  }
+
   override fun includeCurrentCommandAsNavigation(editor: VimEditor) {
     val project = (editor as IjVimEditor).editor.project
     if (project != null) {
       IdeDocumentHistory.getInstance(project).includeCurrentCommandAsNavigation()
+    }
+  }
+
+  override fun getState(): Element {
+    val jumpsElem = Element("jumps");
+    for (jump in jumps) {
+      val jumpElem = Element("jump");
+      jumpElem.setAttribute("line", jump.line.toString())
+      jumpElem.setAttribute("column", jump.col.toString())
+      jumpElem.setAttribute("filename", StringUtil.notNullize(jump.filepath))
+      jumpsElem.addContent(jumpElem);
+      if (logger.isDebug()) {
+        logger.debug("saved jump = $jump");
+      }
+    }
+    return jumpsElem
+  }
+
+  override fun loadState(state: Element) {
+    val jumpList = state.getChildren("jump");
+    for (jumpElement in jumpList) {
+      val jump = Jump(Integer.parseInt(jumpElement.getAttributeValue("line")),
+        Integer.parseInt(jumpElement.getAttributeValue("column")),
+        jumpElement.getAttributeValue("filename"))
+      jumps.add(jump)
+    }
+
+    if (logger.isDebug()) {
+      logger.debug("jumps=$jumps");
     }
   }
 }
