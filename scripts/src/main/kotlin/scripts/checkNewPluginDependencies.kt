3d452015ee42591930989eddf764f324c384fc8e
Alex Plate <aleksei.plate@jetbrains.com>
2022-12-29 12:43:21 +0200

    Small refactoring of scripts

--- scripts/src/main/kotlin/scripts/checkNewPluginDependencies.kt
+++ scripts/src/main/kotlin/scripts/checkNewPluginDependencies.kt
@@ -8,22 +8,18 @@
 
 package scripts
 
-import io.ktor.client.*
 import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.coroutines.runBlocking
 
 /**
- * Marketplace has a API to get all plugins that depend on our plugin.
- * Here we have a list of dependent plugins at some moment and we check if something changed in that.
+ * Marketplace has an API to get all plugins that depend on our plugin.
+ * Here we have a list of dependent plugins at some moment, and we check if something changed in that.
  * If so, we need to update our list of plugins.
  *
  * This script makes no actions and aimed to notify the devs in case they need to update the list of IdeaVim plugins.
  */
 
+@Suppress("SpellCheckingInspection")
 val knownPlugins = listOf(
   "IdeaVimExtension",
   "github.zgqq.intellij-enhance",
@@ -35,20 +31,12 @@ val knownPlugins = listOf(
   "com.joshestein.ideavim-quickscope",
 )
 
-fun main() {
-  val client = HttpClient(CIO) {
-    install(ContentNegotiation) {
-      json()
-    }
+suspend fun main() {
+  val response = client.get("https://plugins.jetbrains.com/api/plugins/") {
+    parameter("dependency", "IdeaVIM")
+    parameter("includeOptional", true)
   }
-
-  runBlocking {
-    val res = client.get("https://plugins.jetbrains.com/api/plugins/") {
-      parameter("dependency", "IdeaVIM")
-      parameter("includeOptional", true)
-    }
-    val output = res.body<List<String>>()
-    println(output)
-    if (knownPlugins != output) error("Unknown plugins list: ${output}")
-  }
+  val output = response.body<List<String>>()
+  println(output)
+  if (knownPlugins != output) error("Unknown plugins list: $output")
 }
---
+++ scripts/src/main/kotlin/scripts/ktor.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2003-2022 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package scripts
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+
+internal val client = HttpClient(CIO) {
+  install(ContentNegotiation) {
+    json()
+  }
+}
+
