e28ec93c8064cb91e41df8b2aec11b6341785212
Alex Plate <aleksei.plate@jetbrains.com>
2023-01-04 08:44:28 +0200

    Reformat project

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/option/StrictMode.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/option/StrictMode.kt
@@ -4,7 +4,6 @@ import com.maddyhome.idea.vim.api.injector
 import com.maddyhome.idea.vim.options.OptionConstants
 import com.maddyhome.idea.vim.options.OptionScope
 
-
 @Deprecated("Please use StrictMode from com.maddyhome.idea.vim.options.helpers", replaceWith = ReplaceWith("com.maddyhome.idea.vim.options.helpers.StrictMode"))
 object StrictMode {
   @JvmName("assertTrue")
