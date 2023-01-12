35ef9c7e68d7cff28227e6fac0aeb76ce88541a2
Alex Plate <aleksei.plate@jetbrains.com>
2023-01-12 09:27:07 +0200

    Update new .ideavimrc template

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/services/VimRcService.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/services/VimRcService.kt
@@ -51,9 +51,14 @@ object VimRcService {
   }
 
   private val newIdeaVimRcTemplate = """
+    " .ideavimrc is a configuration file for IdeaVim plugin. It uses
+    "   the same commands as the original .vimrc configuration.
+    " You can find a list of commands here: https://jb.gg/h38q75
+    " Find more examples here: https://jb.gg/share-ideavimrc
+
     "" Source your .vimrc
     "source ~/.vimrc
-    
+
     "" -- Suggested options --
     " Show a few lines of context around the cursor. Note that this makes the
     " text scroll if you mouse-click near the start or end of the window.
@@ -64,8 +69,15 @@ object VimRcService {
 
     " Don't use Ex mode, use Q for formatting.
     map Q gq
-    
-    
+
+    " --- Enable IdeaVim plugins https://jb.gg/ideavim-plugins
+
+    " Highlight copied text
+    Plug 'machakann/vim-highlightedyank'
+    " Commentary plugin
+    Plug 'tpope/vim-commentary'
+
+
     "" -- Map IDE actions to IdeaVim -- https://jb.gg/abva4t
     "" Map \r to the Reformat Code action
     "map \r <Action>(ReformatCode)
@@ -75,10 +87,7 @@ object VimRcService {
 
     "" Map \b to toggle the breakpoint on the current line
     "map \b <Action>(ToggleLineBreakpoint)
-    
-    
-    " Find more examples here: https://jb.gg/share-ideavimrc
-    
+
   """.trimIndent()
 
   fun findOrCreateIdeaVimRc(): File? {
