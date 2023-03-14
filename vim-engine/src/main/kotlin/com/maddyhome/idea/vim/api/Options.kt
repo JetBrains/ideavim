76a4499c9f1e3ddb6f4cf6588897b70357c7c133
Matt Ellis <m.t.ellis@gmail.com>
2023-03-14 09:09:36 +0000

    Introduce properties for option definitions

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/Options.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/Options.kt
@@ -23,113 +23,62 @@ import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimString
 
+@Suppress("unused", "SpellCheckingInspection")
 public object Options {
   private val logger = vimLogger<Options>()
+  private val options = MultikeyMap()
 
   public fun getOption(key: String): Option<out VimDataType>? = options.get(key)
   public fun getAllOptions(): Set<Option<out VimDataType>> = options.values.toSet()
 
-  public fun addOption(option: Option<out VimDataType>): Unit = options.put(option.name, option.abbrev, option)
+  public fun <T : Option<out VimDataType>> addOption(option: T): T =
+    option.also { options.put(option.name, option.abbrev, option) }
+
   public fun removeOption(optionName: String): Unit = options.remove(optionName)
 
-  private val options = MultikeyMap(
-    // Simple options, sorted by name
-    ToggleOption(OptionConstants.digraph, "dg", false),
-    ToggleOption(OptionConstants.gdefault, "gd", false),
-    UnsignedNumberOption(OptionConstants.history, "hi", 50),
-    ToggleOption(OptionConstants.hlsearch, "hls", false),
-    ToggleOption(OptionConstants.ignorecase, "ic", false),
-    ToggleOption(OptionConstants.incsearch, "is", false),
-    NumberOption(OptionConstants.maxmapdepth, "mmd", 20),
-    ToggleOption(OptionConstants.more, "more", true),
-    StringOption(OptionConstants.nrformats, "nf", "hex", isList = true, setOf("octal", "hex", "alpha")),
-    ToggleOption(OptionConstants.number, "nu", false),
-    ToggleOption(OptionConstants.relativenumber, "rnu", false),
-    NumberOption(OptionConstants.scroll, "scr", 0),
-    NumberOption(OptionConstants.scrolloff, "so", 0),
-    StringOption(OptionConstants.selection, "sel", "inclusive", isList = false, setOf("old", "inclusive", "exclusive")),
-    ToggleOption(OptionConstants.showcmd, "sc", true),
-    ToggleOption(OptionConstants.showmode, "smd", true),
-    NumberOption(OptionConstants.sidescroll, "ss", 0),
-    NumberOption(OptionConstants.sidescrolloff, "siso", 0),
-    ToggleOption(OptionConstants.smartcase, "scs", false),
-    ToggleOption(OptionConstants.startofline, "sol", true),
-    ToggleOption(OptionConstants.timeout, "to", true),
-    UnsignedNumberOption(OptionConstants.timeoutlen, "tm", 1000),
-    UnsignedNumberOption(OptionConstants.undolevels, "ul", 1000),
-    StringOption(OptionConstants.viminfo, "vi", "'100,<50,s10,h", isList = true),
-    StringOption(OptionConstants.virtualedit, "ve", "", isList = false, setOf("onemore", "block", "insert", "all")),
-    ToggleOption(OptionConstants.visualbell, "vb", false),
-    StringOption(OptionConstants.whichwrap, "ww", "b,s", true, setOf("b", "s", "h", "l", "<", ">", "~", "[", "]")),
-    ToggleOption(OptionConstants.wrapscan, "ws", true),
-
-    // Options with longer defaults or additional validation, sorted by name
-    object : StringOption(
-      OptionConstants.guicursor, "gcr",
-      "n-v-c:block-Cursor/lCursor," +
-        "ve:ver35-Cursor," +
-        "o:hor50-Cursor," +
-        "i-ci:ver25-Cursor/lCursor," +
-        "r-cr:hor20-Cursor/lCursor," +
-        "sm:block-Cursor-blinkwait175-blinkoff150-blinkon175",
-      isList = true
-    ) {
-      override fun checkIfValueValid(value: VimDataType, token: String) {
-        super.checkIfValueValid(value, token)
-        val valueAsString = (value as VimString).value
-        valueAsString.split(",").forEach { GuiCursorOptionHelper.convertToken(it) }
-      }
-    },
-    object : StringOption(OptionConstants.iskeyword, "isk", "@,48-57,_", isList = true) {
-      override fun checkIfValueValid(value: VimDataType, token: String) {
-        super.checkIfValueValid(value, token)
-        if (KeywordOptionHelper.isValueInvalid((value as VimString).value)) {
-          throw exExceptionMessage("E474", token)
-        }
-      }
-
-      override fun split(value: String): List<String> {
-        val result = KeywordOptionHelper.parseValues(value)
-        if (result == null) {
-          logger.error("KeywordOptionHelper failed to parse $value")
-          injector.messages.indicateError()
-          injector.messages.showStatusBarMessage(editor = null, "Failed to parse iskeyword option value")
-        }
-        return result ?: split(defaultValue.value)
-      }
-    },
+  // Simple options, sorted by name
+  // Note that we expose options as strongly typed properties to make it easier to consume them. The VimOptionGroup API
+  // will return strongly typed VimDataType derived instances if given a strongly typed option, but fetching by name
+  // loses that type information, and we also have to check that the option is not null, even if we know it exists.
+  // types
+  public val digraph: ToggleOption = addOption(ToggleOption(OptionConstants.digraph, "dg", false))
+  public val gdefault: ToggleOption = addOption(ToggleOption(OptionConstants.gdefault, "gd", false))
+  public val history: UnsignedNumberOption = addOption(UnsignedNumberOption(OptionConstants.history, "hi", 50))
+  public val hlsearch: ToggleOption = addOption(ToggleOption(OptionConstants.hlsearch, "hls", false))
+  public val ignorecase: ToggleOption = addOption(ToggleOption(OptionConstants.ignorecase, "ic", false))
+  public val incsearch: ToggleOption = addOption(ToggleOption(OptionConstants.incsearch, "is", false))
+  public val keymodel: StringOption = addOption(StringOption(
+    OptionConstants.keymodel,
+    "km",
+    "${OptionConstants.keymodel_continueselect},${OptionConstants.keymodel_stopselect}",
+    isList = true,
+    setOf(
+      OptionConstants.keymodel_startsel,
+      OptionConstants.keymodel_stopsel,
+      OptionConstants.keymodel_stopselect,
+      OptionConstants.keymodel_stopvisual,
+      OptionConstants.keymodel_continueselect,
+      OptionConstants.keymodel_continuevisual
+    )
+  ))
+  public val maxmapdepth: NumberOption = addOption(NumberOption(OptionConstants.maxmapdepth, "mmd", 20))
+  public val more: ToggleOption = addOption(ToggleOption(OptionConstants.more, "more", true))
+  public val nrformats: StringOption =
+    addOption(StringOption(OptionConstants.nrformats, "nf", "hex", isList = true, setOf("octal", "hex", "alpha")))
+  public val number: ToggleOption = addOption(ToggleOption(OptionConstants.number, "nu", false))
+  public val relativenumber: ToggleOption = addOption(ToggleOption(OptionConstants.relativenumber, "rnu", false))
+  public val scroll: NumberOption = addOption(NumberOption(OptionConstants.scroll, "scr", 0))
+  public val scrolloff: NumberOption = addOption(NumberOption(OptionConstants.scrolloff, "so", 0))
+  public val selection: StringOption = addOption(
     StringOption(
-      OptionConstants.keymodel,
-      "km",
-      "${OptionConstants.keymodel_continueselect},${OptionConstants.keymodel_stopselect}",
-      isList = true,
-      setOf(
-        OptionConstants.keymodel_startsel,
-        OptionConstants.keymodel_stopsel,
-        OptionConstants.keymodel_stopselect,
-        OptionConstants.keymodel_stopvisual,
-        OptionConstants.keymodel_continueselect,
-        OptionConstants.keymodel_continuevisual
-      )
-    ),
-    object : StringOption(OptionConstants.matchpairs, "mps", "(:),{:},[:]", isList = true) {
-      override fun checkIfValueValid(value: VimDataType, token: String) {
-        super.checkIfValueValid(value, token)
-        for (v in split((value as VimString).value)) {
-          if (!v.matches(Regex(".:."))) {
-            throw exExceptionMessage("E474", token)
-          }
-        }
-      }
-    },
-    object : NumberOption(OptionConstants.scrolljump, "sj", 1) {
-      override fun checkIfValueValid(value: VimDataType, token: String) {
-        super.checkIfValueValid(value, token)
-        if ((value as VimInt).value < -100) {
-          throw ExException("E49: Invalid scroll size: $token")
-        }
-      }
-    },
+      OptionConstants.selection,
+      "sel",
+      "inclusive",
+      isList = false,
+      setOf("old", "inclusive", "exclusive")
+    )
+  )
+  public val selectmode: StringOption = addOption(
     StringOption(
       OptionConstants.selectmode, "slm", "", isList = true,
       setOf(
@@ -138,60 +87,157 @@ public object Options {
         OptionConstants.selectmode_cmd,
         OptionConstants.selectmode_ideaselection
       )
-    ),
+    )
+  )
+  public val shell: StringOption = addOption(
     StringOption(
       OptionConstants.shell,
       "sh",
       if (injector.systemInfoService.isWindows) "cmd.exe" else System.getenv("SHELL") ?: "sh"
-    ),
-    object : StringOption(OptionConstants.shellcmdflag, "shcf", "") {
-      override val defaultValue: VimString
-        get() {
-          // Default value depends on the "shell" option
-          val shell = injector.globalOptions().getStringValue(OptionConstants.shell)
-          return VimString(
-            when {
-              injector.systemInfoService.isWindows && shell.contains("powershell") -> "-Command"
-              injector.systemInfoService.isWindows && !shell.contains("sh") -> "/c"
-              else -> "-c"
-            }
-          )
-        }
-    },
+    )
+  )
+  public val shellxescape: StringOption = addOption(
     StringOption(
       OptionConstants.shellxescape,
       "sxe",
       if (injector.systemInfoService.isWindows) "\"&|<>()@^" else "",
       isList = false
-    ),
-    object : StringOption(OptionConstants.shellxquote, "sxq", "") {
-      override val defaultValue: VimString
-        get() {
-          // Default value depends on the "shell" option
-          val shell = injector.globalOptions().getStringValue(OptionConstants.shell)
-          return VimString(
-            when {
-              injector.systemInfoService.isWindows && shell == "cmd.exe" -> "("
-              injector.systemInfoService.isWindows && shell.contains("sh") -> "\""
-              else -> ""
-            }
-          )
-        }
-    },
+    )
+  )
+  public val showcmd: ToggleOption = addOption(ToggleOption(OptionConstants.showcmd, "sc", true))
+  public val showmode: ToggleOption = addOption(ToggleOption(OptionConstants.showmode, "smd", true))
+  public val sidescroll: NumberOption = addOption(NumberOption(OptionConstants.sidescroll, "ss", 0))
+  public val sidescrolloff: NumberOption = addOption(NumberOption(OptionConstants.sidescrolloff, "siso", 0))
+  public val smartcase: ToggleOption = addOption(ToggleOption(OptionConstants.smartcase, "scs", false))
+  public val startofline: ToggleOption = addOption(ToggleOption(OptionConstants.startofline, "sol", true))
+  public val timeout: ToggleOption = addOption(ToggleOption(OptionConstants.timeout, "to", true))
+  public val timeoutlen: UnsignedNumberOption = addOption(UnsignedNumberOption(OptionConstants.timeoutlen, "tm", 1000))
+  public val undolevels: UnsignedNumberOption = addOption(UnsignedNumberOption(OptionConstants.undolevels, "ul", 1000))
+  public val viminfo: StringOption =
+    addOption(StringOption(OptionConstants.viminfo, "vi", "'100,<50,s10,h", isList = true))
+  public val virtualedit: StringOption = addOption(
+    StringOption(
+      OptionConstants.virtualedit,
+      "ve",
+      "",
+      isList = false,
+      setOf("onemore", "block", "insert", "all")
+    )
+  )
+  public val visualbell: ToggleOption = addOption(ToggleOption(OptionConstants.visualbell, "vb", false))
+  public val whichwrap: StringOption = addOption(
+    StringOption(
+      OptionConstants.whichwrap,
+      "ww",
+      "b,s",
+      true,
+      setOf("b", "s", "h", "l", "<", ">", "~", "[", "]")
+    )
+  )
+  public val wrapscan: ToggleOption = addOption(ToggleOption(OptionConstants.wrapscan, "ws", true))
 
-    // TODO: Clipboard is special - it is overridden to provide a new default value for the IJ implementation
+
+  // More complex options, with additional validation, etc.
+  public val guicursor: StringOption = addOption(object : StringOption(
+    OptionConstants.guicursor, "gcr",
+    "n-v-c:block-Cursor/lCursor," +
+      "ve:ver35-Cursor," +
+      "o:hor50-Cursor," +
+      "i-ci:ver25-Cursor/lCursor," +
+      "r-cr:hor20-Cursor/lCursor," +
+      "sm:block-Cursor-blinkwait175-blinkoff150-blinkon175",
+    isList = true
+  ) {
+    override fun checkIfValueValid(value: VimDataType, token: String) {
+      super.checkIfValueValid(value, token)
+      val valueAsString = (value as VimString).value
+      valueAsString.split(",").forEach { GuiCursorOptionHelper.convertToken(it) }
+    }
+  })
+
+  public val iskeyword: StringOption = addOption(object : StringOption(OptionConstants.iskeyword, "isk", "@,48-57,_", isList = true) {
+    override fun checkIfValueValid(value: VimDataType, token: String) {
+      super.checkIfValueValid(value, token)
+      if (KeywordOptionHelper.isValueInvalid((value as VimString).value)) {
+        throw exExceptionMessage("E474", token)
+      }
+    }
+
+    override fun split(value: String): List<String> {
+      val result = KeywordOptionHelper.parseValues(value)
+      if (result == null) {
+        logger.error("KeywordOptionHelper failed to parse $value")
+        injector.messages.indicateError()
+        injector.messages.showStatusBarMessage(editor = null, "Failed to parse iskeyword option value")
+      }
+      return result ?: split(defaultValue.value)
+    }
+  })
+
+  public val matchpairs: StringOption = addOption(object : StringOption(OptionConstants.matchpairs, "mps", "(:),{:},[:]", isList = true) {
+    override fun checkIfValueValid(value: VimDataType, token: String) {
+      super.checkIfValueValid(value, token)
+      for (v in split((value as VimString).value)) {
+        if (!v.matches(Regex(".:."))) {
+          throw exExceptionMessage("E474", token)
+        }
+      }
+    }
+  })
+
+  public val scrolljump: NumberOption = addOption(object : NumberOption(OptionConstants.scrolljump, "sj", 1) {
+    override fun checkIfValueValid(value: VimDataType, token: String) {
+      super.checkIfValueValid(value, token)
+      if ((value as VimInt).value < -100) {
+        throw ExException("E49: Invalid scroll size: $token")
+      }
+    }
+  })
+
+  public val shellcmdflag: StringOption = addOption(object : StringOption(OptionConstants.shellcmdflag, "shcf", "") {
+    override val defaultValue: VimString
+      get() {
+        // Default value depends on the "shell" option
+        val shell = injector.globalOptions().getStringValue(OptionConstants.shell)
+        return VimString(
+          when {
+            injector.systemInfoService.isWindows && shell.contains("powershell") -> "-Command"
+            injector.systemInfoService.isWindows && !shell.contains("sh") -> "/c"
+            else -> "-c"
+          }
+        )
+      }
+  })
+
+  public val shellxquote: StringOption = addOption(object : StringOption(OptionConstants.shellxquote, "sxq", "") {
+    override val defaultValue: VimString
+      get() {
+        // Default value depends on the "shell" option
+        val shell = injector.globalOptions().getStringValue(OptionConstants.shell)
+        return VimString(
+          when {
+            injector.systemInfoService.isWindows && shell == "cmd.exe" -> "("
+            injector.systemInfoService.isWindows && shell.contains("sh") -> "\""
+            else -> ""
+          }
+        )
+      }
+  })
+
+  // TODO: Clipboard is special - ideaput should only be defined in the IntelliJ build
+  public val clipboard: StringOption = addOption(
     StringOption(
       OptionConstants.clipboard,
       OptionConstants.clipboardAlias,
-      "autoselect,exclude:cons\\|linux",
+      "ideaput,autoselect,exclude:cons\\|linux",
       isList = true
-    ),
-
-    // IdeaVim specific options. Put any editor/IDE specific options in IjVimOptionService
-    ToggleOption(OptionConstants.ideaglobalmode, OptionConstants.ideaglobalmode, false),
-    ToggleOption(OptionConstants.ideastrictmode, OptionConstants.ideastrictmode, false),
-    ToggleOption(OptionConstants.ideatracetime, OptionConstants.ideatracetime, false),
+    )
   )
+
+  // IdeaVim specific options. Put any editor or IDE specific options in IjVimOptionService
+  public val ideaglobalmode: ToggleOption = addOption(ToggleOption(OptionConstants.ideaglobalmode, OptionConstants.ideaglobalmode, false))
+  public val ideastrictmode: ToggleOption = addOption(ToggleOption(OptionConstants.ideastrictmode, OptionConstants.ideastrictmode, false))
+  public val ideatracetime: ToggleOption = addOption(ToggleOption(OptionConstants.ideatracetime, OptionConstants.ideatracetime, false))
 }
 
 private class MultikeyMap(vararg entries: Option<out VimDataType>) {
