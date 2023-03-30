2a6b6f89f42f4d21a2f7c6e534191a3d8ac86428
aleksei.plate <aleksei.plate@TeamCity>
2023-03-30 04:55:46 +0000

    TeamCity change in 'IntelliJ IDEA plugins / IdeaVim' project: project parameters were changed

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -19,6 +19,9 @@ changeProject(DslContext.projectId) {
             password("env.CERTIFICATE_CHAIN", "credentialsJSON:1bab4a88-10e7-4bf9-856c-e6253499dc95", display = ParameterDisplay.HIDDEN)
         }
         add {
+            password("env.PRIVATE_KEY_PASSWORD", "credentialsJSON:7c12c867-fe09-4a2f-884d-6fd0ec0a1e79", display = ParameterDisplay.HIDDEN)
+        }
+        add {
             password("env.PRIVATE_KEY", "credentialsJSON:5d8b553d-fd7e-4347-abd2-51d8d0f2b3f7", display = ParameterDisplay.HIDDEN)
         }
     }
