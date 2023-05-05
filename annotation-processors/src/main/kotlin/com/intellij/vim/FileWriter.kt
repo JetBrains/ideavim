3586ae02057675db0ffad6a4e989963e3d7a7341
filipp <filipp.vakhitov@jetbrains.com>
2023-05-05 09:53:33 +0300

    Add VimscriptFunction annotation

---
+++ annotation-processors/src/main/kotlin/com/intellij/vim/FileWriter.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2003-2023 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.intellij.vim
+
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import java.io.File
+
+class FileWriter {
+  fun generateResourceFile(fileName: String, content: String, environment: SymbolProcessorEnvironment) {
+    val resourcesDir = environment.options["resourcesDir"]
+    val file = File("$resourcesDir/$fileName")
+    file.writeText(content)
+  }
+}
