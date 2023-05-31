5a1e04323cbfe06431b28977f38ab86c426d5001
samabcde <tszsumng90@gmail.com>
2023-06-01 00:30:15 +0800

    VIM-1990 fix repeat delete find or till wrong MotionType

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/motion/leftright/MotionLastMatchCharAction.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/motion/leftright/MotionLastMatchCharAction.kt
@@ -17,7 +17,15 @@ import com.maddyhome.idea.vim.command.OperatorArguments
 import com.maddyhome.idea.vim.handler.Motion
 import com.maddyhome.idea.vim.handler.MotionActionHandler
 
-public class MotionLastMatchCharAction : MotionActionHandler.ForEachCaret() {
+public class MotionLastMatchCharAction : MotionLastMatchCharActionBase(MotionType.INCLUSIVE, reverse = false)
+public class MotionLastMatchCharReverseAction : MotionLastMatchCharActionBase(MotionType.EXCLUSIVE, reverse = true)
+
+public sealed class MotionLastMatchCharActionBase(defaultMotionType: MotionType, private val reverse: Boolean) :
+  MotionActionHandler.ForEachCaret() {
+
+  // injector.motion.lastFTCmd not up-to-date during construction, need to set during getOffset
+  override var motionType: MotionType = defaultMotionType
+
   override fun getOffset(
     editor: VimEditor,
     caret: ImmutableVimCaret,
@@ -25,24 +33,20 @@ public class MotionLastMatchCharAction : MotionActionHandler.ForEachCaret() {
     argument: Argument?,
     operatorArguments: OperatorArguments,
   ): Motion {
-    val repeatLastMatchChar = injector.motion.repeatLastMatchChar(editor, caret, operatorArguments.count1)
+    motionType = when (injector.motion.lastFTCmd) {
+      TillCharacterMotionType.LAST_SMALL_F, TillCharacterMotionType.LAST_SMALL_T -> MotionType.INCLUSIVE
+      TillCharacterMotionType.LAST_F, TillCharacterMotionType.LAST_T -> MotionType.EXCLUSIVE
+    }.let { if (reverse) it.reverse() else it }
+    val repeatLastMatchChar =
+      injector.motion.repeatLastMatchChar(editor, caret, operatorArguments.count1.let { if (reverse) -it else it })
     return if (repeatLastMatchChar < 0) Motion.Error else Motion.AbsoluteOffset(repeatLastMatchChar)
   }
 
-  override val motionType: MotionType = MotionType.EXCLUSIVE
-}
-
-public class MotionLastMatchCharReverseAction : MotionActionHandler.ForEachCaret() {
-  override fun getOffset(
-    editor: VimEditor,
-    caret: ImmutableVimCaret,
-    context: ExecutionContext,
-    argument: Argument?,
-    operatorArguments: OperatorArguments,
-  ): Motion {
-    val repeatLastMatchChar = injector.motion.repeatLastMatchChar(editor, caret, -operatorArguments.count1)
-    return if (repeatLastMatchChar < 0) Motion.Error else Motion.AbsoluteOffset(repeatLastMatchChar)
+  public fun MotionType.reverse(): MotionType {
+    return when (this) {
+      MotionType.INCLUSIVE -> MotionType.EXCLUSIVE
+      MotionType.EXCLUSIVE -> MotionType.INCLUSIVE
+      else -> throw IllegalArgumentException("no reverse for $this")
+    }
   }
-
-  override val motionType: MotionType = MotionType.EXCLUSIVE
 }
