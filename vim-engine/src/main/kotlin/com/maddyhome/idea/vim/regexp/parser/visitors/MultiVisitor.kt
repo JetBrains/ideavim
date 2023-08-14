3efe11f393e28105b0c1038720bcc227931e3110
Emanuel Gestosa <emanuel-367@hotmail.com>
2023-08-14 15:15:11 +0100

    refactoring visitors to prepare for different types of multis

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/regexp/parser/visitors/MultiVisitor.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/regexp/parser/visitors/MultiVisitor.kt
@@ -13,45 +13,48 @@ import com.maddyhome.idea.vim.regexp.parser.generated.RegexParserBaseVisitor
 import org.antlr.v4.runtime.Token
 import org.antlr.v4.runtime.tree.TerminalNode
 
-internal class MultiVisitor : RegexParserBaseVisitor<MultiDelimiter>() {
+internal class MultiVisitor : RegexParserBaseVisitor<Multi>() {
 
-  override fun visitZeroOrMore(ctx: RegexParser.ZeroOrMoreContext): MultiDelimiter {
-    return MultiDelimiter(MultiBoundary.IntMultiBoundary(0), MultiBoundary.InfiniteMultiBoundary, true)
+  override fun visitZeroOrMore(ctx: RegexParser.ZeroOrMoreContext): Multi {
+    return Multi.RangeMulti(MultiBoundary.IntMultiBoundary(0), MultiBoundary.InfiniteMultiBoundary, true)
   }
 
-  override fun visitOneOrMore(ctx: RegexParser.OneOrMoreContext): MultiDelimiter {
-    return MultiDelimiter(MultiBoundary.IntMultiBoundary(1), MultiBoundary.InfiniteMultiBoundary, true)
+  override fun visitOneOrMore(ctx: RegexParser.OneOrMoreContext): Multi {
+    return Multi.RangeMulti(MultiBoundary.IntMultiBoundary(1), MultiBoundary.InfiniteMultiBoundary, true)
   }
 
-  override fun visitZeroOrOne(ctx: RegexParser.ZeroOrOneContext?): MultiDelimiter {
-    return MultiDelimiter(MultiBoundary.IntMultiBoundary(0), MultiBoundary.IntMultiBoundary(1), true)
+  override fun visitZeroOrOne(ctx: RegexParser.ZeroOrOneContext?): Multi {
+    return Multi.RangeMulti(MultiBoundary.IntMultiBoundary(0), MultiBoundary.IntMultiBoundary(1), true)
   }
 
-  override fun visitRangeGreedy(ctx: RegexParser.RangeGreedyContext): MultiDelimiter {
+  override fun visitRangeGreedy(ctx: RegexParser.RangeGreedyContext): Multi {
     return visitRange(ctx.lower_bound, ctx.upper_bound, ctx.COMMA(), true)
   }
 
-  override fun visitRangeLazy(ctx: RegexParser.RangeLazyContext): MultiDelimiter {
+  override fun visitRangeLazy(ctx: RegexParser.RangeLazyContext): Multi {
     return visitRange(ctx.lower_bound, ctx.upper_bound, ctx.COMMA(), false)
   }
 
-  private fun visitRange(lowerBoundToken: Token?, upperBoundToken: Token?, comma: TerminalNode?, isGreedy: Boolean): MultiDelimiter {
+  private fun visitRange(lowerBoundToken: Token?, upperBoundToken: Token?, comma: TerminalNode?, isGreedy: Boolean): Multi {
     val lowerDelimiter = if (lowerBoundToken == null) MultiBoundary.IntMultiBoundary(0) else MultiBoundary.IntMultiBoundary(lowerBoundToken.text.toInt())
     val upperDelimiter = if (comma != null) if (upperBoundToken == null) MultiBoundary.InfiniteMultiBoundary else MultiBoundary.IntMultiBoundary(upperBoundToken.text.toInt())
     else if (lowerBoundToken == null) MultiBoundary.InfiniteMultiBoundary else lowerDelimiter
-    return MultiDelimiter(lowerDelimiter, upperDelimiter, isGreedy)
+    return Multi.RangeMulti(lowerDelimiter, upperDelimiter, isGreedy)
   }
 }
 
-/**
- * Delimits the number of times that a multi should
- * make a certain atom repeat itself
- */
-internal data class MultiDelimiter(
-  val lowerBoundary: MultiBoundary.IntMultiBoundary,
-  val upperBoundary: MultiBoundary,
-  val isGreedy: Boolean
-)
+internal sealed class Multi {
+
+  /**
+   * Delimits the number of times that a multi should
+   * make a certain atom repeat itself
+   */
+  internal data class RangeMulti(
+    val lowerBoundary: MultiBoundary.IntMultiBoundary,
+    val upperBoundary: MultiBoundary,
+    val isGreedy: Boolean
+    ) : Multi()
+}
 
 internal sealed class MultiBoundary {
   /**
