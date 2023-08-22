89973809af330119eed04625f10d6c90ddbb6e83
Emanuel Gestosa <emanuel-367@hotmail.com>
2023-08-22 15:42:06 +0100

    keyword class visitor

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/regexp/parser/visitors/CollectionElementVisitor.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/regexp/parser/visitors/CollectionElementVisitor.kt
@@ -107,6 +107,10 @@ internal class CollectionElementVisitor : RegexParserBaseVisitor<Pair<Collection
     return Pair(CollectionElement.CharacterClassExpression{it.isJavaIdentifierPart()}, false)
   }
 
+  override fun visitKeywordClass(ctx: RegexParser.KeywordClassContext?): Pair<CollectionElement, Boolean> {
+    return Pair(CollectionElement.CharacterClassExpression{it.isLetterOrDigit() || it == '_'}, false)
+  }
+
   private fun cleanLiteralChar(str: String) : Pair<Char, Boolean> {
     return  if (str.length > 2 && str[0] == '\\' && str[1] == 'd') Pair(Char(str.substring(2).toInt()), false)
     else if (str.length > 2 && str[0] == '\\' && str[1] == 'o') Pair(Char(str.substring(2).toInt(8)), false)
