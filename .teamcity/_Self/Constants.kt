5ab549ae96b69afbedce6a6e18344ee0c304aa6a
Alex Plate <aleksei.plate@jetbrains.com>
2023-10-27 10:42:16 +0300

    Switch to 2023.2.4 for github, nvim, proprty, and long running tests as the compilation with the latest EAP is now broken

--- .teamcity/_Self/Constants.kt
+++ .teamcity/_Self/Constants.kt
@@ -5,11 +5,11 @@ object Constants {
   const val EAP_CHANNEL = "eap"
   const val DEV_CHANNEL = "Dev"
 
-  const val GITHUB_TESTS = "LATEST-EAP-SNAPSHOT"
-  const val NVIM_TESTS = "LATEST-EAP-SNAPSHOT"
-  const val PROPERTY_TESTS = "LATEST-EAP-SNAPSHOT"
-  const val LONG_RUNNING_TESTS = "LATEST-EAP-SNAPSHOT"
-  const val QODANA_TESTS = "LATEST-EAP-SNAPSHOT"
+  const val GITHUB_TESTS = "2023.2.4"
+  const val NVIM_TESTS = "2023.2.4"
+  const val PROPERTY_TESTS = "2023.2.4"
+  const val LONG_RUNNING_TESTS = "2023.2.4"
+  const val QODANA_TESTS = "2023.2.4"
   const val RELEASE = "2023.1.2"
 
   const val RELEASE_DEV = "2023.1.2"
