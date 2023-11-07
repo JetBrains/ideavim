0c66fb474e913c80bb5554a05df196294a7bcbe4
Alex Plate <aleksei.plate@jetbrains.com>
2023-11-07 09:42:28 +0200

    Fix(VIM-3130): Change the build version to 2023.1.2
    There is a problem with building IdeaVim on newer version. Some of the internal classes
    was migrated from java to kotlin. However, this change is not binary compatible because of a kotlin bug.
    Building IdeaVim on java version of this class fixes this problem.

--- .teamcity/_Self/Constants.kt
+++ .teamcity/_Self/Constants.kt
@@ -5,13 +5,13 @@ object Constants {
   const val EAP_CHANNEL = "eap"
   const val DEV_CHANNEL = "Dev"
 
-  const val GITHUB_TESTS = "2023.2.4"
-  const val NVIM_TESTS = "2023.2.4"
-  const val PROPERTY_TESTS = "2023.2.4"
-  const val LONG_RUNNING_TESTS = "2023.2.4"
-  const val QODANA_TESTS = "2023.2.4"
-  const val RELEASE = "2023.2.4"
+  const val GITHUB_TESTS = "2023.1.2"
+  const val NVIM_TESTS = "2023.1.2"
+  const val PROPERTY_TESTS = "2023.1.2"
+  const val LONG_RUNNING_TESTS = "2023.1.2"
+  const val QODANA_TESTS = "2023.1.2"
+  const val RELEASE = "2023.1.2"
 
-  const val RELEASE_DEV = "2023.2.4"
-  const val RELEASE_EAP = "2023.2.4"
+  const val RELEASE_DEV = "2023.1.2"
+  const val RELEASE_EAP = "2023.1.2"
 }
