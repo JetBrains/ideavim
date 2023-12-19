e2e2b4d176a7123fb4bebf3a6e8bb4d5ad2d56fc
Alex Plate <aleksei.plate@jetbrains.com>
2023-12-19 09:59:54 +0200

    Proper tear down with tests with mock

--- src/test/java/org/jetbrains/plugins/ideavim/mock/MockTestCase.kt
+++ src/test/java/org/jetbrains/plugins/ideavim/mock/MockTestCase.kt
@@ -12,11 +12,14 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.textarea.TextComponentEditorImpl
+import com.intellij.openapi.util.Disposer
 import com.intellij.testFramework.junit5.TestDisposable
 import com.intellij.testFramework.replaceService
 import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.newapi.ij
 import com.maddyhome.idea.vim.newapi.vim
 import org.jetbrains.plugins.ideavim.VimTestCase
+import org.junit.jupiter.api.AfterEach
 import org.mockito.Mockito
 import javax.swing.JTextArea
 
@@ -27,6 +30,11 @@ open class MockTestCase : VimTestCase() {
 
   val editorStub = TextComponentEditorImpl(null, JTextArea()).vim
   val contextStub: ExecutionContext = DataContext.EMPTY_CONTEXT.vim
+
+  @AfterEach
+  fun tearDown() {
+    editorStub.carets().forEach { Disposer.dispose(it.ij) }
+  }
 
   fun <T : Any> mockService(service: Class<T>): T {
     val mock = Mockito.mock(service)
