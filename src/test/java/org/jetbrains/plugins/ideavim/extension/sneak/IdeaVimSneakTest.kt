0df96a24bd32f2e513b9ea6154306edb78c4bd0b
Alex Plate <aleksei.plate@jetbrains.com>
2024-02-22 09:19:24 +0200

    Add a missing @BeforeEach in tests

--- src/test/java/org/jetbrains/plugins/ideavim/extension/sneak/IdeaVimSneakTest.kt
+++ src/test/java/org/jetbrains/plugins/ideavim/extension/sneak/IdeaVimSneakTest.kt
@@ -11,11 +11,12 @@ package org.jetbrains.plugins.ideavim.extension.sneak
 import com.maddyhome.idea.vim.api.keys
 import com.maddyhome.idea.vim.state.mode.Mode
 import org.jetbrains.plugins.ideavim.VimTestCase
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInfo
 
 class IdeaVimSneakTest : VimTestCase() {
-  @Throws(Exception::class)
+  @BeforeEach
   override fun setUp(testInfo: TestInfo) {
     super.setUp(testInfo)
     enableExtensions("sneak")
