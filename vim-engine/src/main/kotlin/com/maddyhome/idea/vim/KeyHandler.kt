eea3336934a5c520ee35ae1a5bb277b27b99e4e3
filipp <x@lipp.fi>
2024-02-23 14:27:04 +0200

    Move some logic to CommandConsumer

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/KeyHandler.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/KeyHandler.kt
@@ -140,22 +140,7 @@ public class KeyHandler {
         it.consumeKey( key, editor, allowKeyMappings, mappingCompleted, processBuilder, shouldRecord )
       }
       if (!isProcessed) {
-        // Ask the key/action tree if this is an appropriate key at this point in the command and if so,
-        // return the node matching this keystroke
-        val node: Node<LazyVimCommand>? = mapOpCommand(key, commandBuilder.getChildNode(key), editorState.mode, processBuilder.state)
-        LOG.trace("Get the node for the current mode")
-
-        if (node is CommandNode<LazyVimCommand>) {
-          LOG.trace("Node is a command node")
-          handleCommandNode(key, node, processBuilder)
-          commandBuilder.addKey(key)
-          isProcessed = true
-        } else if (node is CommandPartNode<LazyVimCommand>) {
-          LOG.trace("Node is a command part node")
-          commandBuilder.setCurrentCommandPartNode(node)
-          commandBuilder.addKey(key)
-          isProcessed = true
-        } else if (isSelectRegister(key, processBuilder.state, editorState)) {
+        if (isSelectRegister(key, processBuilder.state, editorState)) {
           LOG.trace("Select register")
           editorState.isRegisterPending = true
           commandBuilder.addKey(key)
@@ -250,23 +235,6 @@ public class KeyHandler {
     injector.messages.indicateError()
   }
 
-
-  /**
-   * See the description for [com.maddyhome.idea.vim.command.DuplicableOperatorAction]
-   */
-  private fun mapOpCommand(
-    key: KeyStroke,
-    node: Node<LazyVimCommand>?,
-    mode: Mode,
-    keyState: KeyHandlerState,
-  ): Node<LazyVimCommand>? {
-    return if (isDuplicateOperatorKeyStroke(key, mode, keyState)) {
-      keyState.commandBuilder.getChildNode(KeyStroke.getKeyStroke('_'))
-    } else {
-      node
-    }
-  }
-
   public fun isDuplicateOperatorKeyStroke(key: KeyStroke, mode: Mode, keyState: KeyHandlerState): Boolean {
     return isOperatorPending(mode, keyState) && keyState.commandBuilder.isDuplicateOperatorKeyStroke(key)
   }
@@ -326,134 +294,6 @@ public class KeyHandler {
         injector.actionExecutor.executeCommand(editor, action, name, action)
       }
     }
-  }
-
-  private fun handleCommandNode(
-    key: KeyStroke,
-    node: CommandNode<LazyVimCommand>,
-    processBuilder: KeyProcessResult.KeyProcessResultBuilder,
-  ) {
-    LOG.trace("Handle command node")
-    // The user entered a valid command. Create the command and add it to the stack.
-    val action = node.actionHolder.instance
-    val keyState = processBuilder.state
-    val commandBuilder = keyState.commandBuilder
-    val expectedArgumentType = commandBuilder.expectedArgumentType
-    commandBuilder.pushCommandPart(action)
-    if (!checkArgumentCompatibility(expectedArgumentType, action)) {
-      LOG.trace("Return from command node handling")
-      processBuilder.addExecutionStep { lamdaKeyState, lambdaEditor, _ ->
-        setBadCommand(lambdaEditor, lamdaKeyState)
-      }
-      return
-    }
-    if (action.argumentType == null || stopMacroRecord(node)) {
-      LOG.trace("Set command state to READY")
-      commandBuilder.commandState = CurrentCommandState.READY
-    } else {
-      processBuilder.addExecutionStep { lambdaKeyState, lambdaEditor, lambdaContext ->
-        LOG.trace("Set waiting for the argument")
-        val argumentType = action.argumentType
-        val editorState = lambdaEditor.vimStateMachine
-        startWaitingForArgument(lambdaEditor, lambdaContext, key.keyChar, action, argumentType!!, lambdaKeyState, editorState)
-        lambdaKeyState.partialReset(editorState.mode)
-      }
-    }
-
-    processBuilder.addExecutionStep { _, lambdaEditor, _ ->
-      // TODO In the name of God, get rid of EX_STRING, FLAG_COMPLETE_EX and all the related staff
-      if (expectedArgumentType === Argument.Type.EX_STRING && action.flags.contains(CommandFlags.FLAG_COMPLETE_EX)) {
-        /* The only action that implements FLAG_COMPLETE_EX is ProcessExEntryAction.
-     * When pressing ':', ExEntryAction is chosen as the command. Since it expects no arguments, it is invoked and
-       calls ProcessGroup#startExCommand, pushes CMD_LINE mode, and the action is popped. The ex handler will push
-       the final <CR> through handleKey, which chooses ProcessExEntryAction. Because we're not expecting EX_STRING,
-       this branch does NOT fire, and ProcessExEntryAction handles the ex cmd line entry.
-     * When pressing '/' or '?', SearchEntry(Fwd|Rev)Action is chosen as the command. This expects an argument of
-       EX_STRING, so startWaitingForArgument calls ProcessGroup#startSearchCommand. The ex handler pushes the final
-       <CR> through handleKey, which chooses ProcessExEntryAction, and we hit this branch. We don't invoke
-       ProcessExEntryAction, but pop it, set the search text as an argument on SearchEntry(Fwd|Rev)Action and invoke
-       that instead.
-     * When using '/' or '?' as part of a motion (e.g. "d/foo"), the above happens again, and all is good. Because
-       the text has been applied as an argument on the last command, '.' will correctly repeat it.
-
-     It's hard to see how to improve this. Removing EX_STRING means starting ex input has to happen in ExEntryAction
-     and SearchEntry(Fwd|Rev)Action, and the ex command invoked in ProcessExEntryAction, but that breaks any initial
-     operator, which would be invoked first (e.g. 'd' in "d/foo").
-  */
-        LOG.trace("Processing ex_string")
-        val text = injector.processGroup.endSearchCommand()
-        commandBuilder.popCommandPart() // Pop ProcessExEntryAction
-        commandBuilder.completeCommandPart(Argument(text)) // Set search text on SearchEntry(Fwd|Rev)Action
-        lambdaEditor.mode = lambdaEditor.mode.returnTo()
-      }
-    }
-  }
-
-  private fun stopMacroRecord(node: CommandNode<LazyVimCommand>): Boolean {
-    // TODO
-//    return editorState.isRecording && node.actionHolder.getInstance() is ToggleRecordingAction
-    return injector.registerGroup.isRecording && node.actionHolder.instance.id == "VimToggleRecordingAction"
-  }
-
-  private fun startWaitingForArgument(
-    editor: VimEditor,
-    context: ExecutionContext,
-    key: Char,
-    action: EditorActionHandlerBase,
-    argument: Argument.Type,
-    keyState: KeyHandlerState,
-    editorState: VimStateMachine,
-  ) {
-    val commandBuilder = keyState.commandBuilder
-    when (argument) {
-      Argument.Type.MOTION -> {
-        if (editorState.isDotRepeatInProgress && argumentCaptured != null) {
-          commandBuilder.completeCommandPart(argumentCaptured!!)
-        }
-        editor.mode = Mode.OP_PENDING(editorState.mode.returnTo)
-      }
-
-      Argument.Type.DIGRAPH -> // Command actions represent the completion of a command. Showcmd relies on this - if the action represents a
-        // part of a command, the showcmd output is reset part way through. This means we need to special case entering
-        // digraph/literal input mode. We have an action that takes a digraph as an argument, and pushes it back through
-        // the key handler when it's complete.
-
-        // TODO
-//        if (action is InsertCompletedDigraphAction) {
-        if (action.id == "VimInsertCompletedDigraphAction") {
-          keyState.digraphSequence.startDigraphSequence()
-          setPromptCharacterEx('?')
-        } else if (action.id == "VimInsertCompletedLiteralAction") {
-          keyState.digraphSequence.startLiteralSequence()
-          setPromptCharacterEx('^')
-        }
-
-      Argument.Type.EX_STRING -> {
-        // The current Command expects an EX_STRING argument. E.g. SearchEntry(Fwd|Rev)Action. This won't execute until
-        // state hits READY. Start the ex input field, push CMD_LINE mode and wait for the argument.
-        injector.processGroup.startSearchCommand(editor, context, commandBuilder.count, key)
-        commandBuilder.commandState = CurrentCommandState.NEW_COMMAND
-        val currentMode = editorState.mode
-        check(currentMode is ReturnableFromCmd) { "Cannot enable command line mode $currentMode" }
-        editor.mode = Mode.CMD_LINE(currentMode)
-      }
-
-      else -> Unit
-    }
-
-    // Another special case. Force a mode change to update the caret shape
-    // This was a typed solution
-    // if (action is ChangeCharacterAction || action is ChangeVisualCharacterAction)
-    if (action.id == "VimChangeCharacterAction" || action.id == "VimChangeVisualCharacterAction") {
-      editor.isReplaceCharacter = true
-    }
-  }
-
-  private fun checkArgumentCompatibility(
-    expectedArgumentType: Argument.Type?,
-    action: EditorActionHandlerBase,
-  ): Boolean {
-    return !(expectedArgumentType === Argument.Type.MOTION && action.type !== Command.Type.MOTION)
   }
 
   /**
