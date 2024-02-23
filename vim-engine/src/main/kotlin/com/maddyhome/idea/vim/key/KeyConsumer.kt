e3ec9c614b50ce6c2f9797c4c87bd505b7207cb8
filipp <x@lipp.fi>
2024-02-23 13:37:35 +0200

    Add KeyConsumer
    It will help us to have a more modular KeyHandler in future (chain of different consumers)

---
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/key/KeyConsumer.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2003-2024 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.key
+
+import com.maddyhome.idea.vim.KeyHandler
+import com.maddyhome.idea.vim.KeyProcessResult
+import com.maddyhome.idea.vim.api.VimEditor
+import javax.swing.KeyStroke
+
+public interface KeyConsumer {
+  public fun consumeKey(
+    key: KeyStroke,
+    editor: VimEditor,
+    allowKeyMappings: Boolean,
+    mappingCompleted: Boolean,
+    keyProcessResultBuilder: KeyProcessResult.KeyProcessResultBuilder,
+    shouldRecord: KeyHandler.MutableBoolean,
+  ): Boolean
+}
