76e8fd69bf47f0050712c46744a1ccd371419434
Alex Plate <aleksei.plate@jetbrains.com>
2024-02-27 11:30:35 +0200

    Increase timeout in UI tests

--- tests/ui-fixtures/src/testFixtures/kotlin/ui/pages/ActionMenuFixture.kt
+++ tests/ui-fixtures/src/testFixtures/kotlin/ui/pages/ActionMenuFixture.kt
@@ -14,10 +14,11 @@ import com.intellij.remoterobot.fixtures.ComponentFixture
 import com.intellij.remoterobot.fixtures.FixtureName
 import com.intellij.remoterobot.search.locators.byXpath
 import com.intellij.remoterobot.utils.waitFor
+import java.time.Duration
 
 fun RemoteRobot.actionMenu(text: String): ActionMenuFixture {
   val xpath = byXpath("text '$text'", "//div[@class='ActionMenu' and @text='$text']")
-  waitFor {
+  waitFor(duration = Duration.ofMinutes(1)) {
     findAll<ActionMenuFixture>(xpath).isNotEmpty()
   }
   return findAll<ActionMenuFixture>(xpath).first()
@@ -25,7 +26,7 @@ fun RemoteRobot.actionMenu(text: String): ActionMenuFixture {
 
 fun RemoteRobot.actionMenuItem(text: String): ActionMenuItemFixture {
   val xpath = byXpath("text '$text'", "//div[@class='ActionMenuItem' and @text='$text']")
-  waitFor {
+  waitFor(duration = Duration.ofMinutes(1)) {
     findAll<ActionMenuItemFixture>(xpath).isNotEmpty()
   }
   return findAll<ActionMenuItemFixture>(xpath).first()
