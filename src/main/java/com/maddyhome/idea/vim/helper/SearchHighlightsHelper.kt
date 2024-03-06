43970a8407daf19fa981e12d2ad6458253eaac6e
Matt Ellis <m.t.ellis@gmail.com>
2024-03-06 00:38:18 +0000

    Fix wrong offset returned for current search result
    Fixes VIM-2779

--- src/main/java/com/maddyhome/idea/vim/helper/SearchHighlightsHelper.kt
+++ src/main/java/com/maddyhome/idea/vim/helper/SearchHighlightsHelper.kt
@@ -21,6 +21,7 @@ import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.ui.ColorUtil
+import com.maddyhome.idea.vim.api.VimEditor
 import com.maddyhome.idea.vim.api.globalOptions
 import com.maddyhome.idea.vim.api.injector
 import com.maddyhome.idea.vim.api.options
@@ -28,6 +29,7 @@ import com.maddyhome.idea.vim.common.TextRange
 import com.maddyhome.idea.vim.ex.ranges.LineRange
 import com.maddyhome.idea.vim.newapi.IjVimDocument
 import com.maddyhome.idea.vim.newapi.IjVimEditor
+import com.maddyhome.idea.vim.newapi.ij
 import com.maddyhome.idea.vim.newapi.vim
 import org.jetbrains.annotations.Contract
 import java.awt.Color
@@ -40,7 +42,7 @@ internal fun updateSearchHighlights(
   showHighlights: Boolean,
   forceUpdate: Boolean,
 ) {
-  updateSearchHighlights(pattern, shouldIgnoreSmartCase, showHighlights, -1, null, true, forceUpdate)
+  updateSearchHighlights(null, pattern, shouldIgnoreSmartCase, showHighlights, -1, null, true, forceUpdate)
 }
 
 internal fun updateIncsearchHighlights(
@@ -50,10 +52,18 @@ internal fun updateIncsearchHighlights(
   caretOffset: Int,
   searchRange: LineRange?,
 ): Int {
-  val searchStartOffset =
-    if (searchRange != null) editor.vim.getLineStartOffset(searchRange.startLine) else caretOffset
+  val searchStartOffset = if (searchRange != null) editor.vim.getLineStartOffset(searchRange.startLine) else caretOffset
   val showHighlights = injector.options(editor.vim).hlsearch
-  return updateSearchHighlights(pattern, false, showHighlights, searchStartOffset, searchRange, forwards, false)
+  return updateSearchHighlights(
+    editor.vim,
+    pattern,
+    false,
+    showHighlights,
+    searchStartOffset,
+    searchRange,
+    forwards,
+    false
+  )
 }
 
 internal fun addSubstitutionConfirmationHighlight(editor: Editor, start: Int, end: Int): RangeHighlighter {
@@ -77,6 +87,7 @@ internal fun addSubstitutionConfirmationHighlight(editor: Editor, start: Int, en
  * Refreshes current search highlights for all editors of currently active text editor/document
  */
 private fun updateSearchHighlights(
+  currentEditor: VimEditor?,
   pattern: String?,
   shouldIgnoreSmartCase: Boolean,
   showHighlights: Boolean,
@@ -85,8 +96,8 @@ private fun updateSearchHighlights(
   forwards: Boolean,
   forceUpdate: Boolean,
 ): Int {
-  var currentMatchOffset = -1
-  val projectManager = ProjectManager.getInstanceIfCreated() ?: return currentMatchOffset
+  var currentEditorCurrentMatchOffset = -1
+  val projectManager = ProjectManager.getInstanceIfCreated() ?: return currentEditorCurrentMatchOffset
 
   // TODO: This implementation needs rethinking
   // It's a bit weird that we update search highlights across all open projects. It would make more sense to treat top
@@ -105,6 +116,8 @@ private fun updateSearchHighlights(
         continue
       }
 
+      var currentMatchOffset = -1
+
       // Try to keep existing highlights if possible. Update if hlsearch has changed or if the pattern has changed.
       // Force update for the situations where the text is the same, but the ignore case values have changed.
       // E.g. Use `*` to search for a word (which ignores smartcase), then use `/<Up>` to search for the same pattern,
@@ -122,7 +135,9 @@ private fun updateSearchHighlights(
         val results =
           injector.searchHelper.findAll(IjVimEditor(editor), pattern, startLine, endLine, shouldIgnoreCase(pattern, shouldIgnoreSmartCase))
         if (results.isNotEmpty()) {
-          currentMatchOffset = findClosestMatch(editor, results, initialOffset, forwards)
+          if (editor === currentEditor?.ij) {
+            currentMatchOffset = findClosestMatch(editor, results, initialOffset, forwards)
+          }
           highlightSearchResults(editor, pattern, results, currentMatchOffset)
         }
         editor.vimLastSearch = pattern
@@ -136,7 +151,9 @@ private fun updateSearchHighlights(
         if (!forwards) searchOptions.add(SearchOptions.BACKWARDS)
         val result = injector.searchHelper.findPattern(IjVimEditor(editor), pattern, initialOffset, 1, searchOptions)
         if (result != null) {
-          currentMatchOffset = result.startOffset
+          if (editor === currentEditor?.ij) {
+            currentMatchOffset = result.startOffset
+          }
           val results = listOf(result)
           highlightSearchResults(editor, pattern, results, currentMatchOffset)
         }
@@ -144,13 +161,18 @@ private fun updateSearchHighlights(
         // incsearch. If nothing has changed (e.g. we've edited offset values in `/foo/e+2`) make sure we return the
         // current match offset so the caret remains at the current incsarch match
         val offset = editor.vimIncsearchCurrentMatchOffset
-        if (offset != null) {
+        if (offset != null && editor === currentEditor?.ij) {
           currentMatchOffset = offset
         }
       }
+
+      if (editor === currentEditor?.ij) {
+        currentEditorCurrentMatchOffset = currentMatchOffset
+      }
     }
   }
-  return currentMatchOffset
+
+  return currentEditorCurrentMatchOffset
 }
 
 /**
