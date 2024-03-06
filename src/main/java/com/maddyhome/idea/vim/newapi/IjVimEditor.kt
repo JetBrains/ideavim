7652b16ca6d69c3c97509a9c37e6ec6b4060e802
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2024-03-06 23:03:26 +0200

    Move more MotionGroup methods to its base class

--- src/main/java/com/maddyhome/idea/vim/newapi/IjVimEditor.kt
+++ src/main/java/com/maddyhome/idea/vim/newapi/IjVimEditor.kt
@@ -35,6 +35,7 @@ import com.maddyhome.idea.vim.api.VimScrollingModel
 import com.maddyhome.idea.vim.api.VimSelectionModel
 import com.maddyhome.idea.vim.api.VimVisualPosition
 import com.maddyhome.idea.vim.api.VirtualFile
+import com.maddyhome.idea.vim.api.injector
 import com.maddyhome.idea.vim.command.OperatorArguments
 import com.maddyhome.idea.vim.common.EditorLine
 import com.maddyhome.idea.vim.common.IndentConfig
@@ -358,7 +359,7 @@ internal class IjVimEditor(editor: Editor) : MutableLinearEditor() {
     return EditorHelper.getVirtualFile(editor)?.getUrl()?.let { VirtualFileManager.extractProtocol(it) }
   }
 
-  override val projectId = editor.project?.basePath ?: DEFAULT_PROJECT_ID
+  override val projectId = editor.project?.let { injector.file.getProjectId(it) } ?: DEFAULT_PROJECT_ID
 
   override fun visualPositionToOffset(position: VimVisualPosition): Offset {
     return editor.visualPositionToOffset(VisualPosition(position.line, position.column, position.leansRight)).offset
