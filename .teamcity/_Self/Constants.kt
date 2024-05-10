5a82f05be848b3d12e4c3fde9a960cfcb5a4f49b
Alex Plate <aleksei.plate@jetbrains.com>
2024-05-10 15:46:31 +0300

    Update version of IJ in TC tests

--- .teamcity/_Self/Constants.kt
+++ .teamcity/_Self/Constants.kt
@@ -5,13 +5,13 @@ object Constants {
   const val EAP_CHANNEL = "eap"
   const val DEV_CHANNEL = "Dev"
 
-  const val GITHUB_TESTS = "2023.3.2"
-  const val NVIM_TESTS = "2023.3.2"
-  const val PROPERTY_TESTS = "2023.3.2"
-  const val LONG_RUNNING_TESTS = "2023.3.2"
-  const val QODANA_TESTS = "2023.3.2"
-  const val RELEASE = "2023.3.2"
+  const val GITHUB_TESTS = "2024.1.1"
+  const val NVIM_TESTS = "2024.1.1"
+  const val PROPERTY_TESTS = "2024.1.1"
+  const val LONG_RUNNING_TESTS = "2024.1.1"
+  const val QODANA_TESTS = "2024.1.1"
+  const val RELEASE = "2024.1.1"
 
-  const val RELEASE_DEV = "2023.3.2"
-  const val RELEASE_EAP = "2023.3.2"
+  const val RELEASE_DEV = "2024.1.1"
+  const val RELEASE_EAP = "2024.1.1"
 }
