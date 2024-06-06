64f7859ba7a44ae049a258f53d86562b4d45b5b2
Matt Ellis <m.t.ellis@gmail.com>
2024-06-06 08:46:30 +0100

    Fix switching to insert for non-file backed diff
    Fixes VIM-3442

--- src/main/java/com/maddyhome/idea/vim/group/EditorGroup.java
+++ src/main/java/com/maddyhome/idea/vim/group/EditorGroup.java
@@ -217,22 +217,23 @@ public class EditorGroup implements PersistentStateComponent<Element>, VimEditor
       editorEx.addPropertyChangeListener(FontSizeChangeListener.INSTANCE);
     }
 
-    // We add Vim bindings to all opened editors, even read-only editors. We also add bindings to editors that are used
-    // elsewhere in the IDE, rather than just for editing project files. This includes editors used as part of the UI,
-    // such as the VCS commit message, or used as read-only viewers for text output, such as log files in run
-    // configurations or the Git Console tab. And editors are used for interactive stdin/stdout for console-based run
-    // configurations.
+    // We add Vim bindings to all opened editors, including editors used as UI controls rather than just project file
+    // editors. This includes editors used as part of the UI, such as the VCS commit message, or used as read-only
+    // viewers for text output, such as log files in run configurations or the Git Console tab. And editors are used for
+    // interactive stdin/stdout for console-based run configurations.
     // We want to provide an intuitive experience for working with these additional editors, so we automatically switch
-    // to INSERT mode for interactive editors. Recognising these can be a bit tricky.
+    // to INSERT mode if they are interactive editors. Recognising these can be a bit tricky.
     // These additional interactive editors are not file-based, but must have a writable document. However, log output
     // documents are also writable (the IDE is writing new content as it becomes available) just not user-editable. So
     // we must also check that the editor is not in read-only "viewer" mode (this includes "rendered" mode, which is
     // read-only and also hides the caret).
-    // Furthermore, the interactive stdin/stdout console output is hosted in a read-only editor, but it can still be
-    // edited. The `ConsoleViewImpl` class installs a typing handler that ignores the editor's `isViewer` property and
-    // allows typing if the associated process (if any) is still running. We can get the editor's console view and check
-    // this ourselves, but we have to wait until the editor has finished initialising before it's available in user
-    // data.
+    // Furthermore, interactive stdin/stdout console output in run configurations is hosted in a read-only editor, but
+    // it can still be edited. The `ConsoleViewImpl` class installs a typing handler that ignores the editor's
+    // `isViewer` property and allows typing if the associated process (if any) is still running. We can get the
+    // editor's console view and check this ourselves, but we have to wait until the editor has finished initialising
+    // before it's available in user data.
+    // Finally, we have a special check for diff windows. If we compare against clipboard, we get a diff editor that is
+    // not file based, is writable, and not a viewer, but we don't want to treat this as an interactive editor.
     // Note that we need a similar check in `VimEditor.isWritable` to allow Escape to work to exit insert mode. We need
     // to know that a read-only editor that is hosting a console view with a running process can be treated as writable.
     Runnable switchToInsertMode = () -> {
@@ -243,7 +244,8 @@ public class EditorGroup implements PersistentStateComponent<Element>, VimEditor
     if (!editor.isViewer() &&
         !EditorHelper.isFileEditor(editor) &&
         editor.getDocument().isWritable() &&
-        !CommandStateHelper.inInsertMode(editor)) {
+        !CommandStateHelper.inInsertMode(editor) &&
+        editor.getEditorKind() != EditorKind.DIFF) {
       switchToInsertMode.run();
     }
     ApplicationManager.getApplication().invokeLater(
