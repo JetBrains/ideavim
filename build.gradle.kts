5eb0fae08f6589e54bb796e995e62665316acb43
Matt Ellis <m.t.ellis@gmail.com>
2024-06-12 18:07:20 +0100

    Remove explicit since-build from plugin.xml
    Let the Gradle plugin manage since-build, based on our target compile version. We leave until-build open ended

--- build.gradle.kts
+++ build.gradle.kts
@@ -292,10 +292,13 @@ intellijPlatform {
     )
 
     ideaVersion {
-      // Set the since-build value, but leave until-build open ended (default is MAJOR.*)
-      // Don't forget to update plugin.xml
-      // TODO: Do we need this to be here *and* in plugin.xml?
-      sinceBuild.set("241.15989.150")
+      // Let the Gradle plugin set the since-build version. It defaults to the version of the IDE we're building against
+      // specified as two components, `{branch}.{build}` (e.g., "241.15989"). There is no third component specified.
+      // The until-build version defaults to `{branch}.*`, but we want to support _all_ future versions, so we set it
+      // with a null provider (the provider is important).
+      // By letting the Gradle plugin handle this, the Plugin DevKit IntelliJ plugin cannot help us with the "Usage of
+      // IntelliJ API not available in older IDEs" inspection. However, since our since-build is the version we compile
+      // against, we can never get an API that's newer - it would be an unresolved symbol.
       untilBuild.set(provider { null })
     }
   }
