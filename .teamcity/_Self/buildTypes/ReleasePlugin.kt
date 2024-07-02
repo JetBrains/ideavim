e67c71e4407a93589786aac172e33cd9a3ca32be
Alex Plate <aleksei.plate@jetbrains.com>
2024-07-02 10:05:23 +0300

    Temporally comment out release branch reset

--- .teamcity/_Self/buildTypes/ReleasePlugin.kt
+++ .teamcity/_Self/buildTypes/ReleasePlugin.kt
@@ -67,26 +67,26 @@ sealed class ReleasePlugin(private val releaseType: String) : IdeaVimBuildType({
       name = "Pull git history"
       scriptContent = "git fetch --unshallow"
     }
-    script {
-      name = "Reset release branch"
-      scriptContent = """
-        if [ "major" = "$releaseType" ] || [ "minor" = "$releaseType" ]
-        then
-          echo Resetting the release branch because the release type is $releaseType
-          git checkout master
-          latest_eap=${'$'}(git describe --tags --match="[0-9].[0-9]*.[0-9]-eap.[0-9]*" --abbrev=0 HEAD)
-          echo Latest EAP: ${'$'}latest_eap
-          commit_of_latest_eap=${'$'}(git rev-list -n 1 ${'$'}latest_eap)
-          echo Commit of latest EAP: ${'$'}commit_of_latest_eap
-          git checkout release
-          git reset --hard ${'$'}commit_of_latest_eap
-        else
-          git checkout release
-          echo Do not reset the release branch because the release type is $releaseType
-        fi
-        echo Checked out release branch
-      """.trimIndent()
-    }
+//    script {
+//      name = "Reset release branch"
+//      scriptContent = """
+//        if [ "major" = "$releaseType" ] || [ "minor" = "$releaseType" ]
+//        then
+//          echo Resetting the release branch because the release type is $releaseType
+//          git checkout master
+//          latest_eap=${'$'}(git describe --tags --match="[0-9].[0-9]*.[0-9]-eap.[0-9]*" --abbrev=0 HEAD)
+//          echo Latest EAP: ${'$'}latest_eap
+//          commit_of_latest_eap=${'$'}(git rev-list -n 1 ${'$'}latest_eap)
+//          echo Commit of latest EAP: ${'$'}commit_of_latest_eap
+//          git checkout release
+//          git reset --hard ${'$'}commit_of_latest_eap
+//        else
+//          git checkout release
+//          echo Do not reset the release branch because the release type is $releaseType
+//        fi
+//        echo Checked out release branch
+//      """.trimIndent()
+//    }
     gradle {
       name = "Calculate new version"
       tasks = "scripts:calculateNewVersion"
