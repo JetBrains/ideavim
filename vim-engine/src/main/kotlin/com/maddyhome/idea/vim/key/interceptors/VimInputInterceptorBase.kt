149edefad5e53fddae22a83a614fc7755c695b6f
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2024-07-10 00:14:59 +0300

    Delegate handling closing keystrokes to implementations
    Sometimes it may mean aborting interception (CTRL-C), sometimes finishing it (CR)

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/key/interceptors/VimInputInterceptorBase.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/key/interceptors/VimInputInterceptorBase.kt
@@ -11,21 +11,15 @@ package com.maddyhome.idea.vim.key.interceptors
 import com.maddyhome.idea.vim.api.ExecutionContext
 import com.maddyhome.idea.vim.api.VimEditor
 import com.maddyhome.idea.vim.api.injector
-import com.maddyhome.idea.vim.helper.isCloseKeyStroke
-import java.awt.event.KeyEvent
 import javax.swing.KeyStroke
 
+// TODO make it safer to utilise key.isCloseKeystroke and similar logic
 abstract class VimInputInterceptorBase<T> : VimInputInterceptor<T> {
   override fun consumeKey(
     key: KeyStroke,
     editor: VimEditor,
     context: ExecutionContext
   ) {
-    if (key.isCloseKeyStroke() || key.keyCode == KeyEvent.VK_ENTER) {
-      closeModalInputPrompt()
-      return
-    }
-
     val completeInput = buildInput(key) ?: return
     executeInput(completeInput, editor, context)
     onFinish()
