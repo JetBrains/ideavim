00fd4cd491e940d37a2e36b92c2a54e67a12e1b0
Matt Ellis <m.t.ellis@gmail.com>
2024-08-11 01:01:45 +0100

    Handle op-pending for space and backspace

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/motion/leftright/MotionSpaceAction.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/motion/leftright/MotionSpaceAction.kt
@@ -1,10 +1,11 @@
 /*
- * Copyright 2003-2023 The IdeaVim authors
+ * Copyright 2003-2024 The IdeaVim authors
  *
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE.txt file or at
  * https://opensource.org/licenses/MIT.
  */
+
 package com.maddyhome.idea.vim.action.motion.leftright
 
 import com.intellij.vim.annotations.CommandOrMotion
@@ -20,8 +21,8 @@ import com.maddyhome.idea.vim.command.OperatorArguments
 import com.maddyhome.idea.vim.handler.Motion
 import com.maddyhome.idea.vim.handler.MotionActionHandler
 
-@CommandOrMotion(keys = ["<BS>", "<C-H>"], modes = [Mode.NORMAL, Mode.VISUAL])
-open class MotionBackspaceAction(private val allowPastEnd: Boolean = false) : MotionActionHandler.ForEachCaret() {
+@CommandOrMotion(keys = ["<Space>"], modes = [Mode.NORMAL, Mode.VISUAL])
+open class MotionSpaceAction(private val allowPastEnd: Boolean = false) : MotionActionHandler.ForEachCaret() {
   override fun getOffset(
     editor: VimEditor,
     caret: ImmutableVimCaret,
@@ -29,16 +30,12 @@ open class MotionBackspaceAction(private val allowPastEnd: Boolean = false) : Mo
     argument: Argument?,
     operatorArguments: OperatorArguments,
   ): Motion {
-    val allowWrap = injector.options(editor).whichwrap.contains("b")
-    return injector.motion.getHorizontalMotion(editor, caret, -operatorArguments.count1, allowPastEnd, allowWrap)
+    val allowWrap = injector.options(editor).whichwrap.contains("s")
+    return injector.motion.getHorizontalMotion(editor, caret, operatorArguments.count1, allowPastEnd, allowWrap)
   }
 
   override val motionType: MotionType = MotionType.EXCLUSIVE
 }
 
-// When the motion is used with an operator, the EOL character is counted.
-// This allows e.g., `d<BS>` to delete the end of line character on the previous line when wrap is active
-// ('whichwrap' contains "b")
-// See `:help whichwrap`. This says a delete or change operator, but it appears to apply to all operators
-@CommandOrMotion(keys = ["<BS>", "<C-H>"], modes = [Mode.OP_PENDING])
-class MotionBackspaceOpPendingModeAction : MotionBackspaceAction(allowPastEnd = true)
+@CommandOrMotion(keys = ["<Space>"], modes = [Mode.OP_PENDING])
+class MotionSpaceOpPendingModeAction : MotionSpaceAction(allowPastEnd = true)
