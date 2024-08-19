ff4eb31418cf58126257e1b9314b2aa41a539935
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2024-08-19 05:26:05 +0300

    Support <Left> in command mode

---
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/ex/MoveCaretLeftAction.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2003-2024 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.action.ex
+
+import com.intellij.vim.annotations.CommandOrMotion
+import com.intellij.vim.annotations.Mode
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.api.injector
+import com.maddyhome.idea.vim.command.Command
+import com.maddyhome.idea.vim.command.OperatorArguments
+import com.maddyhome.idea.vim.handler.VimActionHandler
+
+@CommandOrMotion(keys = ["<Left>"], modes = [Mode.CMD_LINE])
+class MoveCaretLeftAction : VimActionHandler.SingleExecution()  {
+  override val type: Command.Type = Command.Type.OTHER_SELF_SYNCHRONIZED
+
+  override fun execute(editor: VimEditor, context: ExecutionContext, cmd: Command, operatorArguments: OperatorArguments): Boolean {
+    val commandLine = injector.commandLine.getActiveCommandLine() ?: return false
+    val caret = commandLine.caret
+
+    if (caret.offset == 0) return true
+    caret.offset -= 1
+
+    return true
+  }
+}
