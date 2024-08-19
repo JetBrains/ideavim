900eeceb175d03e6306fc5f7c5c2c56126dcbafb
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2024-08-19 05:29:55 +0300

    Fix unwanted beep when can't perform an action

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/ex/DeleteNextCharAction.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/ex/DeleteNextCharAction.kt
@@ -26,7 +26,7 @@ class DeleteNextCharAction : VimActionHandler.SingleExecution()  {
     val caretOffset = commandLine.caret.offset
 
     val oldText = commandLine.actualText
-    if (oldText.isEmpty()) return false
+    if (oldText.isEmpty()) return true
 
     val newText = if (caretOffset == oldText.length) {
       oldText.substring(0, oldText.lastIndex)
