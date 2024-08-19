3d03494354b7bbb1532456d0f684ec61795b65a7
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2024-08-19 04:58:17 +0300

    Support <DEL> in command mode

---
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/ex/DeleteNextCharAction.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2003-2024 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.action.ex
+
+import com.intellij.vim.annotations.CommandOrMotion
+import com.intellij.vim.annotations.Mode
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.api.injector
+import com.maddyhome.idea.vim.command.Command
+import com.maddyhome.idea.vim.command.OperatorArguments
+import com.maddyhome.idea.vim.handler.VimActionHandler
+
+@CommandOrMotion(keys = ["<DEL>"], modes = [Mode.CMD_LINE])
+class DeleteNextCharAction : VimActionHandler.SingleExecution()  {
+  override val type: Command.Type = Command.Type.OTHER_SELF_SYNCHRONIZED
+
+  override fun execute(editor: VimEditor, context: ExecutionContext, cmd: Command, operatorArguments: OperatorArguments): Boolean {
+    val commandLine = injector.commandLine.getActiveCommandLine() ?: return false
+    val caretOffset = commandLine.caret.offset
+
+    val oldText = commandLine.actualText
+    if (oldText.isEmpty()) return false
+
+    val newText = if (caretOffset == oldText.length) {
+      oldText.substring(0, oldText.lastIndex)
+    } else {
+      oldText.substring(0, caretOffset) + oldText.substring(caretOffset + 1)
+    }
+    commandLine.setText(newText)
+
+    return true
+  }
+}
