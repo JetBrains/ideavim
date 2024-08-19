7bcfcd9a561efef511a3a6ff4c26198bb798efee
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2024-08-20 02:17:18 +0300

    Move some Executor logic to its base class

--- src/main/java/com/maddyhome/idea/vim/vimscript/Executor.kt
+++ src/main/java/com/maddyhome/idea/vim/vimscript/Executor.kt
@@ -8,131 +8,25 @@
 
 package com.maddyhome.idea.vim.vimscript
 
-import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.vfs.VirtualFileManager
-import com.maddyhome.idea.vim.VimPlugin
-import com.maddyhome.idea.vim.api.ExecutionContext
-import com.maddyhome.idea.vim.api.VimEditor
 import com.maddyhome.idea.vim.api.VimScriptExecutorBase
-import com.maddyhome.idea.vim.api.injector
-import com.maddyhome.idea.vim.ex.ExException
-import com.maddyhome.idea.vim.ex.FinishException
 import com.maddyhome.idea.vim.extension.VimExtensionRegistrar
-import com.maddyhome.idea.vim.history.HistoryConstants
-import com.maddyhome.idea.vim.newapi.vim
-import com.maddyhome.idea.vim.register.RegisterConstants.LAST_COMMAND_REGISTER
-import com.maddyhome.idea.vim.vimscript.model.CommandLineVimLContext
-import com.maddyhome.idea.vim.vimscript.model.ExecutionResult
-import com.maddyhome.idea.vim.vimscript.model.VimLContext
-import com.maddyhome.idea.vim.vimscript.model.commands.Command
-import com.maddyhome.idea.vim.vimscript.model.commands.RepeatCommand
-import com.maddyhome.idea.vim.vimscript.parser.VimscriptParser
 import java.io.File
-import java.io.IOException
 
 @Service
 internal class Executor : VimScriptExecutorBase() {
-  private val logger = logger<Executor>()
-  override var executingVimscript = false
-  override var executingIdeaVimRcConfiguration = false
-
-  @Throws(ExException::class)
-  override fun execute(script: String, editor: VimEditor, context: ExecutionContext, skipHistory: Boolean, indicateErrors: Boolean, vimContext: VimLContext?): ExecutionResult {
-    try {
-      injector.vimscriptExecutor.executingVimscript = true
-      var finalResult: ExecutionResult = ExecutionResult.Success
-
-      val myScript = VimscriptParser.parse(script)
-      myScript.units.forEach { it.vimContext = vimContext ?: myScript }
-
-      for (unit in myScript.units) {
-        try {
-          val result = unit.execute(editor, context)
-          if (result is ExecutionResult.Error) {
-            finalResult = ExecutionResult.Error
-            if (indicateErrors) {
-              VimPlugin.indicateError()
-            }
-          }
-        } catch (e: ExException) {
-          if (e is FinishException) {
-            break
-          }
-          finalResult = ExecutionResult.Error
-          if (indicateErrors) {
-            VimPlugin.showMessage(e.message)
-            VimPlugin.indicateError()
-          } else {
-            logger.warn("Failed while executing $unit. " + e.message)
-          }
-        } catch (e: NotImplementedError) {
-          if (indicateErrors) {
-            VimPlugin.showMessage("Not implemented yet :(")
-            VimPlugin.indicateError()
-          }
-        } catch (e: Exception) {
-          logger.warn(e)
-          if (injector.application.isUnitTest()) {
-            throw e
-          }
-        }
-      }
-
-      if (!skipHistory) {
-        VimPlugin.getHistory().addEntry(HistoryConstants.COMMAND, script)
-        if (myScript.units.size == 1 && myScript.units[0] is Command && myScript.units[0] !is RepeatCommand) {
-          VimPlugin.getRegister().storeTextSpecial(LAST_COMMAND_REGISTER, script)
-        }
-      }
-      return finalResult
-    } finally {
-      injector.vimscriptExecutor.executingVimscript = false
-
-      // Initialize any extensions that were enabled during execution of this vimscript
-      // See the doc of this function for details
-      VimExtensionRegistrar.enableDelayedExtensions()
-    }
+  override fun enableDelayedExtensions() {
+    VimExtensionRegistrar.enableDelayedExtensions()
   }
 
-  override fun executeFile(file: File, editor: VimEditor, fileIsIdeaVimRcConfig: Boolean, indicateErrors: Boolean) {
-    val context = DataContext.EMPTY_CONTEXT.vim
-    try {
-      if (fileIsIdeaVimRcConfig) {
-        injector.vimscriptExecutor.executingIdeaVimRcConfiguration = true
-      }
-      ensureFileIsSaved(file)
-      execute(file.readText(), editor, context, skipHistory = true, indicateErrors)
-    } catch (ignored: IOException) {
-      LOG.error(ignored)
-    } finally {
-      if (fileIsIdeaVimRcConfig) {
-        injector.vimrcFileState.saveFileState(file.absolutePath)
-        injector.vimscriptExecutor.executingIdeaVimRcConfiguration = false
-      }
-    }
-  }
-
-  private fun ensureFileIsSaved(file: File) {
+  override fun ensureFileIsSaved(file: File) {
     val documentManager = FileDocumentManager.getInstance()
 
     VirtualFileManager.getInstance().findFileByNioPath(file.toPath())
       ?.let(documentManager::getCachedDocument)
       ?.takeIf(documentManager::isDocumentUnsaved)
       ?.let(documentManager::saveDocumentAsIs)
-  }
-
-  @Throws(ExException::class)
-  override fun executeLastCommand(editor: VimEditor, context: ExecutionContext): Boolean {
-    val reg = VimPlugin.getRegister().getRegister(':') ?: return false
-    val text = reg.text ?: return false
-    execute(text, editor, context, skipHistory = false, indicateErrors = true, CommandLineVimLContext)
-    return true
-  }
-
-  companion object {
-    val LOG = logger<Executor>()
   }
 }
--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimScriptExecutorBase.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimScriptExecutorBase.kt
@@ -8,4 +8,110 @@
 
 package com.maddyhome.idea.vim.api
 
-abstract class VimScriptExecutorBase : VimscriptExecutor
+import com.maddyhome.idea.vim.diagnostic.vimLogger
+import com.maddyhome.idea.vim.ex.ExException
+import com.maddyhome.idea.vim.ex.FinishException
+import com.maddyhome.idea.vim.history.HistoryConstants
+import com.maddyhome.idea.vim.history.VimHistory
+import com.maddyhome.idea.vim.register.RegisterConstants.LAST_COMMAND_REGISTER
+import com.maddyhome.idea.vim.vimscript.model.CommandLineVimLContext
+import com.maddyhome.idea.vim.vimscript.model.ExecutionResult
+import com.maddyhome.idea.vim.vimscript.model.VimLContext
+import com.maddyhome.idea.vim.vimscript.model.commands.Command
+import com.maddyhome.idea.vim.vimscript.model.commands.RepeatCommand
+import java.io.File
+import java.io.IOException
+
+abstract class VimScriptExecutorBase : VimscriptExecutor {
+  private val logger = vimLogger<VimScriptExecutorBase>()
+  override var executingVimscript = false
+  override var executingIdeaVimRcConfiguration = false
+
+  @Throws(ExException::class)
+  override fun execute(script: String, editor: VimEditor, context: ExecutionContext, skipHistory: Boolean, indicateErrors: Boolean, vimContext: VimLContext?): ExecutionResult {
+    try {
+      injector.vimscriptExecutor.executingVimscript = true
+      var finalResult: ExecutionResult = ExecutionResult.Success
+
+      val myScript = injector.vimscriptParser.parse(script)
+      myScript.units.forEach { it.vimContext = vimContext ?: myScript }
+
+      for (unit in myScript.units) {
+        try {
+          val result = unit.execute(editor, context)
+          if (result is ExecutionResult.Error) {
+            finalResult = ExecutionResult.Error
+            if (indicateErrors) {
+              injector.messages.indicateError()
+            }
+          }
+        } catch (e: ExException) {
+          if (e is FinishException) {
+            break
+          }
+          finalResult = ExecutionResult.Error
+          if (indicateErrors) {
+            injector.messages.showStatusBarMessage(editor, e.message)
+            injector.messages.indicateError()
+          } else {
+            logger.warn("Failed while executing $unit. " + e.message)
+          }
+        } catch (e: NotImplementedError) {
+          if (indicateErrors) {
+            injector.messages.showStatusBarMessage(editor, "Not implemented yet :(")
+            injector.messages.indicateError()
+          }
+        } catch (e: Exception) {
+          logger.warn(e.toString())
+          if (injector.application.isUnitTest()) {
+            throw e
+          }
+        }
+      }
+
+      if (!skipHistory) {
+        injector.historyGroup.addEntry(VimHistory.Type.Command, script)
+        if (myScript.units.size == 1 && myScript.units[0] is Command && myScript.units[0] !is RepeatCommand) {
+          injector.registerGroup.storeTextSpecial(LAST_COMMAND_REGISTER, script)
+        }
+      }
+      return finalResult
+    } finally {
+      injector.vimscriptExecutor.executingVimscript = false
+
+      // Initialize any extensions that were enabled during execution of this vimscript
+      // See the doc of this function for details
+      enableDelayedExtensions()
+    }
+  }
+
+  protected abstract fun enableDelayedExtensions()
+
+  override fun executeFile(file: File, editor: VimEditor, fileIsIdeaVimRcConfig: Boolean, indicateErrors: Boolean) {
+    val context = injector.executionContextManager.getEditorExecutionContext(editor)
+    try {
+      if (fileIsIdeaVimRcConfig) {
+        injector.vimscriptExecutor.executingIdeaVimRcConfiguration = true
+      }
+      ensureFileIsSaved(file)
+      execute(file.readText(), editor, context, skipHistory = true, indicateErrors)
+    } catch (ignored: IOException) {
+      logger.error(ignored.toString())
+    } finally {
+      if (fileIsIdeaVimRcConfig) {
+        injector.vimrcFileState.saveFileState(file.absolutePath)
+        injector.vimscriptExecutor.executingIdeaVimRcConfiguration = false
+      }
+    }
+  }
+
+  protected abstract fun ensureFileIsSaved(file: File)
+
+  @Throws(ExException::class)
+  override fun executeLastCommand(editor: VimEditor, context: ExecutionContext): Boolean {
+    val reg = injector.registerGroup.getRegister(':') ?: return false
+    val text = reg.text ?: return false
+    execute(text, editor, context, skipHistory = false, indicateErrors = true, CommandLineVimLContext)
+    return true
+  }
+}
