93e6adf5a9dd2b2590c08a9861520e49878e4fa4
Filipp Vakhitov <filipp.vakhitov@jetbrains.com>
2024-08-19 19:40:02 +0300

    Support <C-W> in command mode

---
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/ex/DeletePrevWordAction.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2003-2024 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.action.ex
+
+import com.intellij.vim.annotations.CommandOrMotion
+import com.intellij.vim.annotations.Mode
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.api.injector
+import com.maddyhome.idea.vim.command.Command
+import com.maddyhome.idea.vim.command.OperatorArguments
+import com.maddyhome.idea.vim.handler.Motion
+import com.maddyhome.idea.vim.handler.VimActionHandler
+
+@CommandOrMotion(keys = ["<C-W>"], modes = [Mode.CMD_LINE])
+class DeletePrevWordAction : VimActionHandler.SingleExecution()  {
+  override val type: Command.Type = Command.Type.OTHER_SELF_SYNCHRONIZED
+
+  override fun execute(editor: VimEditor, context: ExecutionContext, cmd: Command, operatorArguments: OperatorArguments): Boolean {
+    val commandLine = injector.commandLine.getActiveCommandLine() ?: return false
+
+    val caretOffset = commandLine.caret.offset
+    if (caretOffset == 0) return true
+
+    val oldText = commandLine.actualText
+    val motion = injector.motion.findOffsetOfNextWord(oldText, oldText.length, commandLine.caret.offset, -1, true, editor)
+    when (motion) {
+      is Motion.AbsoluteOffset -> {
+        val newText = oldText.substring(0, motion.offset) + oldText.substring(caretOffset)
+        commandLine.caret.offset = motion.offset
+        commandLine.setText(newText)
+      }
+      else -> {}
+    }
+
+    return true
+  }
+}
