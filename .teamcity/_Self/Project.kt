6a73f9e65a2134c8129bad43a59bb975b5779212
Alex Plate <aleksei.plate@jetbrains.com>
2024-11-05 05:09:19 +0200

    Bump TC configurations to use 2024.2.1

--- .teamcity/_Self/Project.kt
+++ .teamcity/_Self/Project.kt
@@ -25,8 +25,7 @@ object Project : Project({
 
   // Active tests
   buildType(TestingBuildType("Latest EAP", "<default>", version = "LATEST-EAP-SNAPSHOT"))
-  buildType(TestingBuildType("2024.1.1", "<default>"))
-  buildType(TestingBuildType("2024.2", "<default>"))
+  buildType(TestingBuildType("2024.2.1", "<default>"))
   buildType(TestingBuildType("Latest EAP With Xorg", "<default>", version = "LATEST-EAP-SNAPSHOT"))
 
   buildType(PropertyBased)
