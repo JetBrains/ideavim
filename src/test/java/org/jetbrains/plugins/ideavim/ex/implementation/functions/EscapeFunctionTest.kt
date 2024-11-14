ba47cf40a74830b26b0ff0733735e62300a4985c
Julien Phalip <jphalip@gmail.com>
2024-11-14 13:43:28 -0800

    Add support for escape() vim function

---
+++ src/test/java/org/jetbrains/plugins/ideavim/ex/implementation/functions/EscapeFunctionTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2003-2024 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package org.jetbrains.plugins.ideavim.ex.implementation.functions
+
+import org.jetbrains.plugins.ideavim.VimTestCase
+import org.junit.jupiter.api.Test
+
+
+class EscapeFunctionTest : VimTestCase() {
+  @Test
+  fun `test escape windows path with spaces`() {
+    configureByText("\n")
+    typeText(commandToKeys("""echo escape('c:\program files\vim', ' \')"""))
+    assertExOutput("""c:\\program\ files\\vim""")
+  }
+
+  @Test
+  fun `test escape multiple special characters`() {
+    configureByText("\n")
+    typeText(commandToKeys("""echo escape('special chars: #$^', '#$^')"""))
+    assertExOutput("""special chars: \#\$\^""")
+  }
+
+  @Test
+  fun `test escape when no escaping needed`() {
+    configureByText("\n")
+    typeText(commandToKeys("""echo escape('no escaping needed', 'xyz')"""))
+    assertExOutput("no escaping needed")
+  }
+
+  @Test
+  fun `test escape empty strings`() {
+    configureByText("\n")
+    typeText(commandToKeys("""echo escape('', '')"""))
+    assertExOutput("")
+  }
+
+  @Test
+  fun `test escape consecutive special characters`() {
+    configureByText("\n")
+    typeText(commandToKeys("""echo escape('$$$$', '$')"""))
+    assertExOutput("""\$\$\$\$""")
+  }
+
+  @Test
+  fun `test escape with double backslashes`() {
+    configureByText("\n")
+    typeText(commandToKeys("""echo escape('test\\here', '\\')"""))
+    assertExOutput("""test\\\\here""")
+  }
+
+  @Test
+  fun `test escape with unicode characters`() {
+    configureByText("\n")
+    typeText(commandToKeys("""echo escape('Hello 👋 #world', '#')"""))
+    assertExOutput("""Hello 👋 \#world""")
+
+    typeText(commandToKeys("""echo escape('🎉$🎊$', '$')"""))
+    assertExOutput("""🎉\$🎊\$""")
+  }
+}
