0f1a4d523fe6857d7bb1d1d4a9979e89420b5e18
Alex Plate <aleksei.plate@jetbrains.com>
2024-11-25 14:01:23 +0200

    Move the docs to the `init` method of VimExtension

--- src/main/java/com/maddyhome/idea/vim/extension/VimExtension.java
+++ src/main/java/com/maddyhome/idea/vim/extension/VimExtension.java
@@ -27,6 +27,30 @@ public interface VimExtension {
     return MappingOwner.Plugin.Companion.get(getName());
   }
 
+  /**
+   * This method is always called AFTER the full execution of the `.ideavimrc` file.
+   * <p>
+   * During vim initialization process, it firstly loads the .vimrc file, then executes scripts from the plugins folder.
+   * This practically means that the .vimrc file is initialized first; then the plugins are loaded.
+   * See `:h initialization`
+   * <p>
+   * Why does this matter? Because this affects the order of commands are executed. For example,
+   * ```
+   * plug 'tommcdo/vim-exchange'
+   * let g:exchange_no_mappings=1
+   * ```
+   * Here the user will expect that the exchange plugin won't have default mappings. However, if we load vim-exchange
+   *    immediately, this variable won't be initialized at the moment of plugin initialization.
+   * <p>
+   * There is also a tricky case for mappings override:
+   * ```
+   * plug 'tommcdo/vim-exchange'
+   * map X <Plug>(ExchangeLine)
+   * ```
+   * For this case, a plugin with a good implementation detects that there is already a defined mapping for
+   *   `<Plug>(ExchangeLine)` and doesn't register the default cxx mapping. However, such detection requires the mapping
+   *   to be defined before the plugin initialization.
+   */
   void init();
 
   default void dispose() {
