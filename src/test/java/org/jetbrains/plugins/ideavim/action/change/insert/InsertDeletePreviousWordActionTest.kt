b6df43d774f41ffcb4139b438e6d886e3db9b2fd
Matt Ellis <m.t.ellis@gmail.com>
2025-01-15 12:16:56 +0000

    Fix end of file edge case deleting previous word
    Adds extra tests to confirm that deleting previous word in Insert mode was already working as expected (relates to VIM-1650)
    
    Also fixes an edge case for a test that was marked as behaving differently to Vim, but was actually showing buggy behaviour due to its unorthodox caret placement. It looked like the caret was placed at the end of the line, but it was actually placed _passed_ the last character in the file. Adding extra text below the caret placement would cause the action to behave as expected, and the test would then match Vim behaviour. However, it is possible to get this caret position in the editor and in Vim, with `virtualedit=onemore`, and the IdeaVim implementation was wrong. The test has been updated to provide the correct scenario, and the implementation has been fixed.

--- src/test/java/org/jetbrains/plugins/ideavim/action/change/insert/InsertDeletePreviousWordActionTest.kt
+++ src/test/java/org/jetbrains/plugins/ideavim/action/change/insert/InsertDeletePreviousWordActionTest.kt
@@ -12,7 +12,6 @@ package org.jetbrains.plugins.ideavim.action.change.insert
 
 import com.maddyhome.idea.vim.api.injector
 import com.maddyhome.idea.vim.state.mode.Mode
-import org.jetbrains.plugins.ideavim.VimBehaviorDiffers
 import org.jetbrains.plugins.ideavim.VimTestCase
 import org.junit.jupiter.api.Test
 
@@ -76,17 +75,63 @@ class InsertDeletePreviousWordActionTest : VimTestCase() {
     )
   }
 
-  @VimBehaviorDiffers(
-    """
-            If (found) {
-               if (it) {
-                  legendary
-               }
-            ${c}
-  """,
-  )
   @Test
-  fun `test delete starting from the line end`() {
+  fun `test delete previous word and trailing whitespace`() {
+    doTest(
+      listOf("i", "<C-W>"),
+      "Lorem       ${c}ipsum",
+      "${c}ipsum",
+      Mode.INSERT,
+    )
+  }
+
+  @Test
+  fun `test delete leading whitespace but not previous new line`() {
+    doTest(
+      listOf("i", "<C-W>"),
+      """
+        |Lorem Ipsum
+        |.....${c}.....
+        |Lorem ipsum dolor sit amet,
+      """.trimMargin().dotToSpace(),
+      """
+        |Lorem Ipsum
+        |${c}.....
+        |Lorem ipsum dolor sit amet,
+      """.trimMargin().dotToSpace(),
+      Mode.INSERT
+    )
+  }
+
+  // VIM-1650
+  @Test
+  fun `test delete leading whitespace but not previous new line 2`() {
+    doTest(
+      listOf("i", "<C-W>"),
+      """
+        |    public static void Main()
+        |    {
+        |..${c}..
+        |    }
+      """.trimMargin().dotToSpace(),
+      """
+        |    public static void Main()
+        |    {
+        |${c}..
+        |    }
+      """.trimMargin().dotToSpace(),
+      Mode.INSERT
+    )
+  }
+
+  @Test
+  fun `test delete starting from the last character of the file`() {
+    // This test was originally trying to delete the previous word with the caret positioned at the end of line and
+    // recorded different behaviour to Vim. The problem was that the caret was incorrectly positioned _passed_ the end
+    // of the line, and indeed passed the end of the file.
+    // This placement is valid, both in IdeaVim and Vim, but only when `:set virtualedit=onemore` is set. The test was
+    // showing a bug in the implementation in this scenario. The test is now explicit in what it's trying to do, and
+    // matches Vim's behaviour.
     doTest(
       listOf("i", "<C-W>"),
       """
@@ -100,10 +145,13 @@ class InsertDeletePreviousWordActionTest : VimTestCase() {
             If (found) {
                if (it) {
                   legendary
-               ${c}
+               }
+            ${c}
       """.trimIndent(),
       Mode.INSERT,
-    )
+    ) {
+      enterCommand("set virtualedit=onemore")
+    }
   }
 
   // VIM-569 |a| |i_CTRL-W|
