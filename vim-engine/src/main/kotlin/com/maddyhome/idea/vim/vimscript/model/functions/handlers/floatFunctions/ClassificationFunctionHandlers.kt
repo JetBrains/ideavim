9a5c55806081d1b94ccfaeb17806767be50b13ed
Matt Ellis <m.t.ellis@gmail.com>
2025-02-19 02:12:23 +0000

    Add float classification functions

---
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/functions/handlers/floatFunctions/ClassificationFunctionHandlers.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2003-2025 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.vimscript.model.functions.handlers.floatFunctions
+
+import com.intellij.vim.annotations.VimscriptFunction
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.vimscript.model.VimLContext
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimFloat
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
+import com.maddyhome.idea.vim.vimscript.model.datatypes.asVimInt
+import com.maddyhome.idea.vim.vimscript.model.expressions.Expression
+import com.maddyhome.idea.vim.vimscript.model.functions.FunctionHandler
+
+@VimscriptFunction(name = "isinf")
+internal class IsInfFunctionHandler : FunctionHandler() {
+  override val minimumNumberOfArguments = 1
+  override val maximumNumberOfArguments = 1
+
+  override fun doFunction(
+    argumentValues: List<Expression>,
+    editor: VimEditor,
+    context: ExecutionContext,
+    vimContext: VimLContext,
+  ): VimDataType {
+    val argument = argumentValues[0].evaluate(editor, context, vimContext)
+    if (argument !is VimInt && argument !is VimFloat) {
+      return false.asVimInt()
+    }
+    return when (argument.asDouble()) {
+      Double.POSITIVE_INFINITY -> VimInt.Companion.ONE
+      Double.NEGATIVE_INFINITY -> VimInt.Companion.MINUS_ONE
+      else -> VimInt.Companion.ZERO
+    }
+  }
+}
+
+@VimscriptFunction("isnan")
+internal class IsNanFunctionHandler : FunctionHandler() {
+  override val minimumNumberOfArguments = 1
+  override val maximumNumberOfArguments = 1
+
+  override fun doFunction(
+    argumentValues: List<Expression>,
+    editor: VimEditor,
+    context: ExecutionContext,
+    vimContext: VimLContext,
+  ): VimDataType {
+    val argument = argumentValues[0].evaluate(editor, context, vimContext)
+    if (argument !is VimInt && argument !is VimFloat) {
+      return false.asVimInt()
+    }
+    return argument.asDouble().isNaN().asVimInt()
+  }
+}
