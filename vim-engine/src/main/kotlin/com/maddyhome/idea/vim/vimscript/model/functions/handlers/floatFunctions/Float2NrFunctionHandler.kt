dd4e002c7cb50be19f52678141a5b1f8fdc6976b
Matt Ellis <m.t.ellis@gmail.com>
2025-02-19 09:08:33 +0000

    Add float2nr function

---
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/functions/handlers/floatFunctions/Float2NrFunctionHandler.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2003-2025 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.vimscript.model.functions.handlers.floatFunctions
+
+import com.intellij.vim.annotations.VimscriptFunction
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.ex.exExceptionMessage
+import com.maddyhome.idea.vim.vimscript.model.VimLContext
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimFloat
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
+import com.maddyhome.idea.vim.vimscript.model.expressions.Expression
+import com.maddyhome.idea.vim.vimscript.model.functions.FunctionHandler
+
+@VimscriptFunction(name = "float2nr")
+internal class Float2NrFunctionHandler : FunctionHandler() {
+  override val minimumNumberOfArguments = 1
+  override val maximumNumberOfArguments = 1
+
+  override fun doFunction(
+    argumentValues: List<Expression>,
+    editor: VimEditor,
+    context: ExecutionContext,
+    vimContext: VimLContext,
+  ): VimDataType {
+    val argument = argumentValues[0].evaluate(editor, context, vimContext)
+    if (argument !is VimFloat && argument !is VimInt) {
+      throw exExceptionMessage("E808")  // E808: Number or Float required
+    }
+    return VimInt(argument.asDouble().toInt())
+  }
+}
