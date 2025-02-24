1d25927676737808593813eadf9acba03b09314d
Alex Plate <aleksei.plate@jetbrains.com>
2025-02-24 11:16:08 +0200

    [TC] Upgrade the version of IJ for some build

--- .teamcity/_Self/Constants.kt
+++ .teamcity/_Self/Constants.kt
@@ -5,12 +5,12 @@ object Constants {
   const val EAP_CHANNEL = "eap"
   const val DEV_CHANNEL = "Dev"
 
-  const val NVIM_TESTS = "2024.2.1"
-  const val PROPERTY_TESTS = "2024.2.1"
-  const val LONG_RUNNING_TESTS = "2024.2.1"
-  const val QODANA_TESTS = "2024.2.1"
-  const val RELEASE = "2024.2.1"
+  const val NVIM_TESTS = "2024.3.3"
+  const val PROPERTY_TESTS = "2024.3.3"
+  const val LONG_RUNNING_TESTS = "2024.3.3"
+  const val QODANA_TESTS = "2024.3.3"
+  const val RELEASE = "2024.3.3"
 
-  const val RELEASE_DEV = "2024.2.1"
-  const val RELEASE_EAP = "2024.2.1"
+  const val RELEASE_DEV = "2024.3.3"
+  const val RELEASE_EAP = "2024.3.3"
 }
