2c1cba96d2a808e2863c0122eea2f2411ebaf697
Alex Plate <aleksei.plate@jetbrains.com>
2025-02-28 20:28:28 +0200

    Update TeamCity config version

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -30,5 +30,5 @@ node (Plugins -> teamcity-configs -> teamcity-configs:generate),
 the 'Debug' option is available in the context menu for the task.
 */
 
-version = "2024.03"
+version = "2024.12"
 project(_Self.Project)
