a322525c2a2640dbcde9582a0ee56f53d077ab35
Matt Ellis <m.t.ellis@gmail.com>
2025-03-16 22:07:03 +0100

    Be more explicit with is and isnot implementation

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/expressions/operators/handlers/binary/IsNotHandler.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/expressions/operators/handlers/binary/IsNotHandler.kt
@@ -8,24 +8,54 @@
 
 package com.maddyhome.idea.vim.vimscript.model.expressions.operators.handlers.binary
 
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimBlob
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDictionary
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimFloat
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimFuncref
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimList
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimString
+import com.maddyhome.idea.vim.vimscript.model.datatypes.asVimInt
 
 internal object IsNotHandler : BinaryOperatorWithIgnoreCaseOption(IsNotIgnoreCaseHandler, IsNotCaseSensitiveHandler)
 
 internal object IsNotIgnoreCaseHandler : BinaryOperatorHandler() {
   override fun performOperation(left: VimDataType, right: VimDataType): VimDataType {
-    return if (left is VimString && right is VimString) {
-      VimInt(if (left.value.compareTo(right.value, ignoreCase = true) != 0) 1 else 0)
-    } else {
-      VimInt(if (left != right) 1 else 0)
-    }
+    return when (left) {
+      // Check the value is the same with simple equals (data classes). Vim does not convert between Number and String!
+      is VimFloat -> left != right
+      is VimInt -> left != right
+
+      // Ignore case
+      is VimString -> {
+        if (right is VimString) left.value.compareTo(right.value, ignoreCase = true) != 0 else false
+      }
+
+      // Check the instance is the same with reference equals
+      is VimList -> left !== right
+      is VimDictionary -> left !== right
+      is VimFuncref -> left !== right
+      is VimBlob -> left !== right
+      else -> false
+    }.asVimInt()
   }
 }
 
 internal object IsNotCaseSensitiveHandler : BinaryOperatorHandler() {
   override fun performOperation(left: VimDataType, right: VimDataType): VimDataType {
-    return VimInt(if (left != right) 1 else 0)
+    return when (left) {
+      // Check the value is the same with simple equals (data classes). Vim does not convert between Number and String!
+      is VimFloat -> left != right
+      is VimInt -> left != right
+      is VimString -> left != right
+
+      // Check the instance is the same with reference equals
+      is VimList -> left !== right
+      is VimDictionary -> left !== right
+      is VimFuncref -> left !== right
+      is VimBlob -> left !== right
+      else -> false
+    }.asVimInt()
   }
 }
