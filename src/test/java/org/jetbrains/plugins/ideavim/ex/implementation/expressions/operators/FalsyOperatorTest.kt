3b2b863c8874e24c87353f5157ee0da6e5bd1d70
Matt Ellis <m.t.ellis@gmail.com>
2025-03-16 22:57:57 +0100

    Update truthy expressions

--- src/test/java/org/jetbrains/plugins/ideavim/ex/implementation/expressions/operators/FalsyOperatorTest.kt
+++ src/test/java/org/jetbrains/plugins/ideavim/ex/implementation/expressions/operators/FalsyOperatorTest.kt
@@ -8,6 +8,9 @@
 
 package org.jetbrains.plugins.ideavim.ex.implementation.expressions.operators
 
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDictionary
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimFloat
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimList
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimString
@@ -20,25 +23,67 @@ import kotlin.test.assertEquals
 class FalsyOperatorTest : VimTestCase() {
 
   @Test
-  fun `left expression is true`() {
-    assertEquals(VimInt("42"), VimscriptParser.parseExpression("42 ?? 999")!!.evaluate())
+  fun `test non-zero Number treated as truthy`() {
+    assertEquals(VimInt(42), VimscriptParser.parseExpression("42 ?? 999")!!.evaluate())
   }
 
   @Test
-  fun `left expression is false`() {
-    assertEquals(VimInt("42"), VimscriptParser.parseExpression("0 ?? 42")!!.evaluate())
+  fun `test non-zero negative Number treated as truthy`() {
+    assertEquals(VimInt(-1), VimscriptParser.parseExpression("-1 ?? 999")!!.evaluate())
   }
 
   @Test
-  fun `empty list as a left expression`() {
+  fun `test Number 0 is treated as falsy`() {
+    assertEquals(VimInt(42), VimscriptParser.parseExpression("0 ?? 42")!!.evaluate())
+  }
+
+  @Test
+  fun `test zero Float is treated as falsy`() {
+    assertEquals(VimInt(42), VimscriptParser.parseExpression("0.0 ?? 42")!!.evaluate())
+  }
+
+  @Test
+  fun `test non-zero Float treated as truthy`() {
+    assertEquals(VimFloat(42.0), VimscriptParser.parseExpression("42.0 ?? 999")!!.evaluate())
+  }
+
+  @Test
+  fun `test empty String treated as falsy`() {
+    assertEquals(VimString("string is empty"), VimscriptParser.parseExpression("'' ?? 'string is empty'")!!.evaluate())
+  }
+
+  @Test
+  fun `test String '0' treated as falsy`() {
+    assertEquals(VimString("string is empty"), VimscriptParser.parseExpression("'0' ?? 'string is empty'")!!.evaluate())
+  }
+
+  @Test
+  fun `test non-empty String treated as truthy`() {
+    assertEquals(VimString("string is not empty"), VimscriptParser.parseExpression("'string is not empty' ?? 'string is empty'")!!.evaluate())
+  }
+
+  @Test
+  fun `test empty list treated as falsy`() {
     assertEquals(VimString("list is empty"), VimscriptParser.parseExpression("[] ?? 'list is empty'")!!.evaluate())
   }
 
   @Test
-  fun `nonempty list as a left expression`() {
+  fun `test non-empty List treated as truthy`() {
     assertEquals(
       VimList(mutableListOf(VimInt(1), VimInt(2), VimInt(3))),
       VimscriptParser.parseExpression("[1, 2, 3] ?? 'list is empty'")!!.evaluate(),
     )
+  }
+
+  @Test
+  fun `test empty Dictionary treated as falsy`() {
+    assertEquals(VimString("dict is empty"), VimscriptParser.parseExpression("{} ?? 'dict is empty'")!!.evaluate())
+  }
+
+  @Test
+  fun `test non-empty Dictionary treated as truthy`() {
+    val dictionary = LinkedHashMap<VimString, VimDataType>()
+    dictionary.put(VimString("1"), VimInt(1))
+    assertEquals(VimDictionary(dictionary), VimscriptParser.parseExpression("{'1': 1} ?? 'dict is empty'")!!.evaluate())
   }
 }
