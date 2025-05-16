761f6f5fb93b54dbecd630cf0279e76d9b55c997
chylex <contact@chylex.com>
2025-05-16 17:00:01 +0200

    Implement pumvisible() function

---
+++ src/main/java/com/maddyhome/idea/vim/vimscript/model/functions/handlers/PopupMenuVisibleFunctionHandler.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2003-2023 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.vimscript.model.functions.handlers
+
+import com.intellij.codeInsight.completion.CompletionService
+import com.intellij.vim.annotations.VimscriptFunction
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.vimscript.model.VimLContext
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
+import com.maddyhome.idea.vim.vimscript.model.expressions.Expression
+import com.maddyhome.idea.vim.vimscript.model.functions.FunctionHandler
+
+@VimscriptFunction(name = "pumvisible")
+internal class PopupMenuVisibleFunctionHandler : FunctionHandler() {
+  override val minimumNumberOfArguments = 0
+  override val maximumNumberOfArguments = 0
+
+  override fun doFunction(
+    argumentValues: List<Expression>,
+    editor: VimEditor,
+    context: ExecutionContext,
+    vimContext: VimLContext,
+  ): VimDataType {
+    return if (CompletionService.getCompletionService().currentCompletion == null)
+      VimInt.ZERO
+    else
+      VimInt.ONE
+  }
+}
