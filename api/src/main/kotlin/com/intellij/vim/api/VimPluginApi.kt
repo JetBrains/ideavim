886dd0f09cbd926fed7dedea84c455e7b9fb0e80
Mia Vucinic <mia.vucinic@jetbrains.com>
2025-05-27 12:57:28 +0200

    have functions in VimPluginApi take editor and context as parameters instead of scopes

--- api/src/main/kotlin/com/intellij/vim/api/VimPluginApi.kt
+++ api/src/main/kotlin/com/intellij/vim/api/VimPluginApi.kt
@@ -8,41 +8,55 @@
 
 package com.intellij.vim.api
 
-import com.intellij.vim.api.scopes.Read
-import com.intellij.vim.api.scopes.Transaction
 import com.intellij.vim.api.scopes.VimScope
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
 
 
 interface VimPluginApi {
   fun getResourceGuard(): ResourceGuard
 
-  fun getRegisterContent(read: Read, caretId: CaretId, register: Char): String?
-  fun getCurrentRegisterName(read: Read, caretId: CaretId): Char
-  fun getRegisterType(read: Read, caretId: CaretId, register: Char): RegisterType?
+  fun getRegisterContent(
+    editor: VimEditor,
+    context: ExecutionContext,
+    caretId: CaretId,
+    register: Char,
+  ): String?
 
-  fun addMapping(scope: VimScope, fromKeys: String, toKeys: String, isRecursive: Boolean, vararg mode: Mode)
+  fun getCurrentRegisterName(
+    editor: VimEditor,
+    caretId: CaretId,
+  ): Char
+
+  fun getRegisterType(
+    editor: VimEditor,
+    context: ExecutionContext,
+    caretId: CaretId,
+    register: Char,
+  ): RegisterType?
+
+  fun addMapping(fromKeys: String, toKeys: String, isRecursive: Boolean, vararg mode: Mode)
   fun addMapping(
     fromKeys: String,
-    scope: VimScope,
     isRecursive: Boolean,
     isRepeatable: Boolean,
     action: VimScope.() -> Unit,
     vararg mode: Mode,
   )
 
-  fun removeMapping(scope: VimScope, fromKeys: String, vararg mode: Mode)
+  fun removeMapping(fromKeys: String, vararg mode: Mode)
 
-  fun exportOperatorFunction(name: String, scope: VimScope, function: VimScope.() -> Boolean)
-  fun setOperatorFunction(scope: VimScope, name: String)
-  fun executeNormal(scope: VimScope, command: String)
+  fun exportOperatorFunction(name: String, vimPluginApi: VimPluginApi, function: VimScope.() -> Boolean)
+  fun setOperatorFunction(name: String)
+  fun executeNormal(editor: VimEditor, command: String)
 
-  fun getMode(scope: VimScope): Mode
-  fun getSelectionTypeForCurrentMode(scope: VimScope): TextSelectionType?
-  fun exitVisualMode(scope: VimScope)
+  fun getMode(editor: VimEditor): Mode
+  fun getSelectionTypeForCurrentMode(editor: VimEditor): TextSelectionType?
+  fun exitVisualMode(editor: VimEditor)
 
-  fun deleteText(transaction: Transaction, startOffset: Int, endOffset: Int)
+  fun deleteText(editor: VimEditor, startOffset: Int, endOffset: Int)
   fun replaceText(
-    transaction: Transaction,
+    editor: VimEditor,
     caretId: CaretId,
     startOffset: Int,
     endOffset: Int,
@@ -50,31 +64,31 @@ interface VimPluginApi {
   )
 
   fun replaceTextBlockwise(
-    transaction: Transaction,
+    editor: VimEditor,
     caretId: CaretId,
     startOffset: Int,
     endOffset: Int,
-    text: List<String>
+    text: List<String>,
   )
 
-  fun getChangeMarks(read: Read, caretId: CaretId): Pair<Int, Int>?
-  fun getVisualMarks(read: Read, caretId: CaretId): Pair<Int, Int>?
+  fun getChangeMarks(editor: VimEditor, caretId: CaretId): Pair<Int, Int>?
+  fun getVisualMarks(editor: VimEditor, caretId: CaretId): Pair<Int, Int>?
 
-  fun getLineStartOffset(read: Read, line: Int): Int
-  fun getLineEndOffset(read: Read, line: Int, allowEnd: Boolean): Int
+  fun getLineStartOffset(editor: VimEditor, line: Int): Int
+  fun getLineEndOffset(editor: VimEditor, line: Int, allowEnd: Boolean): Int
 
-  fun getAllCaretIds(read: Read): List<CaretId>
-  fun getALlCaretIdsSortedByOffset(read: Read): List<CaretId>
-  fun getCaretInfo(read: Read, caretId: CaretId): CaretInfo?
+  fun getAllCaretIds(editor: VimEditor): List<CaretId>
+  fun getALlCaretIdsSortedByOffset(editor: VimEditor): List<CaretId>
+  fun getCaretInfo(editor: VimEditor, caretId: CaretId): CaretInfo?
 
-  fun getAllCaretsData(read: Read): List<CaretData>
-  fun getAllCaretsDataSortedByOffset(read: Read): List<CaretData>
+  fun getAllCaretsData(editor: VimEditor): List<CaretData>
+  fun getAllCaretsDataSortedByOffset(editor: VimEditor): List<CaretData>
 
-  fun updateCaret(transaction: Transaction, caretId: CaretId, caretInfo: CaretInfo)
+  fun updateCaret(editor: VimEditor, caretId: CaretId, caretInfo: CaretInfo)
 
-  fun getCaretLine(read: Read, caretId: CaretId): Int?
-  fun addCaret(transaction: Transaction, caretInfo: CaretInfo): CaretId
-  fun removeCaret(transaction: Transaction, caretId: CaretId)
+  fun getCaretLine(editor: VimEditor, caretId: CaretId): Int?
+  fun addCaret(editor: VimEditor, caretInfo: CaretInfo): CaretId
+  fun removeCaret(editor: VimEditor, caretId: CaretId)
 
-  fun getVimVariableInt(scope: VimScope, vimVariablesScope: VimVariablesScope, name: String): Int?
+  fun getVimVariableInt(vimVariablesScope: VimVariablesScope, name: String): Int?
 }
