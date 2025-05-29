1861730c4ea31c237ee6606a871916a78651e005
Mia Vucinic <mia.vucinic@jetbrains.com>
2025-05-29 13:56:31 +0200

    huge refactoring
    - In the `api` module:
      - remove dependency on `vim-engine` module due to circular dependencies
      - move implementations of scopes to the `vim-engine`
      - add VimPluginDsl annotation to interfaces
      - make VimScope abstract class
      - remove ScopeBuilders file and move scope builders to the VimScope abstract class
     - In the `vim-engine` module:
      - add dependency on `api` module
      - add implementation of scopes
      - in VimInjector add new field - pluginService (reason for that is because functions from VimExtensionFacade are not available in the VimEngine)

---
+++ src/main/java/com/maddyhome/idea/vim/extension/thin/api/VimPluginServiceImpl.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2003-2025 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.extension.thin.api
+
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.api.injector
+import com.maddyhome.idea.vim.extension.VimExtensionFacade
+import com.maddyhome.idea.vim.extension.exportOperatorFunction
+import com.maddyhome.idea.vim.key.OperatorFunction
+import com.maddyhome.idea.vim.newapi.ij
+import com.maddyhome.idea.vim.thinapi.VimPluginService
+
+class VimPluginServiceImpl : VimPluginService {
+  override fun executeNormalWithoutMapping(command: String, editor: VimEditor) {
+    VimExtensionFacade.executeNormalWithoutMapping(injector.parser.parseKeys("g@"), editor.ij)
+  }
+
+  override fun exportOperatorFunction(name: String, function: OperatorFunction) {
+    VimExtensionFacade.exportOperatorFunction(name, function)
+  }
+}
