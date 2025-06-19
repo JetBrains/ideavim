29782137a2391cf5918108524f7b73ef18ae33d8
Mia Vucinic <mia.vucinic@jetbrains.com>
2025-06-19 11:46:09 +0200

    add more functions and properties into the color class and move parse rgba color to the highlighted yank extension

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/thinapi/ThinApi.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/thinapi/ThinApi.kt
@@ -10,17 +10,11 @@ package com.maddyhome.idea.vim.thinapi
 
 import com.intellij.vim.api.CaretId
 import com.intellij.vim.api.CaretInfo
-import com.intellij.vim.api.Color
 import com.intellij.vim.api.Range
 import com.intellij.vim.api.TextType
 import com.maddyhome.idea.vim.api.VimCaret
 import com.maddyhome.idea.vim.common.TextRange
 import com.maddyhome.idea.vim.state.mode.SelectionType
-import java.lang.String
-import kotlin.text.removePrefix
-import kotlin.text.toLong
-import kotlin.to
-import java.awt.Color as AwtColor
 
 fun SelectionType.toTextSelectionType(): TextType {
   return when (this) {
@@ -46,23 +40,6 @@ val VimCaret.caretInfo: CaretInfo
     this.offset,
     if (hasSelection()) selectionStart to selectionEnd else null
   )
-
-fun Color.toAwtColor(): AwtColor {
-  val argb = hexCode.removePrefix("#").toLong(16)
-  val hasAlpha = hexCode.length > 7
-
-  val r = (argb shr 16).toInt() and 0xFF
-  val g = (argb shr 8).toInt() and 0xFF
-  val b = argb.toInt() and 0xFF
-  val a = if (hasAlpha) (argb shr 24).toInt() and 0xFF else 0xFF
-
-  return AwtColor(r, g, b, a)
-}
-
-fun AwtColor.toHexColor(): Color {
-  val hexColor = String.format("#%02x%02x%02x%02x", alpha, red, green, blue)
-  return Color(hexColor)
-}
 
 fun TextRange.toRange(): Range.Simple {
   return Range.Simple(startOffset, endOffset)
