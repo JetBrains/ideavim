d2c0c0a6082fb3c7730cda7a44f3036b6df7ee96
Mia Vucinic <mia.vucinic@jetbrains.com>
2025-07-10 13:07:32 +0200

    make jetbrains annotation compileOnly in api module

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -20,8 +20,7 @@ dependencies {
   testImplementation(platform("org.junit:junit-bom:5.10.0"))
   testImplementation("org.junit.jupiter:junit-jupiter")
   compileOnly("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-  // https://mvnrepository.com/artifact/org.jetbrains/annotations
-  implementation("org.jetbrains:annotations:26.0.2")
+  compileOnly("org.jetbrains:annotations:26.0.2")
 }
 
 tasks.test {
