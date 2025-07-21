1056278a7703eb4455282ea1ffbf8d943400745f
Xinhe Wang <xinhe.wang@jetbrains.com>
2025-07-21 14:25:54 +0100

    Fix(VIM-3437): correct the behavior of NERDTree-X

---
+++ src/test/java/org/jetbrains/plugins/ideavim/extension/nerdtree/NerdTreeTest.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2003-2025 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package org.jetbrains.plugins.ideavim.extension.nerdtree
+
+import com.intellij.openapi.options.advanced.AdvancedSettings
+import org.jetbrains.plugins.ideavim.VimTestCase
+import org.junit.jupiter.api.Test
+
+class NerdTreeTest : VimTestCase() {
+  @Test
+  fun `test collapse recursively advanced setting id`() {
+    AdvancedSettings.getBoolean("ide.tree.collapse.recursively") // will throw if the id is invalid
+  }
+}
