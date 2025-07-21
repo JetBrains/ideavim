38a4512b00eef9a5741e675f9a5a766451d6b943
Mia Vucinic <mia.vucinic@jetbrains.com>
2025-07-21 20:48:02 +0200

    revert "convert VimExtension file to Kotlin"
    This reverts commit 58c84a7d3af3fc3a3c597949e059d86784825697.

--- src/main/java/com/maddyhome/idea/vim/extension/VimExtension.java
+++ src/main/java/com/maddyhome/idea/vim/extension/VimExtension.java
@@ -5,37 +5,41 @@
  * license that can be found in the LICENSE.txt file or at
  * https://opensource.org/licenses/MIT.
  */
-package com.maddyhome.idea.vim.extension
 
-import com.intellij.openapi.extensions.ExtensionPointName
-import com.maddyhome.idea.vim.VimPlugin
-import com.maddyhome.idea.vim.api.injector
-import com.maddyhome.idea.vim.common.ListenerOwner
-import com.maddyhome.idea.vim.helper.VimNlsSafe
-import com.maddyhome.idea.vim.key.MappingOwner
+package com.maddyhome.idea.vim.extension;
+
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.maddyhome.idea.vim.VimPlugin;
+import com.maddyhome.idea.vim.api.VimInjectorKt;
+import com.maddyhome.idea.vim.common.ListenerOwner;
+import com.maddyhome.idea.vim.helper.VimNlsSafe;
+import com.maddyhome.idea.vim.key.MappingOwner;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author vlan
  */
-interface VimExtension {
-  @get:VimNlsSafe
-  val name: String
+public interface VimExtension {
+  @NotNull ExtensionPointName<ExtensionBeanClass> EP_NAME = ExtensionPointName.create("IdeaVIM.vimExtension");
 
-  val owner: MappingOwner
-    get() = MappingOwner.Plugin.Companion.get(this.name)
+  @VimNlsSafe
+  @NotNull String getName();
 
-  val listenerOwner: ListenerOwner
-    get() = ListenerOwner.Plugin.Companion.get(this.name)
+  default MappingOwner getOwner() {
+    return MappingOwner.Plugin.Companion.get(getName());
+  }
+
+  default ListenerOwner getListenerOwner() {
+    return ListenerOwner.Plugin.Companion.get(getName());
+  }
 
   /**
    * This method is always called AFTER the full execution of the `.ideavimrc` file.
-   *
-   *
+   * <p>
    * During vim initialization process, it firstly loads the .vimrc file, then executes scripts from the plugins folder.
    * This practically means that the .vimrc file is initialized first; then the plugins are loaded.
    * See `:h initialization`
-   *
-   *
+   * <p>
    * Why does this matter? Because this affects the order of commands are executed. For example,
    * ```
    * plug 'tommcdo/vim-exchange'
@@ -43,8 +47,7 @@ interface VimExtension {
    * ```
    * Here the user will expect that the exchange plugin won't have default mappings. However, if we load vim-exchange
    * immediately, this variable won't be initialized at the moment of plugin initialization.
-   *
-   *
+   * <p>
    * There is also a tricky case for mappings override:
    * ```
    * plug 'tommcdo/vim-exchange'
@@ -53,15 +56,11 @@ interface VimExtension {
    * For this case, a plugin with a good implementation detects that there is already a defined mapping for
    * `<Plug>(ExchangeLine)` and doesn't register the default cxx mapping. However, such detection requires the mapping
    * to be defined before the plugin initialization.
-  </Plug></Plug> */
-  fun init()
+   */
+  void init();
 
-  fun dispose() {
-    VimPlugin.getKey().removeKeyMapping(this.owner)
-    injector.listenersNotifier.unloadListeners(this.listenerOwner)
-  }
-
-  companion object {
-    internal val EP_NAME: ExtensionPointName<ExtensionBeanClass> = ExtensionPointName.create<ExtensionBeanClass>("IdeaVIM.vimExtension")
+  default void dispose() {
+    VimPlugin.getKey().removeKeyMapping(getOwner());
+    VimInjectorKt.getInjector().getListenersNotifier().unloadListeners(getListenerOwner());
   }
 }
