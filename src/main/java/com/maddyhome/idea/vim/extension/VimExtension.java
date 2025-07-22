96734867e5fc3a6d9ff7bccfa20fe75ab7c29c4b
Mia Vucinic <mia.vucinic@jetbrains.com>
2025-07-22 12:52:11 +0200

    revert VimExtension to previous state

--- src/main/java/com/maddyhome/idea/vim/extension/VimExtension.java
+++ src/main/java/com/maddyhome/idea/vim/extension/VimExtension.java
@@ -10,8 +10,6 @@ package com.maddyhome.idea.vim.extension;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.maddyhome.idea.vim.VimPlugin;
-import com.maddyhome.idea.vim.api.VimInjectorKt;
-import com.maddyhome.idea.vim.common.ListenerOwner;
 import com.maddyhome.idea.vim.helper.VimNlsSafe;
 import com.maddyhome.idea.vim.key.MappingOwner;
 import org.jetbrains.annotations.NotNull;
@@ -27,10 +25,6 @@ public interface VimExtension {
 
   default MappingOwner getOwner() {
     return MappingOwner.Plugin.Companion.get(getName());
-  }
-
-  default ListenerOwner getListenerOwner() {
-    return ListenerOwner.Plugin.Companion.get(getName());
   }
 
   /**
@@ -61,6 +55,5 @@ public interface VimExtension {
 
   default void dispose() {
     VimPlugin.getKey().removeKeyMapping(getOwner());
-    VimInjectorKt.getInjector().getListenersNotifier().unloadListeners(getListenerOwner());
   }
 }
