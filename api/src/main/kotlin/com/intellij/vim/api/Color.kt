fd5e84eddc050a26a36c53fe203d20ef3f0c71db
Mia Vucinic <mia.vucinic@jetbrains.com>
2025-07-22 12:39:10 +0200

    add docs

--- api/src/main/kotlin/com/intellij/vim/api/Color.kt
+++ api/src/main/kotlin/com/intellij/vim/api/Color.kt
@@ -8,17 +8,43 @@
 
 package com.intellij.vim.api
 
+/**
+ * Represents a color in RGBA format.
+ *
+ * @property hexCode The string representation of the color in hex format (#RRGGBB or #RRGGBBAA).
+ */
 data class Color(
   val hexCode: String
 ) {
+  /**
+   * Creates a color from individual RGB(A) components.
+   *
+   * @param r Red component (0-255).
+   * @param g Green component (0-255).
+   * @param b Blue component (0-255).
+   * @param a Alpha component (0-255), defaults to 255 (fully opaque).
+   */
   constructor(r: Int, g: Int, b: Int, a: Int = 255): this(String.format("#%02x%02x%02x%02x", r, g, b, a))
 
+  /**
+   * The red component of the color (0-255).
+   */
   val r: Int = hexCode.substring(1..2).toInt(16)
 
+  /**
+   * The green component of the color (0-255).
+   */
   val g: Int = hexCode.substring(3..4).toInt(16)
 
+  /**
+   * The blue component of the color (0-255).
+   */
   val b: Int = hexCode.substring(5..6).toInt(16)
 
+  /**
+   * The alpha component of the color (0-255).
+   * Defaults to 255 (fully opaque) if not specified in the hex code.
+   */
   val a: Int = if (hexCode.length == 9) hexCode.substring(7..8).toInt(16) else 255
 
   init {
--- api/src/main/kotlin/com/intellij/vim/api/Range.kt
+++ api/src/main/kotlin/com/intellij/vim/api/Range.kt
@@ -8,9 +8,25 @@
 
 package com.intellij.vim.api
 
+/**
+ * Represents a range of text in the editor.
+ * Can be either a simple linear range or a block (rectangular) range.
+ */
 sealed interface Range {
+  /**
+   * Represents a simple linear range of text from start to end offset.
+   *
+   * @property start The starting offset of the range.
+   * @property end The ending offset of the range (exclusive).
+   */
   data class Simple(val start: Int, val end: Int): Range
 
+  /**
+   * Represents a block (rectangular) selection consisting of multiple simple ranges.
+   * Each simple range typically represents a line segment in the block selection.
+   *
+   * @property ranges An array of simple ranges that make up the block selection.
+   */
   data class Block(val ranges: Array<Simple>): Range {
     override fun equals(other: Any?): Boolean {
       if (this === other) return true
