a25b88327ef30867d3931b15b7a001c431453586
Mia Vucinic <mia.vucinic@jetbrains.com>
2025-07-31 14:19:55 +0200

    implement PlugDisable command

---
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/commands/PlugDisable.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2003-2025 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.vimscript.model.commands
+
+import com.intellij.vim.annotations.ExCommand
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.api.injector
+import com.maddyhome.idea.vim.command.OperatorArguments
+import com.maddyhome.idea.vim.ex.ranges.Range
+import com.maddyhome.idea.vim.vimscript.model.ExecutionResult
+
+/**
+ * Command that is used for disabling plugins. E.g. `:PlugDisable [plugin-name]`
+ */
+@ExCommand(command = "PlugDisable")
+class PlugDisable(val range: Range, val modifier: CommandModifier, val argument: String) :
+  Command.SingleExecution(range, modifier, argument) {
+
+  override val argFlags: CommandHandlerFlags =
+    flags(RangeFlag.RANGE_FORBIDDEN, ArgumentFlag.ARGUMENT_REQUIRED, Access.READ_ONLY)
+
+  override fun processCommand(
+    editor: VimEditor,
+    context: ExecutionContext,
+    operatorArguments: OperatorArguments,
+  ): ExecutionResult {
+    val extensionName = argument
+    if (extensionName.isEmpty()) return ExecutionResult.Error
+
+    injector.extensionLoader.disableExtension(extensionName)
+
+    return ExecutionResult.Success
+  }
+}
