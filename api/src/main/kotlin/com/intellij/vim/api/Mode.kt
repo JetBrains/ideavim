a0c213b90d86bd3a6b1ad0502bbba5944b24ce69
Alex Plate <aleksei.plate@jetbrains.com>
2025-08-01 14:53:54 +0300

    Refactor Mode in API to be a simple enum
    The current representation of Mode with `returnTo` is quite complicated and we're not even sure it'll remain like that.
    At the same time, `mode()` function in Vim has quite a reach specification and there is a small chance it'll be changed. With this approach, we use values from Vim, yet in a form of enum.

--- api/src/main/kotlin/com/intellij/vim/api/Mode.kt
+++ api/src/main/kotlin/com/intellij/vim/api/Mode.kt
@@ -12,64 +12,129 @@ package com.intellij.vim.api
 /**
  * Represents a Vim editor mode.
  */
-sealed interface Mode {
-  /**
-   * The mode to return to when exiting the current mode.
-   * This is used to determine which mode to switch to after completing an operation.
-   */
-  val returnTo: Mode
-
+enum class Mode {
   /**
    * Normal mode - the default mode where commands and motions can be executed.
-   *
-   * @property originalMode The mode to return to when exiting this mode, if specified.
    */
-  data class NORMAL(private val originalMode: Mode? = null) : Mode {
-    override val returnTo: Mode
-      get() = originalMode ?: this
-  }
+  NORMAL,
 
   /**
    * Operator-pending mode - entered after an operator command is given but before a motion is provided.
-   *
-   * @property returnTo The mode to return to after the operation is complete.
    */
-  data class OP_PENDING(override val returnTo: Mode) : Mode
+  OP_PENDING,
 
   /**
-   * Visual mode - used for selecting text.
-   *
-   * @property selectionType The type of selection (character, line, or block).
-   * @property returnTo The mode to return to when exiting visual mode.
+   * Operator-pending mode with forced characterwise operation.
    */
-  data class VISUAL(val selectionType: TextType, override val returnTo: Mode = NORMAL()) : Mode
+  OP_PENDING_CHARACTERWISE,
 
   /**
-   * Select mode - similar to visual mode but with different behavior.
-   *
-   * @property selectionType The type of selection (character, line, or block).
-   * @property returnTo The mode to return to when exiting select mode.
+   * Operator-pending mode with forced linewise operation.
    */
-  data class SELECT(val selectionType: TextType, override val returnTo: Mode = NORMAL()) : Mode
+  OP_PENDING_LINEWISE,
+
+  /**
+   * Operator-pending mode with forced blockwise operation.
+   */
+  OP_PENDING_BLOCKWISE,
+
+  /**
+   * Normal mode using i_CTRL-O in Insert-mode.
+   */
+  NORMAL_FROM_INSERT,
+
+  /**
+   * Normal mode using i_CTRL-O in Replace-mode.
+   */
+  NORMAL_FROM_REPLACE,
+
+  /**
+   * Normal mode using i_CTRL-O in Virtual-Replace-mode.
+   */
+  NORMAL_FROM_VIRTUAL_REPLACE,
+
+  /**
+   * Visual mode with character-wise selection.
+   */
+  VISUAL_CHARACTER,
+
+  /**
+   * Visual mode with character-wise selection using v_CTRL-O in Select mode.
+   */
+  VISUAL_CHARACTER_FROM_SELECT,
+
+  /**
+   * Visual mode with line-wise selection.
+   */
+  VISUAL_LINE,
+
+  /**
+   * Visual mode with line-wise selection using v_CTRL-O in Select mode.
+   */
+  VISUAL_LINE_FROM_SELECT,
+
+  /**
+   * Visual mode with block-wise selection.
+   */
+  VISUAL_BLOCK,
+
+  /**
+   * Visual mode with block-wise selection using v_CTRL-O in Select mode.
+   */
+  VISUAL_BLOCK_FROM_SELECT,
+
+  /**
+   * Select mode with character-wise selection.
+   */
+  SELECT_CHARACTER,
+
+  /**
+   * Select mode with line-wise selection.
+   */
+  SELECT_LINE,
+
+  /**
+   * Select mode with block-wise selection.
+   */
+  SELECT_BLOCK,
 
   /**
    * Insert mode - used for inserting text.
    */
-  object INSERT : Mode {
-    override val returnTo: Mode = NORMAL()
-  }
+  INSERT,
 
   /**
    * Replace mode - used for replacing existing text.
    */
-  object REPLACE : Mode {
-    override val returnTo: Mode = NORMAL()
-  }
+  REPLACE,
 
   /**
    * Command-line mode - used for entering Ex commands.
-   *
-   * @property returnTo The mode to return to after executing the command.
    */
-  data class CMD_LINE(override val returnTo: Mode) : Mode
+  COMMAND_LINE;
+
+  /**
+   * Returns the TextType associated with this mode, if applicable.
+   * Only visual and select modes have a TextType.
+   */
+  val selectionType: TextType?
+    get() = when (this) {
+      VISUAL_CHARACTER, VISUAL_CHARACTER_FROM_SELECT, SELECT_CHARACTER -> TextType.CHARACTER_WISE
+      VISUAL_LINE, VISUAL_LINE_FROM_SELECT, SELECT_LINE -> TextType.LINE_WISE
+      VISUAL_BLOCK, VISUAL_BLOCK_FROM_SELECT, SELECT_BLOCK -> TextType.BLOCK_WISE
+      else -> null
+    }
+
+  /**
+   * Returns true if this mode is a visual mode.
+   */
+  val isVisual: Boolean
+    get() = this == VISUAL_CHARACTER || this == VISUAL_LINE || this == VISUAL_BLOCK ||
+      this == VISUAL_CHARACTER_FROM_SELECT || this == VISUAL_LINE_FROM_SELECT || this == VISUAL_BLOCK_FROM_SELECT
+
+  /**
+   * Returns true if this mode is a select mode.
+   */
+  val isSelect: Boolean
+    get() = this == SELECT_CHARACTER || this == SELECT_LINE || this == SELECT_BLOCK
 }
--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/thinapi/Modes.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/thinapi/Modes.kt
@@ -22,44 +22,123 @@ import com.maddyhome.idea.vim.state.mode.Mode as EngineMode
 
 fun EngineMode.toMode(): Mode {
   return when (this) {
-    is EngineMode.NORMAL -> Mode.NORMAL(if (returnTo == this) Mode.NORMAL() else returnTo.toMode())
-    is EngineMode.VISUAL -> Mode.VISUAL(
-      selectionType.toTextSelectionType(),
-      if (returnTo == this) Mode.NORMAL() else returnTo.toMode()
-    )
-
-    is EngineMode.SELECT -> Mode.SELECT(
-      selectionType.toTextSelectionType(),
-      if (returnTo == this) Mode.NORMAL() else returnTo.toMode()
-    )
-
-    is EngineMode.OP_PENDING -> Mode.OP_PENDING(if (returnTo == this) Mode.NORMAL() else returnTo.toMode())
+    is EngineMode.NORMAL -> {
+      // Check if this is a normal mode that returns to another mode
+      if (returnTo is EngineMode.INSERT) {
+        Mode.NORMAL_FROM_INSERT
+      } else if (returnTo is EngineMode.REPLACE) {
+        Mode.NORMAL_FROM_REPLACE
+      } else {
+        Mode.NORMAL
+      }
+    }
+    is EngineMode.VISUAL -> {
+      // Determine the visual mode based on selection type and return mode
+      when (selectionType) {
+        com.maddyhome.idea.vim.state.mode.SelectionType.CHARACTER_WISE -> {
+          if (returnTo is EngineMode.SELECT) Mode.VISUAL_CHARACTER_FROM_SELECT else Mode.VISUAL_CHARACTER
+        }
+        com.maddyhome.idea.vim.state.mode.SelectionType.LINE_WISE -> {
+          if (returnTo is EngineMode.SELECT) Mode.VISUAL_LINE_FROM_SELECT else Mode.VISUAL_LINE
+        }
+        com.maddyhome.idea.vim.state.mode.SelectionType.BLOCK_WISE -> {
+          if (returnTo is EngineMode.SELECT) Mode.VISUAL_BLOCK_FROM_SELECT else Mode.VISUAL_BLOCK
+        }
+      }
+    }
+    is EngineMode.SELECT -> {
+      // Determine the select mode based on selection type
+      when (selectionType) {
+        com.maddyhome.idea.vim.state.mode.SelectionType.CHARACTER_WISE -> Mode.SELECT_CHARACTER
+        com.maddyhome.idea.vim.state.mode.SelectionType.LINE_WISE -> Mode.SELECT_LINE
+        com.maddyhome.idea.vim.state.mode.SelectionType.BLOCK_WISE -> Mode.SELECT_BLOCK
+      }
+    }
+    is EngineMode.OP_PENDING -> Mode.OP_PENDING
     is EngineMode.INSERT -> Mode.INSERT
-    is EngineMode.CMD_LINE -> Mode.CMD_LINE(if (returnTo == this) Mode.NORMAL() else returnTo.toMode())
+    is EngineMode.CMD_LINE -> Mode.COMMAND_LINE
     is EngineMode.REPLACE -> Mode.REPLACE
   }
 }
 
 fun Mode.toEngineMode(): EngineMode {
-  val returnTo = this.returnTo
   return when (this) {
-    is Mode.NORMAL -> EngineMode.NORMAL()
-    is Mode.VISUAL -> {
+    // Normal modes
+    Mode.NORMAL -> EngineMode.NORMAL()
+    Mode.NORMAL_FROM_INSERT -> EngineMode.NORMAL(EngineMode.INSERT)
+    Mode.NORMAL_FROM_REPLACE -> EngineMode.NORMAL(EngineMode.REPLACE)
+    Mode.NORMAL_FROM_VIRTUAL_REPLACE -> EngineMode.NORMAL(EngineMode.REPLACE) // Using REPLACE as fallback
+    
+    // Operator pending modes
+    Mode.OP_PENDING, 
+    Mode.OP_PENDING_CHARACTERWISE,
+    Mode.OP_PENDING_LINEWISE,
+    Mode.OP_PENDING_BLOCKWISE -> EngineMode.OP_PENDING(EngineMode.NORMAL())
+    
+    // Visual modes
+    Mode.VISUAL_CHARACTER -> EngineMode.VISUAL(
+      com.maddyhome.idea.vim.state.mode.SelectionType.CHARACTER_WISE, 
+      EngineMode.NORMAL()
+    )
+    Mode.VISUAL_LINE -> EngineMode.VISUAL(
+      com.maddyhome.idea.vim.state.mode.SelectionType.LINE_WISE, 
+      EngineMode.NORMAL()
+    )
+    Mode.VISUAL_BLOCK -> EngineMode.VISUAL(
+      com.maddyhome.idea.vim.state.mode.SelectionType.BLOCK_WISE, 
+      EngineMode.NORMAL()
+    )
+    
+    // Visual modes from select
+    Mode.VISUAL_CHARACTER_FROM_SELECT -> {
+      val selectMode = EngineMode.SELECT(
+        com.maddyhome.idea.vim.state.mode.SelectionType.CHARACTER_WISE, 
+        EngineMode.NORMAL()
+      )
       EngineMode.VISUAL(
-        selectionType.toSelectionType(),
-        returnTo.toEngineMode()
+        com.maddyhome.idea.vim.state.mode.SelectionType.CHARACTER_WISE, 
+        selectMode
       )
     }
-
-    is Mode.SELECT -> EngineMode.SELECT(
-      selectionType.toSelectionType(),
-      returnTo.toEngineMode()
+    Mode.VISUAL_LINE_FROM_SELECT -> {
+      val selectMode = EngineMode.SELECT(
+        com.maddyhome.idea.vim.state.mode.SelectionType.LINE_WISE, 
+        EngineMode.NORMAL()
+      )
+      EngineMode.VISUAL(
+        com.maddyhome.idea.vim.state.mode.SelectionType.LINE_WISE, 
+        selectMode
+      )
+    }
+    Mode.VISUAL_BLOCK_FROM_SELECT -> {
+      val selectMode = EngineMode.SELECT(
+        com.maddyhome.idea.vim.state.mode.SelectionType.BLOCK_WISE, 
+        EngineMode.NORMAL()
+      )
+      EngineMode.VISUAL(
+        com.maddyhome.idea.vim.state.mode.SelectionType.BLOCK_WISE, 
+        selectMode
+      )
+    }
+    
+    // Select modes
+    Mode.SELECT_CHARACTER -> EngineMode.SELECT(
+      com.maddyhome.idea.vim.state.mode.SelectionType.CHARACTER_WISE, 
+      EngineMode.NORMAL()
     )
-
-    is Mode.OP_PENDING -> EngineMode.OP_PENDING(returnTo.toEngineMode())
-    is Mode.INSERT -> EngineMode.INSERT
-    is Mode.CMD_LINE -> EngineMode.CMD_LINE(returnTo.toEngineMode())
-    is Mode.REPLACE -> EngineMode.REPLACE
+    Mode.SELECT_LINE -> EngineMode.SELECT(
+      com.maddyhome.idea.vim.state.mode.SelectionType.LINE_WISE, 
+      EngineMode.NORMAL()
+    )
+    Mode.SELECT_BLOCK -> EngineMode.SELECT(
+      com.maddyhome.idea.vim.state.mode.SelectionType.BLOCK_WISE, 
+      EngineMode.NORMAL()
+    )
+    
+    // Other modes
+    Mode.INSERT -> EngineMode.INSERT
+    Mode.REPLACE -> EngineMode.REPLACE
+    Mode.COMMAND_LINE -> EngineMode.CMD_LINE(EngineMode.NORMAL())
   }
 }
 
