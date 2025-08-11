6d773785b0d34aee0f8902928b3bdc92d5c0e444
Xinhe Wang <xinhe.wang@jetbrains.com>
2025-08-11 10:59:02 +0100

    Select elements to click

--- src/main/java/com/maddyhome/idea/vim/extension/hints/ToggleHintsAction.kt
+++ src/main/java/com/maddyhome/idea/vim/extension/hints/ToggleHintsAction.kt
@@ -11,13 +11,19 @@ package com.maddyhome.idea.vim.extension.hints
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAwareToggleAction
+import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.wm.WindowManager
 import com.intellij.openapi.wm.impl.IdeGlassPaneImpl
 import com.intellij.ui.JBColor
+import com.intellij.ui.components.JBList
+import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.treeStructure.Tree
+import java.awt.BorderLayout
 import java.awt.Color
 import java.awt.Component
 import java.awt.Container
+import java.awt.Dimension
+import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.SwingUtilities
@@ -30,6 +36,8 @@ class ToggleHintsAction : DumbAwareToggleAction() {
     layout = null // no layout manager (absolute positioning)
     background = JBColor(0x1F000000, 0x1F000000)
   }
+
+  private var hints: List<Hint> = emptyList()
 
   override fun isSelected(e: AnActionEvent): Boolean = enabled
 
@@ -48,6 +56,14 @@ class ToggleHintsAction : DumbAwareToggleAction() {
     if (cover !in glassPane.components) glassPane.add(cover)
     glassPane.isVisible = true
     cover.isVisible = true
+    val dialog = MySelectDialog()
+    if (dialog.showAndGet()) {
+      val selectedOption = dialog.getSelected()
+      println("Selected: $selectedOption")
+      selectedOption?.component?.accessibleContext?.accessibleAction?.doAccessibleAction(0)
+    } else {
+      println("Cancelled")
+    }
 
     enabled = true
   }
@@ -60,18 +76,41 @@ class ToggleHintsAction : DumbAwareToggleAction() {
 
   private fun updateCovers(rootComponent: Component, glassPane: Component) {
     cover.removeAll() // clear existing covers
-    rootComponent.createCovers(glassPane).forEach(cover::add)
+    hints = rootComponent.createCovers(glassPane)
+    hints.map(Hint::cover).forEach(cover::add)
     cover.size = glassPane.size
+  }
+
+  private inner class MySelectDialog : DialogWrapper(true) {
+
+    private var list = JBList(*hints.toTypedArray())
+
+    init {
+      title = "Select Component to Click"
+      init()
+    }
+
+    override fun createCenterPanel(): JComponent {
+      val panel = JPanel(BorderLayout())
+
+      val scrollPane = JBScrollPane(list)
+      scrollPane.preferredSize = Dimension(300, 150)
+
+      panel.add(scrollPane, BorderLayout.CENTER)
+      return panel
+    }
+
+    fun getSelected(): Hint? = list.selectedValue
   }
 }
 
 private fun Component.isClickable(): Boolean = (accessibleContext?.accessibleAction?.accessibleActionCount ?: 0) > 0
 
-private class Hint(val component: Component, glassPane: Component) : JPanel() {
+private class Hint(val component: Component, glassPane: Component) {
   val label: String?
     get() = component.accessibleContext?.accessibleName
 
-  init {
+  val cover = JPanel().apply {
     background = JBColor(Color(0, 0, 0, 0), Color(0, 0, 0, 0))
     bounds = SwingUtilities.convertRectangle(component.parent, component.bounds, glassPane)
     border = javax.swing.border.LineBorder(JBColor.GREEN, 2)
@@ -88,6 +127,8 @@ private class Hint(val component: Component, glassPane: Component) : JPanel() {
     }
     isVisible = true
   }
+
+  override fun toString(): String = label ?: "<not labelled>"
 }
 
 /**
