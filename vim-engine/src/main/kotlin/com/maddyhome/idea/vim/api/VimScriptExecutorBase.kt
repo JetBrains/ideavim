139a021d0f56c0a036885ac04fc8d8e159841aae
Matt Ellis <m.t.ellis@gmail.com>
2025-08-22 00:17:30 +0100

    Add cmd history before executing
    But save to register after executing. Matches Vim behaviour

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimScriptExecutorBase.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimScriptExecutorBase.kt
@@ -42,6 +42,11 @@ abstract class VimScriptExecutorBase : VimscriptExecutor {
       val myScript = injector.vimscriptParser.parse(script)
       myScript.units.forEach { it.vimContext = vimContext ?: myScript }
 
+      // Record the command in the history before executing it
+      if (!skipHistory) {
+        injector.historyGroup.addEntry(VimHistory.Type.Command, script)
+      }
+
       for (unit in myScript.units) {
         try {
           val result = unit.execute(editor, context)
@@ -75,11 +80,8 @@ abstract class VimScriptExecutorBase : VimscriptExecutor {
         }
       }
 
-      if (!skipHistory) {
-        injector.historyGroup.addEntry(VimHistory.Type.Command, script)
-        if (myScript.units.size == 1 && myScript.units[0] is Command && myScript.units[0] !is RepeatCommand) {
-          injector.registerGroup.storeTextSpecial(LAST_COMMAND_REGISTER, script)
-        }
+      if (!skipHistory && myScript.units.size == 1 && myScript.units[0] is Command && myScript.units[0] !is RepeatCommand) {
+        injector.registerGroup.storeTextSpecial(LAST_COMMAND_REGISTER, script)
       }
       return finalResult
     } finally {
