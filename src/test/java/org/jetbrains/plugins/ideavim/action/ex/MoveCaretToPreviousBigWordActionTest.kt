9b8153ba8a668e994fc1de36007a2a3369a4b7ef
Matt Ellis <m.t.ellis@gmail.com>
2025-08-22 17:49:57 +0100

    Fix move caret to previous big word to match Vim
    The docs says it moves to the previous WORD, but in practice it moves to the previous whitespace character

--- src/test/java/org/jetbrains/plugins/ideavim/action/ex/MoveCaretToPreviousBigWordActionTest.kt
+++ src/test/java/org/jetbrains/plugins/ideavim/action/ex/MoveCaretToPreviousBigWordActionTest.kt
@@ -12,17 +12,59 @@ import org.junit.jupiter.api.Test
 
 class MoveCaretToPreviousBigWordActionTest : VimExTestCase() {
   @Test
-  fun `test Shift-Left moves caret one WORD left`() {
+  fun `test Shift-Left moves caret to start of current WORD`() {
     typeText(":set incsearch<S-Left>")
-    assertExOffset(4)
+    assertExText("set ${c}incsearch")
+  }
+
+  @Test
+  fun `test Shift-Left moves caret to start of command line`() {
+    typeText(":set incsearch<S-Left><S-Left>")
+    assertExText("${c}set incsearch")
+  }
+
+  @Test
+  fun `test multiple Shift-Left moves caret to previous whitespace`() {
+    typeText(":set     incsearch<S-Left>")
+    assertExText("set     ${c}incsearch")
+
+    typeText("<S-Left><S-Left>")
+    assertExText("set   $c  incsearch")
+  }
+
+  @Test
+  fun `test Shift-Left at start of text does nothing`() {
+    typeText(":set incsearch<C-B>")
+    assertExOffset(0)
     typeText("<S-Left>")
     assertExOffset(0)
   }
 
   @Test
-  fun `test CTRL-Left moves caret one WORD left`() {
+  fun `test Ctrl-Left moves caret to start of current WORD`() {
     typeText(":set incsearch<C-Left>")
-    assertExOffset(4)
+    assertExText("set ${c}incsearch")
+  }
+
+  @Test
+  fun `test Ctrl-Left moves caret to start of command line`() {
+    typeText(":set incsearch<C-Left><C-Left>")
+    assertExText("${c}set incsearch")
+  }
+
+  @Test
+  fun `test multiple Ctrl-Left moves caret to previous whitespace`() {
+    typeText(":set     incsearch<C-Left>")
+    assertExText("set     ${c}incsearch")
+
+    typeText("<C-Left><C-Left>")
+    assertExText("set   $c  incsearch")
+  }
+
+  @Test
+  fun `test Ctrl-Left at start of text does nothing`() {
+    typeText(":set incsearch<C-B>")
+    assertExOffset(0)
     typeText("<C-Left>")
     assertExOffset(0)
   }
--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/ex/MoveCaretToPreviousBigWordAction.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/ex/MoveCaretToPreviousBigWordAction.kt
@@ -11,22 +11,18 @@ package com.maddyhome.idea.vim.action.ex
 import com.intellij.vim.annotations.CommandOrMotion
 import com.intellij.vim.annotations.Mode
 import com.maddyhome.idea.vim.api.VimCommandLine
-import com.maddyhome.idea.vim.api.injector
-import com.maddyhome.idea.vim.handler.Motion
+import com.maddyhome.idea.vim.helper.CharacterHelper
 
 @CommandOrMotion(keys = ["<C-Left>", "<S-Left>"], modes = [Mode.CMD_LINE])
 class MoveCaretToPreviousBigWordAction : CommandLineActionHandler() {
   override fun execute(commandLine: VimCommandLine): Boolean {
+    // The docs say to move one WORD to the left, but in practice, we move to after the previous whitespace character
     val text = commandLine.text
-    // TODO: Should this be WORD or word?
-    val motion = injector.motion.findOffsetOfNextWord(text, commandLine.caret.offset, -1, true, commandLine.editor)
-    when (motion) {
-      is Motion.AbsoluteOffset -> {
-        commandLine.caret.offset = motion.offset
-      }
-
-      else -> {}
+    var pos = commandLine.caret.offset - 1
+    while (pos > 0 && !CharacterHelper.isWhitespace(commandLine.editor, text[pos - 1], true)) {
+      pos--
     }
+    commandLine.caret.offset = pos.coerceAtLeast(0)
     return true
   }
 }
