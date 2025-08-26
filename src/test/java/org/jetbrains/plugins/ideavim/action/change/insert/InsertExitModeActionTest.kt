9125dadc9378f14a1c6412840e93cdacc1985748
NaMinhyeok <nmh9097@gmail.com>
2025-08-26 21:12:57 +0900

    Prevent entering insert mode in read-only files
    Signed-off-by: NaMinhyeok <nmh9097@gmail.com>

--- src/test/java/org/jetbrains/plugins/ideavim/action/change/insert/InsertExitModeActionTest.kt
+++ src/test/java/org/jetbrains/plugins/ideavim/action/change/insert/InsertExitModeActionTest.kt
@@ -11,6 +11,8 @@ package org.jetbrains.plugins.ideavim.action.change.insert
 import com.maddyhome.idea.vim.state.mode.Mode
 import org.jetbrains.plugins.ideavim.VimTestCase
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class InsertExitModeActionTest : VimTestCase() {
   @Test
@@ -21,5 +23,44 @@ class InsertExitModeActionTest : VimTestCase() {
   @Test
   fun `test exit visual mode on line start`() {
     doTest("i<Esc>", "${c}123", "${c}123", Mode.NORMAL())
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = ["i", "a", "o", "O"])
+  fun `test cannot enter insert mode in read-only file`(insertCommand: String) {
+    configureByText("12${c}3")
+    fixture.editor.document.setReadOnly(true)
+
+    typeText(insertCommand)
+
+    assertMode(Mode.NORMAL())
+    assertState("12${c}3")
+  }
+
+  @Test
+  fun `test cannot change text in read-only file with c motion`() {
+    configureByText("12${c}3")
+    fixture.editor.document.setReadOnly(true)
+
+    // Try to change with 'cw' - should prevent the change
+    typeText("cw")
+
+    // Should remain in normal mode
+    assertMode(Mode.NORMAL())
+    assertState("12${c}3")
+  }
+
+  @Test
+  fun `test cannot change text in read-only file with r motion`() {
+    configureByText("12${c}3")
+    fixture.editor.document.setReadOnly(true)
+
+    // Try to change with 'r' - should prevent the change
+    typeText("rX")
+
+    // Should remain in normal mode
+    assertMode(Mode.NORMAL())
+    assertState("12${c}3")
+
   }
 }
