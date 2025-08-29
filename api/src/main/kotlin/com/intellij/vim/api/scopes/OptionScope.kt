0b919b18b75d2aad33cd3ae410e39fb89b6872e2
Alex Plate <aleksei.plate@jetbrains.com>
2025-08-29 19:00:37 +0300

    [API] Fix handling of empty strings in list option functions
    The append, prepend, and remove functions now correctly handle empty
    option values. When an option is empty (""), we use an empty list
    instead of splitting the empty string which would result in [""].
    
    This fixes test failures in OptionScopeTest for operations on empty options.

--- api/src/main/kotlin/com/intellij/vim/api/scopes/OptionScope.kt
+++ api/src/main/kotlin/com/intellij/vim/api/scopes/OptionScope.kt
@@ -189,7 +189,8 @@ fun OptionScope.toggle(name: String) {
  * @param values The values to append (duplicates will be ignored)
  */
 fun OptionScope.append(name: String, vararg values: String) {
-  val currentList = get<String>(name).split()
+  val current = get<String>(name)
+  val currentList = if (current.isEmpty()) emptyList() else current.split()
   val valuesToAdd = values.filterNot { it in currentList }
   val newList = currentList + valuesToAdd
   set(name, newList.joinToString(","))
@@ -213,7 +214,8 @@ fun OptionScope.append(name: String, vararg values: String) {
  * @param values The values to prepend (duplicates will be ignored)
  */
 fun OptionScope.prepend(name: String, vararg values: String) {
-  val currentList = get<String>(name).split()
+  val current = get<String>(name)
+  val currentList = if (current.isEmpty()) emptyList() else current.split()
   val valuesToAdd = values.filterNot { it in currentList }
   val newList = valuesToAdd + currentList
   set(name, newList.joinToString(","))
@@ -235,7 +237,8 @@ fun OptionScope.prepend(name: String, vararg values: String) {
  * @param values The values to remove
  */
 fun OptionScope.remove(name: String, vararg values: String) {
-  val currentList = get<String>(name).split()
+  val current = get<String>(name)
+  val currentList = if (current.isEmpty()) emptyList() else current.split()
   val newList = currentList.filterNot { it in values }
   set(name, newList.joinToString(","))
 }
