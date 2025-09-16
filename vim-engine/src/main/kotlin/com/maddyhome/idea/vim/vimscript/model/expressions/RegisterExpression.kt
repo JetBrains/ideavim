ea4ff522f5ee29651fcc7b7a28c236b53833e512
Matt Ellis <m.t.ellis@gmail.com>
2025-09-17 00:51:54 +0100

    Update RegisterExpression to assign value

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/expressions/RegisterExpression.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/expressions/RegisterExpression.kt
@@ -11,16 +11,41 @@ package com.maddyhome.idea.vim.vimscript.model.expressions
 import com.maddyhome.idea.vim.api.ExecutionContext
 import com.maddyhome.idea.vim.api.VimEditor
 import com.maddyhome.idea.vim.api.injector
-import com.maddyhome.idea.vim.ex.ExException
+import com.maddyhome.idea.vim.ex.exExceptionMessage
+import com.maddyhome.idea.vim.register.RegisterConstants
 import com.maddyhome.idea.vim.vimscript.model.VimLContext
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimString
 
-data class RegisterExpression(val char: Char) : Expression() {
-
+data class RegisterExpression(val char: Char) : LValueExpression() {
   override fun evaluate(editor: VimEditor, context: ExecutionContext, vimContext: VimLContext): VimDataType {
-    val register =
-      injector.registerGroup.getRegister(editor, context, char) ?: throw ExException("Register is not supported yet")
-    return VimString(injector.parser.toPrintableString(register.keys))
+    // TODO: We should return some kind of nullable string here
+    // Assuming @a is uninitialised, Vim returns: `echo type(@a) == v:t_string` => 1, `echo @a == v:null` => 1
+    // While uninitialised register is not the same as an empty register, `echo @a==''` => 1, so this is a reasonable
+    // workaround until we support v:null
+    val register = injector.registerGroup.getRegister(editor, context, char)
+    if (register != null) {
+      return VimString(injector.parser.toPrintableString(register.keys))
+    }
+    return VimString.EMPTY
+  }
+
+  /**
+   * Register expressions are strongly typed, always String
+   *
+   * This means arithmetic operations on a register are not valid, only string concatenation.
+   */
+  override fun isStronglyTyped() = true
+
+  override fun assign(
+    value: VimDataType,
+    editor: VimEditor,
+    context: ExecutionContext,
+    vimContext: VimLContext,
+  ) {
+    if (!RegisterConstants.WRITABLE_REGISTERS.contains(char)) {
+      throw exExceptionMessage("E354", char)
+    }
+    injector.registerGroup.storeText(editor, context, char, value.toVimString().value)
   }
 }
