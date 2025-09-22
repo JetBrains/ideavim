4a784f0947ccc7cd4b73e34b2675847664cc9e1f
Matt Ellis <m.t.ellis@gmail.com>
2025-09-23 00:34:32 +0100

    Remove tests for indexed expressions

--- src/test/java/org/jetbrains/plugins/ideavim/ex/implementation/expressions/SublistExpressionTest.kt
+++ src/test/java/org/jetbrains/plugins/ideavim/ex/implementation/expressions/SublistExpressionTest.kt
@@ -12,37 +12,9 @@ import org.jetbrains.plugins.ideavim.VimTestCase
 import org.junit.jupiter.api.Test
 
 class SublistExpressionTest : VimTestCase("\n") {
-
   @Test
   fun `test string sublist`() {
     assertCommandOutput("echo 'abc'[0:1]", "ab")
-  }
-
-  @Test
-  fun `test negative index with string`() {
-    assertCommandOutput("echo 'abc'[-1]", "")
-  }
-
-  @Test
-  fun `test index greater than size with string`() {
-    assertCommandOutput("echo 'abc'[1000]", "")
-  }
-
-  // TODO: This (and above) are indexed expressions, not sublist expressions
-  @Test
-  fun `test negative index with list`() {
-    assertCommandOutput("echo [1, 2][-1]", "2")
-  }
-
-  @Test
-  fun `test index greater than size with list`() {
-    enterCommand("echo [1, 2][1000]")
-    assertPluginErrorMessage("E684: List index out of range: 1000")
-  }
-
-  @Test
-  fun `test list with correct index`() {
-    assertCommandOutput("echo [1, 2][1]", "2")
   }
 
   @Test
