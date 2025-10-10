d07a154a7723361904bdbeb6ec1e3045a9eb81f6
Matt Ellis <m.t.ellis@gmail.com>
2025-10-10 16:47:06 +0100

    Add Dictionary keys() function

---
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/functions/handlers/dictionaryFunctions/KeysFunctionHandler.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2003-2025 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.vimscript.model.functions.handlers.dictionaryFunctions
+
+import com.intellij.vim.annotations.VimscriptFunction
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.ex.exExceptionMessage
+import com.maddyhome.idea.vim.vimscript.model.VimLContext
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDictionary
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimList
+import com.maddyhome.idea.vim.vimscript.model.functions.UnaryFunctionHandler
+
+@VimscriptFunction(name = "keys")
+internal class KeysFunctionHandler : UnaryFunctionHandler<VimList>() {
+  override fun doFunction(
+    arguments: Arguments,
+    editor: VimEditor,
+    context: ExecutionContext,
+    vimContext: VimLContext,
+  ): VimList {
+    val dict = arguments[0]
+    if (dict !is VimDictionary) {
+      throw exExceptionMessage("E1206", 1)
+    }
+    return VimList(dict.dictionary.keys.toMutableList())
+  }
+}
