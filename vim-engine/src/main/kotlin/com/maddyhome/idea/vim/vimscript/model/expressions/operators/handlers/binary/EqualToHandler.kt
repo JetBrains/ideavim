8b61d33c2c3dea97105a999c6be388b250b8dd52
Matt Ellis <m.t.ellis@gmail.com>
2025-10-23 00:05:57 +0100

    Improve not equal to operator support
    List, Dictionary, Funcref and recursion

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/expressions/operators/handlers/binary/EqualToHandler.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/expressions/operators/handlers/binary/EqualToHandler.kt
@@ -8,11 +8,23 @@
 
 package com.maddyhome.idea.vim.vimscript.model.expressions.operators.handlers.binary
 
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDictionary
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimFuncref
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimList
+import com.maddyhome.idea.vim.vimscript.model.datatypes.asVimInt
 
-internal class EqualToHandler(ignoreCase: Boolean? = null) : ComparisonOperatorHandler(ignoreCase) {
+internal class EqualToHandler(ignoreCase: Boolean? = null) : EqualToHandlerBase(ignoreCase)
+
+internal class NotEqualToHandler(ignoreCase: Boolean? = null) : EqualToHandlerBase(ignoreCase) {
+  override fun performOperation(left: VimDataType, right: VimDataType, ignoreCase: Boolean): VimDataType {
+    val result = super.performOperation(left, right, ignoreCase) as VimInt
+    return (!result.booleanValue).asVimInt()
+  }
+}
+
+internal open class EqualToHandlerBase(ignoreCase: Boolean? = null) : ComparisonOperatorHandler(ignoreCase) {
   override fun compare(left: Double, right: Double) = left == right
   override fun compare(left: Int, right: Int) = left == right
   override fun compare(left: String, right: String, ignoreCase: Boolean) = left.compareTo(right, ignoreCase) == 0
