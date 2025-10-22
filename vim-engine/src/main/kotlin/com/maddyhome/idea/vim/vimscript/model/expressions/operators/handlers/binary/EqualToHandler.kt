4baf80130f36fd1c1eff4d6df5771bcdb3395acd
Matt Ellis <m.t.ellis@gmail.com>
2025-10-22 17:34:43 +0100

    Support equal to operator for Lists

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/expressions/operators/handlers/binary/EqualToHandler.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/expressions/operators/handlers/binary/EqualToHandler.kt
@@ -8,10 +8,29 @@
 
 package com.maddyhome.idea.vim.vimscript.model.expressions.operators.handlers.binary
 
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimList
+
 internal class EqualToHandler(ignoreCase: Boolean? = null) : ComparisonOperatorHandler(ignoreCase) {
   override fun compare(left: Double, right: Double) = left == right
   override fun compare(left: Int, right: Int) = left == right
   override fun compare(left: String, right: String, ignoreCase: Boolean) = left.compareTo(right, ignoreCase) == 0
 
-  // TODO: Implement for List, Dictionary, Funcref, Blob
+  override fun compare(left: VimList, right: VimList, ignoreCase: Boolean, depth: Int): Boolean {
+    // If the recursive structure is deep enough, treat it as equal.
+    // The value is fairly arbitrary but based on Vim's behaviour. Vim will also reduce the limit for every comparison
+    // once we're deep enough, so the tail of a list will both reduce the limit and short-circuit any further
+    // comparisons, including potentially expensive nested comparisons.
+    // So it should be possible to create a data structure that is 1001 levels deep (the first comparison is level 0)
+    // but has different values in the tail of the list, and Vim would still treat it as equal.
+    if (depth > 1000) return true
+
+    if (left === right) return true
+    if (left.values.size != right.values.size) return false
+    if (left.values.isEmpty()) return true
+    return left.values.zip(right.values).all { (first, second) ->
+      first::class == second::class && doCompare(first, second, ignoreCase, depth + 1)
+    }
+  }
+
+  // TODO: Implement for Dictionary, Funcref, Blob
 }
