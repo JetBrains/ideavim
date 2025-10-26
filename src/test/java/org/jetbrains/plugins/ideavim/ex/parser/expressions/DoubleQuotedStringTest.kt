5c6b95cf184fd77bdf8457173c0203e0575b0154
Matt Ellis <m.t.ellis@gmail.com>
2025-10-26 13:18:19 +0000

    Fix test base classes

--- src/test/java/org/jetbrains/plugins/ideavim/ex/parser/expressions/DoubleQuotedStringTest.kt
+++ src/test/java/org/jetbrains/plugins/ideavim/ex/parser/expressions/DoubleQuotedStringTest.kt
@@ -9,12 +9,13 @@
 package org.jetbrains.plugins.ideavim.ex.parser.expressions
 
 import com.maddyhome.idea.vim.api.injector
+import org.jetbrains.plugins.ideavim.VimTestCase
 import org.junit.jupiter.api.Test
 import java.awt.event.InputEvent.CTRL_DOWN_MASK
 import javax.swing.KeyStroke.getKeyStroke
 import kotlin.test.assertEquals
 
-class DoubleQuotedStringTest {
+class DoubleQuotedStringTest : VimTestCase() {
 
   @Test
   fun `test three-digit octal number`() {
