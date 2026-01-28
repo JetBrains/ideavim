5a51b69174d7b99c1c11f382a2737f0caf3adfca
1grzyb1 <wiktor.keska.external@jetbrains.com>
2026-01-28 16:53:33 +0100

    Fix flaky TemplateTest by adjusting escape sequence timing
    Fix flaky inline rename test

--- tests/java-tests/src/test/kotlin/org/jetbrains/plugins/ideavim/group/visual/TemplateTest.kt
+++ tests/java-tests/src/test/kotlin/org/jetbrains/plugins/ideavim/group/visual/TemplateTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2024 The IdeaVim authors
+ * Copyright 2003-2026 The IdeaVim authors
  *
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE.txt file or at
@@ -809,7 +809,6 @@ class TemplateTest : VimJavaTestCase() {
     waitAndAssertMode(fixture, Mode.SELECT(SelectionType.CHARACTER_WISE))
     typeText("myNewVar", "<CR>")  // Typing first char switches to Insert
 
-    // Insert ends after the symbol, then switching to Normal moves back a char
     assertState(
       """
         |class Hello {
