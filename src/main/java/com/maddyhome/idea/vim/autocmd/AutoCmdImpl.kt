29a02a102baeb3f61f888f2642cee3b4a15924f6
1grzyb1 <wiktor.keska.external@jetbrains.com>
2026-02-02 12:12:04 +0100

    VIM-1693 Use thread-safe collections for autocmd event handling

--- src/main/java/com/maddyhome/idea/vim/autocmd/AutoCmdImpl.kt
+++ src/main/java/com/maddyhome/idea/vim/autocmd/AutoCmdImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2025 The IdeaVim authors
+ * Copyright 2003-2026 The IdeaVim authors
  *
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE.txt file or at
@@ -10,14 +10,16 @@ package com.maddyhome.idea.vim.autocmd
 
 import com.maddyhome.idea.vim.api.AutoCmdService
 import com.maddyhome.idea.vim.api.injector
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
 
 class AutoCmdImpl : AutoCmdService {
 
-  private val eventHandlers: MutableMap<AutoCmdEvent, MutableList<AuCommand>> = mutableMapOf()
+  private val eventHandlers: MutableMap<AutoCmdEvent, MutableList<AuCommand>> = ConcurrentHashMap()
   private var currentAugroup: String? = null
 
   override fun registerEventCommand(command: String, event: AutoCmdEvent) {
-    eventHandlers.getOrPut(event) { mutableListOf() }.add(AuCommand(command, currentAugroup))
+    eventHandlers.getOrPut(event) { CopyOnWriteArrayList() }.add(AuCommand(command, currentAugroup))
   }
 
   override fun clearEvents() {
