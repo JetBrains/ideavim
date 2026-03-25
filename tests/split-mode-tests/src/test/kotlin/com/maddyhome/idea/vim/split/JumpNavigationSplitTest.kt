bbf25b13ad543b8f36db9fda830dced22107d504
1grzyb1 <wiktor.keska.external@jetbrains.com>
2026-03-25 11:04:19 +0100

    VIM-3918 Add split-mode tests for jump navigation

---
+++ tests/split-mode-tests/src/test/kotlin/com/maddyhome/idea/vim/split/JumpNavigationSplitTest.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2003-2026 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.split
+
+import org.junit.jupiter.api.Test
+
+class JumpNavigationSplitTest : IdeaVimStarterTestBase() {
+
+  private fun longFile(name: String): String {
+    val lines = (1..50).joinToString("\n") { "Line $it of content" }
+    return createFile("src/$name.txt", lines + "\n")
+  }
+
+  @Test
+  fun `G motion creates jump and Ctrl-O navigates back`() {
+    openFile(longFile("Jump1"))
+
+    typeVim("G")
+    pause(300)
+    assertCaretAfter(40, "G should go to end of file")
+
+    ctrlO()
+    pause(300)
+    assertCaretBefore(10, "Ctrl-O should jump back to start")
+  }
+
+  @Test
+  fun `search creates jump and Ctrl-O navigates back`() {
+    openFile(longFile("Jump2"))
+
+    typeVim("gg")
+    pause(300)
+
+    typeVim("/Line 30\n")
+    pause()
+    assertCaretAfter(20, "Search should jump past line 20")
+
+    esc()
+    ctrlO()
+    pause()
+    assertCaretBefore(10, "Ctrl-O should jump back near start after search")
+  }
+}
