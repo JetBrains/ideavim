JetBrains/ideavim

commit ed50fa28f50289047805701ceb1f5a90b3871248

Refactor argument handling for functions

src/main/java/com/maddyhome/idea/vim/vimscript/model/functions/handlers/PopupMenuVisibleFunctionHandler.kt
@@ -13,22 +13,12 @@ import com.intellij.vim.annotations.VimscriptFunction
 import com.maddyhome.idea.vim.api.ExecutionContext
 import com.maddyhome.idea.vim.api.VimEditor
 import com.maddyhome.idea.vim.vimscript.model.VimLContext
-import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
-import com.maddyhome.idea.vim.vimscript.model.expressions.Expression
-import com.maddyhome.idea.vim.vimscript.model.functions.FunctionHandler
+import com.maddyhome.idea.vim.vimscript.model.datatypes.asVimInt
+import com.maddyhome.idea.vim.vimscript.model.functions.FunctionHandlerBase
 
 @VimscriptFunction(name = "pumvisible")
-internal class PopupMenuVisibleFunctionHandler : FunctionHandler() {
-  override fun doFunction(
-    argumentValues: List<Expression>,
-    editor: VimEditor,
-    context: ExecutionContext,
-    vimContext: VimLContext,
-  ): VimDataType {
-    return if (CompletionService.getCompletionService().currentCompletion == null)
-      VimInt.ZERO
-    else
-      VimInt.ONE
-  }
+internal class PopupMenuVisibleFunctionHandler : FunctionHandlerBase<VimInt>() {
+  override fun doFunction(arguments: Arguments, editor: VimEditor, context: ExecutionContext, vimContext: VimLContext) =
+    (CompletionService.getCompletionService().currentCompletion == null).asVimInt()
 }

src/main/java/com/maddyhome/idea/vim/vimscript/model/functions/handlers/HasFunctionHandler.kt
@@ -15,22 +15,16 @@ import com.maddyhome.idea.vim.api.ExecutionContext
 import com.maddyhome.idea.vim.api.VimEditor
 import com.maddyhome.idea.vim.statistic.VimscriptState
 import com.maddyhome.idea.vim.vimscript.model.VimLContext
-import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
 import com.maddyhome.idea.vim.vimscript.model.datatypes.asVimInt
-import com.maddyhome.idea.vim.vimscript.model.expressions.Expression
-import com.maddyhome.idea.vim.vimscript.model.functions.FunctionHandler
+import com.maddyhome.idea.vim.vimscript.model.functions.FunctionHandlerBase
 
 @VimscriptFunction(name = "has")
-internal class HasFunctionHandler : FunctionHandler(minArity = 1, maxArity = 2) {
+internal class HasFunctionHandler : FunctionHandlerBase<VimInt>(minArity = 1, maxArity = 2) {
   private val supportedFeatures = Features.discover()
 
-  override fun doFunction(
-    argumentValues: List<Expression>,
-    editor: VimEditor,
-    context: ExecutionContext,
-    vimContext: VimLContext,
-  ): VimDataType {
-    val feature = argumentValues[0].evaluate(editor, context, vimContext).toVimString().value
+  override fun doFunction(arguments: Arguments, editor: VimEditor, context: ExecutionContext, vimContext: VimLContext): VimInt {
+    val feature = arguments.getString(0).value
     if (feature == "ide") {
       VimscriptState.Util.isIDESpecificConfigurationUsed = true
     }

src/test/java/org/jetbrains/plugins/ideavim/ex/implementation/expressions/datatypes/VimFuncrefTest.kt
@@ -19,8 +19,7 @@ import com.maddyhome.idea.vim.vimscript.model.datatypes.VimFuncref.Type
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimList
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimString
-import com.maddyhome.idea.vim.vimscript.model.expressions.Expression
-import com.maddyhome.idea.vim.vimscript.model.functions.FunctionHandler
+import com.maddyhome.idea.vim.vimscript.model.functions.UnaryFunctionHandler
 import org.jetbrains.plugins.ideavim.VimBehaviorDiffers
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -203,16 +202,13 @@ class VimFuncrefTest : VimDataTypeTest() {
   }
 
   // We'll never call this
-  object FakeHandler: FunctionHandler() {
+  object FakeHandler: UnaryFunctionHandler<VimDataType>() {
     init {
       name = "Fake"
     }
 
-    override val minimumNumberOfArguments = 1
-    override val maximumNumberOfArguments = 2
-
     override fun doFunction(
-      argumentValues: List<Expression>,
+      arguments: Arguments,
       editor: VimEditor,
       context: ExecutionContext,
       vimContext: VimLContext,

src/test/java/org/jetbrains/plugins/ideavim/ex/implementation/statements/FunctionDeclarationTest.kt
@@ -41,6 +41,30 @@ class FunctionDeclarationTest : VimTestCase() {
   }
 
   @Test
+  fun `test function arguments are eagerly evaluated left to right`() {
+    configureByText("\n")
+    typeText(
+      commandToKeys("""
+        |function printArg(arg) |
+        |    echo "arg: " . a:arg |
+        |endfunction |
+        |function test(x, y, z) |
+        |    echo "Function call done" |
+        |endfunction |
+      """.trimMargin()
+      )
+    )
+    enterCommand("call test(printArg(1), printArg(2), printArg(3))")
+    assertExOutput("""
+        |arg: 1
+        |arg: 2
+        |arg: 3
+        |Function call done
+      """.trimMargin()
+    )
+  }
+
+  @Test
   fun `test nested function`() {
     configureByText("\n")
     typeText(

vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/functions/DefinedFunctionHandler.kt
@@ -23,14 +23,13 @@ import com.maddyhome.idea.vim.vimscript.model.VimLContext
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimList
-import com.maddyhome.idea.vim.vimscript.model.expressions.Expression
 import com.maddyhome.idea.vim.vimscript.model.expressions.Scope
-import com.maddyhome.idea.vim.vimscript.model.expressions.VariableExpression
 import com.maddyhome.idea.vim.vimscript.model.statements.FunctionDeclaration
 import com.maddyhome.idea.vim.vimscript.model.statements.FunctionFlag
 
 data class DefinedFunctionHandler(val function: FunctionDeclaration) :
-  FunctionHandler(function.args.size, getMaxArity(function)) {
+  FunctionHandlerBase<VimDataType>(function.args.size, getMaxArity(function)) {
+
   private val logger = vimLogger<DefinedFunctionHandler>()
   override val scope: Scope? = function.scope
 
@@ -44,7 +43,7 @@ data class DefinedFunctionHandler(val function: FunctionDeclaration) :
   }
 
   override fun doFunction(
-    argumentValues: List<Expression>,
+    arguments: Arguments,
     editor: VimEditor,
     context: ExecutionContext,
     vimContext: VimLContext,
@@ -58,29 +57,14 @@ data class DefinedFunctionHandler(val function: FunctionDeclaration) :
         addAddresses(Address.createRangeAddresses(".", 0, false)!!)
       }
     }
-    initializeFunctionVariables(argumentValues, editor, context, vimContext)
+    initializeFunctionVariables(arguments, editor, context, vimContext)
 
     if (function.flags.contains(FunctionFlag.RANGE)) {
-      val line = (injector.variableService.getNonNullVariableValue(
-        VariableExpression(Scope.FUNCTION_VARIABLE, "firstline"),
-        editor,
-        context,
-        function
-      ) as VimInt).value
+      val line = arguments.getVariable("firstline", function).toVimNumber().value
       returnValue = executeBodyForLine(line, isRangeGiven, exceptionsCaught, editor, context)
     } else {
-      val firstLine = (injector.variableService.getNonNullVariableValue(
-        VariableExpression(Scope.FUNCTION_VARIABLE, "firstline"),
-        editor,
-        context,
-        function
-      ) as VimInt).value
-      val lastLine = (injector.variableService.getNonNullVariableValue(
-        VariableExpression(Scope.FUNCTION_VARIABLE, "lastline"),
-        editor,
-        context,
-        function
-      ) as VimInt).value
+      val firstLine = arguments.getVariable("firstline", function).toVimNumber().value
+      val lastLine = arguments.getVariable("lastline", function).toVimNumber().value
       for (line in firstLine..lastLine) {
         returnValue = executeBodyForLine(line, isRangeGiven, exceptionsCaught, editor, context)
       }
@@ -90,7 +74,7 @@ data class DefinedFunctionHandler(val function: FunctionDeclaration) :
       injector.messages.indicateError()
       injector.messages.showStatusBarMessage(editor, exceptionsCaught.last().message)
     }
-    return returnValue ?: VimInt(0)
+    return returnValue ?: VimInt.ZERO
   }
 
   private fun executeBodyForLine(
@@ -151,65 +135,39 @@ data class DefinedFunctionHandler(val function: FunctionDeclaration) :
   }
 
   private fun initializeFunctionVariables(
-    argumentValues: List<Expression>,
+    arguments: Arguments,
     editor: VimEditor,
     context: ExecutionContext,
     functionCallContext: VimLContext,
   ) {
     // non-optional function arguments
     for ((index, name) in function.args.withIndex()) {
-      injector.variableService.storeVariable(
-        VariableExpression(Scope.FUNCTION_VARIABLE, name),
-        argumentValues[index].evaluate(editor, context, functionCallContext),
-        editor,
-        context,
-        function,
-      )
+      arguments.setVariable(name, arguments[index], editor, context, function)
     }
     // optional function arguments with default values
     for (index in 0 until function.defaultArgs.size) {
-      val expressionToStore =
-        if (index + function.args.size < argumentValues.size) argumentValues[index + function.args.size] else function.defaultArgs[index].second
-      injector.variableService.storeVariable(
-        VariableExpression(Scope.FUNCTION_VARIABLE, function.defaultArgs[index].first),
-        expressionToStore.evaluate(editor, context, functionCallContext),
-        editor,
-        context,
-        function,
-      )
+      val expressionToStore = if (index + function.args.size < arguments.size) {
+        arguments[index + function.args.size]
+      } else {
+        function.defaultArgs[index].second.evaluate(editor, context, functionCallContext)
+      }
+      arguments.setVariable(function.defaultArgs[index].first, expressionToStore, editor, context, function)
     }
     // all the other optional arguments passed to function are stored in a:000 variable
     if (function.hasOptionalArguments) {
-      val remainingArgs = if (function.args.size + function.defaultArgs.size < argumentValues.size) {
-        VimList(
-          argumentValues.subList(function.args.size + function.defaultArgs.size, argumentValues.size)
-            .map { it.evaluate(editor, context, functionCallContext) }.toMutableList(),
-        )
+      val remainingArgs = if (function.args.size + function.defaultArgs.size < arguments.size) {
+        val list = mutableListOf<VimDataType>()
+        for (i in function.args.size + function.defaultArgs.size until arguments.size) {
+          list.add(arguments[i])
+        }
+        VimList(list)
       } else {
         VimList(mutableListOf())
       }
-      injector.variableService.storeVariable(
-        VariableExpression(Scope.FUNCTION_VARIABLE, "000"),
-        remainingArgs,
-        editor,
-        context,
-        function,
-      )
+      arguments.setVariable("000", remainingArgs, editor, context, function)
     }
     val lineRange = range!!.getLineRange(editor, editor.currentCaret())
-    injector.variableService.storeVariable(
-      VariableExpression(Scope.FUNCTION_VARIABLE, "firstline"),
-      VimInt(lineRange.startLine + 1),
-      editor,
-      context,
-      function,
-    )
-    injector.variableService.storeVariable(
-      VariableExpression(Scope.FUNCTION_VARIABLE, "lastline"),
-      VimInt(lineRange.endLine + 1),
-      editor,
-      context,
-      function,
-    )
+    arguments.setVariable("firstline", VimInt(lineRange.startLine1), editor, context, function)
+    arguments.setVariable("lastline", VimInt(lineRange.endLine1), editor, context, function)
   }
 }

vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/functions/FunctionHandler.kt
@@ -10,12 +10,29 @@ package com.maddyhome.idea.vim.vimscript.model.functions
 
 import com.maddyhome.idea.vim.api.ExecutionContext
 import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.api.injector
 import com.maddyhome.idea.vim.ex.exExceptionMessage
 import com.maddyhome.idea.vim.ex.ranges.Range
 import com.maddyhome.idea.vim.vimscript.model.VimLContext
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimFloat
+import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
 import com.maddyhome.idea.vim.vimscript.model.expressions.Expression
 import com.maddyhome.idea.vim.vimscript.model.expressions.Scope
+import com.maddyhome.idea.vim.vimscript.model.expressions.VariableExpression
+
+interface FunctionHandler {
+  val name: String
+  val scope: Scope?
+  var range: Range?
+
+  fun executeFunction(
+    arguments: List<Expression>,
+    editor: VimEditor,
+    context: ExecutionContext,
+    vimContext: VimLContext,
+  ): VimDataType
+}
 
 /**
  * Base class for all function handlers
@@ -23,31 +40,35 @@ import com.maddyhome.idea.vim.vimscript.model.expressions.Scope
  * Pass in the minimum and maximum number of arguments for the function. If [maxArity] is null, then the function has
  * optional arguments.
  */
-abstract class FunctionHandler(protected val minArity: Int = 0, protected val maxArity: Int? = null) {
+abstract class FunctionHandlerBase<T : VimDataType>(protected val minArity: Int = 0, protected val maxArity: Int? = null)
+  : FunctionHandler {
   constructor(arity: Int) : this(arity, arity)
 
-  lateinit var name: String
-  open val scope: Scope? = null
-  var range: Range? = null
+  override lateinit var name: String
+  override val scope: Scope? = null
+  override var range: Range? = null
 
-  protected abstract fun doFunction(
-    argumentValues: List<Expression>,
-    editor: VimEditor,
-    context: ExecutionContext,
-    vimContext: VimLContext,
-  ): VimDataType
-
-  fun executeFunction(
+  override fun executeFunction(
     arguments: List<Expression>,
     editor: VimEditor,
     context: ExecutionContext,
     vimContext: VimLContext,
-  ): VimDataType {
+  ): T {
     checkFunctionCall(arguments)
-    val result = doFunction(arguments, editor, context, vimContext)
+
+    // It's fairly trivial to confirm that Vim eagerly evaluates all function arguments from left to right
+    val values = arguments.map { it.evaluate(editor, context, vimContext) }
+    val result = doFunction(Arguments(values, editor, context), editor, context, vimContext)
     range = null
     return result
   }
+
+  protected abstract fun doFunction(
+    arguments: Arguments,
+    editor: VimEditor,
+    context: ExecutionContext,
+    vimContext: VimLContext,
+  ): T
 
   private fun checkFunctionCall(arguments: List<Expression>) {
     if (arguments.size < minArity) {
@@ -59,8 +80,55 @@ abstract class FunctionHandler(protected val minArity: Int = 0, protected val ma
       throw exExceptionMessage("E118", name)
     }
   }
+
+  protected class Arguments(
+    private val arguments: List<VimDataType>,
+    private val editor: VimEditor,
+    private val context: ExecutionContext,
+  ) {
+    val size = arguments.size
+
+    operator fun get(index: Int) = arguments[index]
+    fun getOrNull(index: Int) = arguments.getOrNull(index)
+
+    fun getNumber(index: Int) = arguments[index].toVimNumber()
+    fun getNumberOrNull(index: Int) = arguments.getOrNull(index)?.toVimNumber()
+    fun getString(index: Int) = arguments[index].toVimString()
+    fun getStringOrNull(index: Int) = arguments.getOrNull(index)?.toVimString()
+
+    fun getNumberOrFloat(index: Int) = when(arguments[index]) {
+      is VimFloat -> arguments[index].toVimFloat().value
+      is VimInt -> arguments[index].toVimNumber().value.toDouble()
+      else -> throw exExceptionMessage("E808")  // E808: Number or Float required
+    }
+
+    fun getVariable(name: String, vimContext: VimLContext): VimDataType {
+      return injector.variableService.getNonNullVariableValue(
+        VariableExpression(Scope.FUNCTION_VARIABLE, name),
+        editor,
+        context,
+        vimContext
+      )
+    }
+
+    fun setVariable(
+      name: String,
+      value: VimDataType,
+      editor: VimEditor,
+      context: ExecutionContext,
+      vimContext: VimLContext,
+    ) {
+      injector.variableService.storeVariable(
+        VariableExpression(Scope.FUNCTION_VARIABLE, name),
+        value,
+        editor,
+        context,
+        vimContext,
+      )
+    }
+  }
 }
 
-abstract class UnaryFunctionHandler : FunctionHandler(arity = 1)
-abstract class BinaryFunctionHandler : FunctionHandler(arity = 2)
-abstract class VariadicFunctionHandler(minArity: Int) : FunctionHandler(minArity, null)
+abstract class UnaryFunctionHandler<T : VimDataType> : FunctionHandlerBase<T>(arity = 1)
+abstract class BinaryFunctionHandler<T : VimDataType> : FunctionHandlerBase<T>(arity = 2)
+abstract class VariadicFunctionHandler<T : VimDataType>(minArity: Int) : FunctionHandlerBase<T>(minArity, null)

vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/functions/LazyVimscriptFunction.kt
@@ -14,7 +14,7 @@ class LazyVimscriptFunction(val name: String, className: String, classLoader: Cl
   LazyInstance<FunctionHandler>(className, classLoader) {
   override val instance: FunctionHandler by lazy {
     val function = super.instance
-    function.name = name
+    (function as? FunctionHandlerBase<*>)?.let { it.name = name }
     function
   }
 }

vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/functions/handlers/CopyFunctionHandler.kt
@@ -13,21 +13,16 @@ import com.maddyhome.idea.vim.api.ExecutionContext
 import com.maddyhome.idea.vim.api.VimEditor
 import com.maddyhome.idea.vim.vimscript.model.VimLContext
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimDataType
-import com.maddyhome.idea.vim.vimscript.model.expressions.Expression
-import com.maddyhome.idea.vim.vimscript.model.functions.FunctionHandler
+import com.maddyhome.idea.vim.vimscript.model.functions.UnaryFunctionHandler
 
 @VimscriptFunction(name = "copy")
-internal class CopyFunctionHandler : FunctionHandler() {
-  override val minimumNumberOfArguments = 1
-  override val maximumNumberOfArguments = 1
-
+internal class CopyFunctionHandler : UnaryFunctionHandler<VimDataType>() {
   override fun doFunction(
-    argumentValues: List<Expression>,
+    arguments: Arguments,
     editor: VimEditor,
     context: ExecutionContext,
     vimContext: VimLContext,
   ): VimDataType {
-    val expr = argumentValues[0].evaluate(editor, context, vimContext)
-    return expr.copy()
+    return arguments[0].copy()
   }
 }

commit 9bff158d0fa80a17ad9cdbbe9961989d773ec306

Fix pumvisible returning opposite result
The implementation was broken in ed50fa28f50289047805701ceb1f5a90b3871248, which inverted the result but did not invert the condition.

src/main/java/com/maddyhome/idea/vim/vimscript/model/functions/handlers/PopupMenuVisibleFunctionHandler.kt
@@ -20,5 +20,5 @@ import com.maddyhome.idea.vim.vimscript.model.functions.BuiltinFunctionHandler
 @VimscriptFunction(name = "pumvisible")
 internal class PopupMenuVisibleFunctionHandler : BuiltinFunctionHandler<VimInt>() {
   override fun doFunction(arguments: Arguments, editor: VimEditor, context: ExecutionContext, vimContext: VimLContext) =
-    (CompletionService.getCompletionService().currentCompletion == null).asVimInt()
+    (CompletionService.getCompletionService().currentCompletion != null).asVimInt()
 }