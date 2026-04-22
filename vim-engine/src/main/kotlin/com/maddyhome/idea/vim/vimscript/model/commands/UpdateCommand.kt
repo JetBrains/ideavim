572eb34a5006cc1d2427de99e71d7a2d321ef6d6
1grzyb1 <wiktor.keska.external@jetbrains.com>
2026-04-22 08:53:35 +0200

    VIM-4207 Add support for update command

---
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/vimscript/model/commands/UpdateCommand.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2003-2026 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.vimscript.model.commands
+
+import com.intellij.vim.annotations.ExCommand
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.api.injector
+import com.maddyhome.idea.vim.command.OperatorArguments
+import com.maddyhome.idea.vim.ex.ranges.Range
+import com.maddyhome.idea.vim.vimscript.model.ExecutionResult
+
+/**
+ * see "h :update"
+ *
+ * Like `:write`, but only writes when the buffer has been modified.
+ *
+ * XXX: [argument] is currently unused. When `:update {file}` support is added, expand [argument] using
+ * [VimPathExpansion.expandPath] to support environment variables (`$VAR`, `${VAR}`) and tilde (`~`, `~/`).
+ */
+@ExCommand(command = "up[date]")
+data class UpdateCommand(val range: Range, val modifier: CommandModifier, val argument: String) :
+  Command.SingleExecution(range, modifier, argument) {
+
+  override val argFlags: CommandHandlerFlags =
+    flags(RangeFlag.RANGE_OPTIONAL, ArgumentFlag.ARGUMENT_OPTIONAL, Access.READ_ONLY)
+
+  override fun processCommand(
+    editor: VimEditor,
+    context: ExecutionContext,
+    operatorArguments: OperatorArguments,
+  ): ExecutionResult {
+    if (editor.hasUnsavedChanges()) {
+      injector.file.saveFile(editor, context)
+    }
+    return ExecutionResult.Success
+  }
+}
