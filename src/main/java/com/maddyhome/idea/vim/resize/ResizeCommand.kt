c2d8da03fcc2a0d7183506d1286ec0059b426d32
1grzyb1 <wiktor.keska.external@jetbrains.com>
2026-06-03 09:48:29 +0200

    VIM-1638 Resize windows height

---
+++ src/main/java/com/maddyhome/idea/vim/resize/ResizeCommand.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2003-2026 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.resize
+
+import com.intellij.vim.annotations.ExCommand
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.command.OperatorArguments
+import com.maddyhome.idea.vim.ex.ranges.Range
+import com.maddyhome.idea.vim.vimscript.model.ExecutionResult
+import com.maddyhome.idea.vim.vimscript.model.commands.Command
+import com.maddyhome.idea.vim.vimscript.model.commands.CommandModifier
+
+/**
+ * Partial implementation of `:resize` (`:res`), which sets the *height* of the current window.
+ * The actual resizing is delegated to [ResizeService].
+ *
+ * Supported argument forms:
+ * - `:resize {n}`  - set the window height to `n` rows
+ * - `:resize +{n}` - increase the height by `n` rows
+ * - `:resize -{n}` - decrease the height by `n` rows
+ * - `:resize`      - maximise the window height
+ *
+ * `:vertical resize` (width) is not supported yet: IdeaVim does not parse the `:vertical` modifier.
+ *
+ * see "h :resize"
+ */
+@ExCommand(command = "res[ize]")
+internal data class ResizeCommand(val range: Range, val modifier: CommandModifier, val argument: String) :
+  Command.SingleExecution(range, modifier) {
+
+  override val argFlags = flags(RangeFlag.RANGE_OPTIONAL, ArgumentFlag.ARGUMENT_OPTIONAL, Access.SELF_SYNCHRONIZED)
+
+  override fun processCommand(
+    editor: VimEditor,
+    context: ExecutionContext,
+    operatorArguments: OperatorArguments,
+  ): ExecutionResult {
+    val resizeArgument = ResizeArgument.parse(argument)
+    ResizeService().resizeCurrentWindowHeight(editor, resizeArgument)
+    return ExecutionResult.Success
+  }
+}
---
+++ src/main/java/com/maddyhome/idea/vim/resize/ResizeWindowActions.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2003-2026 The IdeaVim authors
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.maddyhome.idea.vim.resize
+
+import com.intellij.vim.annotations.CommandOrMotion
+import com.intellij.vim.annotations.Mode
+import com.maddyhome.idea.vim.api.ExecutionContext
+import com.maddyhome.idea.vim.api.VimEditor
+import com.maddyhome.idea.vim.command.Command
+import com.maddyhome.idea.vim.command.OperatorArguments
+import com.maddyhome.idea.vim.handler.VimActionHandler
+
+/**
+ * `CTRL-W +` - increase the current window's height by [count][Command.count] rows (default 1).
+ *
+ * see "h CTRL-W_+"
+ */
+@CommandOrMotion(keys = ["<C-W>+"], modes = [Mode.NORMAL])
+class IncreaseWindowHeightAction : VimActionHandler.SingleExecution() {
+  override val type: Command.Type = Command.Type.OTHER_READONLY
+
+  override fun execute(
+    editor: VimEditor,
+    context: ExecutionContext,
+    cmd: Command,
+    operatorArguments: OperatorArguments,
+  ): Boolean {
+    ResizeService().resizeCurrentWindowHeight(editor, ResizeArgument.Relative(cmd.count))
+    return true
+  }
+}
+
+/**
+ * `CTRL-W -` - decrease the current window's height by [count][Command.count] rows (default 1).
+ *
+ * see "h CTRL-W_-"
+ */
+@CommandOrMotion(keys = ["<C-W>-"], modes = [Mode.NORMAL])
+class DecreaseWindowHeightAction : VimActionHandler.SingleExecution() {
+  override val type: Command.Type = Command.Type.OTHER_READONLY
+
+  override fun execute(
+    editor: VimEditor,
+    context: ExecutionContext,
+    cmd: Command,
+    operatorArguments: OperatorArguments,
+  ): Boolean {
+    ResizeService().resizeCurrentWindowHeight(editor, ResizeArgument.Relative(-cmd.count))
+    return true
+  }
+}
+
+/**
+ * `CTRL-W _` (and `CTRL-W CTRL-_`) - set the current window's height to [count][Command.rawCount] rows,
+ * or maximise it when no count is given.
+ *
+ * see "h CTRL-W__"
+ */
+@CommandOrMotion(keys = ["<C-W>_", "<C-W><C-_>"], modes = [Mode.NORMAL])
+class MaximizeWindowHeightAction : VimActionHandler.SingleExecution() {
+  override val type: Command.Type = Command.Type.OTHER_READONLY
+
+  override fun execute(
+    editor: VimEditor,
+    context: ExecutionContext,
+    cmd: Command,
+    operatorArguments: OperatorArguments,
+  ): Boolean {
+    val argument = if (cmd.rawCount == 0) ResizeArgument.Maximize else ResizeArgument.Absolute(cmd.rawCount)
+    ResizeService().resizeCurrentWindowHeight(editor, argument)
+    return true
+  }
+}
