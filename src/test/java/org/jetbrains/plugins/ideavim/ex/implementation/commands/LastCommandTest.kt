0d5a52953651f3441ab0118a69c5b16003fcbd71
1grzyb1 <wiktor.keska.external@jetbrains.com>
2026-07-24 09:53:29 +0200

    Fix unflaky last command tests

--- src/test/java/org/jetbrains/plugins/ideavim/ex/implementation/commands/LastCommandTest.kt
+++ src/test/java/org/jetbrains/plugins/ideavim/ex/implementation/commands/LastCommandTest.kt
@@ -8,12 +8,14 @@
 
 package org.jetbrains.plugins.ideavim.ex.implementation.commands
 
+import com.intellij.ide.ui.UISettings
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.ComponentManagerEx
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.util.coroutines.childScope
+import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
 import com.intellij.testFramework.replaceService
@@ -22,6 +24,7 @@ import com.maddyhome.idea.vim.vimscript.model.commands.SelectLastFileCommand
 import org.jetbrains.plugins.ideavim.SkipNeovimReason
 import org.jetbrains.plugins.ideavim.TestWithoutNeovim
 import org.jetbrains.plugins.ideavim.VimTestCase
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInfo
@@ -33,6 +36,10 @@ class LastCommandTest : VimTestCase() {
 
   private lateinit var fileEditorManager: FileEditorManagerImpl
 
+  private val uiSettings get() = UISettings.getInstance()
+  private var savedReuseNotModifiedTabs = false
+  private var savedOpenTabsAtTheEnd = false
+
   @BeforeEach
   override fun setUp(testInfo: TestInfo) {
     super.setUp(testInfo)
@@ -43,6 +50,22 @@ class LastCommandTest : VimTestCase() {
         (fixture.project as ComponentManagerEx).getCoroutineScope().childScope(name = "LastCommandTestScope"),
       )
     fixture.project.replaceService(FileEditorManager::class.java, fileEditorManager, fixture.testRootDisposable)
+
+    // Make the tab order deterministic. Otherwise the test is flaky (especially on CI):
+    // - reuseNotModifiedTabs would replace the current unmodified tab when opening the next file, so not all files
+    //   would end up open.
+    // - openTabsAtTheEnd=false inserts new tabs next to the current one, and since the "current" tab is updated
+    //   asynchronously, the resulting order of `openFiles` (which `:last` indexes into) would vary between runs.
+    savedReuseNotModifiedTabs = uiSettings.reuseNotModifiedTabs
+    savedOpenTabsAtTheEnd = uiSettings.openTabsAtTheEnd
+    uiSettings.reuseNotModifiedTabs = false
+    uiSettings.openTabsAtTheEnd = true
+  }
+
+  @AfterEach
+  fun restoreUiSettings() {
+    uiSettings.reuseNotModifiedTabs = savedReuseNotModifiedTabs
+    uiSettings.openTabsAtTheEnd = savedOpenTabsAtTheEnd
   }
 
   // A heavy fixture is required: FileEditorManagerImpl (used to open several files below) is forbidden in light tests.
@@ -67,12 +90,19 @@ class LastCommandTest : VimTestCase() {
     openFile("aaa.txt")
     openFile("bbb.txt")
     val lastFile = openFile("ccc.txt")
+    waitForCondition("Test setup: all three files should be open") { fileEditorManager.openFiles.size == 3 }
 
     // Move the selection away from the last file so that `:last` has something to do
     selectFile(fileEditorManager.openFiles.first())
+    waitForCondition("Test setup: should not start on the last file") {
+      fileEditorManager.selectedFiles.firstOrNull().let { it != null && it != lastFile }
+    }
     assertNotEquals(lastFile, fileEditorManager.selectedFiles.first(), "Test setup: should not start on the last file")
 
     enterCommand("last")
+    waitForCondition("`:last` should select the last open file") {
+      fileEditorManager.selectedFiles.firstOrNull() == lastFile
+    }
 
     assertEquals(lastFile, fileEditorManager.selectedFiles.first())
     assertEquals(fileEditorManager.openFiles.last(), fileEditorManager.selectedFiles.first())
@@ -83,6 +113,7 @@ class LastCommandTest : VimTestCase() {
     ApplicationManager.getApplication().invokeAndWait {
       file = fixture.createFile(filename, "lorem ipsum")
       fixture.openFileInEditor(file!!)
+      PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue()
     }
     return file!!
   }
@@ -90,6 +121,22 @@ class LastCommandTest : VimTestCase() {
   private fun selectFile(file: VirtualFile) {
     ApplicationManager.getApplication().invokeAndWait {
       fileEditorManager.openFile(file, true)
+      PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue()
+    }
+  }
+
+  /**
+   * Opening and selecting files through [FileEditorManagerImpl] completes asynchronously (via its coroutine scope and
+   * focus events), so reading [FileEditorManagerImpl.selectedFiles] / [FileEditorManagerImpl.openFiles] right away can
+   * observe stale state. Pump the event queue until the expected state is reached (or we give up), instead of racing it.
+   */
+  private fun waitForCondition(message: String, condition: () -> Boolean) {
+    ApplicationManager.getApplication().invokeAndWait {
+      repeat(100) {
+        PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue()
+        if (condition()) return@invokeAndWait
+      }
+      assertTrue(condition(), message)
     }
   }
 }
