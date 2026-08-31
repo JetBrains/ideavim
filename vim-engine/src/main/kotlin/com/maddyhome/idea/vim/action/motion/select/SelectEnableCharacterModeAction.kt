22ba489dfd7f40c15f4af4989de0b5cf78c0ec87
AndDe-gourav <gourav.and.de@gmail.com>
2026-08-31 16:57:42 +0530

    select mode on empty line

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/motion/select/SelectEnableCharacterModeAction.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/motion/select/SelectEnableCharacterModeAction.kt
@@ -38,7 +38,7 @@ class SelectEnableCharacterModeAction : VimActionHandler.SingleExecution() {
     editor.nativeCarets().sortedByDescending { it.offset }.forEach { caret ->
       val lineEnd = editor.getLineEndForOffset(caret.offset)
       val offset = caret.offset
-      val nextOffset = (caret.offset + 1).coerceAtMost(lineEnd)
+      val nextOffset = (caret.offset + 1)
       val updatedCaret = caret.moveToInlayAwareOffset(nextOffset)
       updatedCaret.vimSetSystemSelectionSilently(offset, nextOffset)
     }
